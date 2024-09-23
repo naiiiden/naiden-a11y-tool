a9ebbb7f1b6b87c5600ed80d2174775e280743e4
naiiiden <na4ko359@gmail.com>
2024-09-23 23:27:03 +0300

    context menu

--- service_worker.js
+++ service_worker.js
@@ -1,3 +1,19 @@
 chrome.action.onClicked.addListener((tab) => {
     chrome.sidePanel.open({ tabId: tab.id });
 });
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: "openNaidensA11yTool",
+      title: "naiden's a11y tool",
+      contexts: ["all"]
+    });
+  });
+  
+  chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "openNaidensA11yTool") {
+      chrome.sidePanel.open({ tabId: tab.id });
+    }
+  });
+  
+  chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
