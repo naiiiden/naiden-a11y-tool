a44ce6027fcf087695aac55fd69c2db64292126a
naiiiden <na4ko359@gmail.com>
2024-09-24 23:45:04 +0300

    if iframe exists reload page

--- scripts/content.js
+++ scripts/content.js
@@ -1,14 +1,19 @@
 function injectIframe() {
-  const iframeElement = document.createElement('iframe');
-  iframeElement.src = chrome.runtime.getURL('index.html');
-  iframeElement.style.position = 'fixed';
-  iframeElement.style.top = '0';
-  iframeElement.style.left = '0';
-  iframeElement.style.width = '50%';
-  iframeElement.style.height = '50%';
-  iframeElement.style.zIndex = '9999';
+  if (!document.getElementById('my-a11y-tool-iframe')) {
+    const iframeElement = document.createElement('iframe');
+    iframeElement.id = 'my-a11y-tool-iframe';
+    iframeElement.src = chrome.runtime.getURL('index.html');
+    iframeElement.style.position = 'fixed';
+    iframeElement.style.top = '0';
+    iframeElement.style.left = '0';
+    iframeElement.style.width = '50%';
+    iframeElement.style.height = '50%';
+    iframeElement.style.zIndex = '9999';
 
-  document.body.insertBefore(iframeElement, document.body.firstChild);
+    document.body.insertBefore(iframeElement, document.body.firstChild);
+  } else {
+    window.location.reload();
+  }
 }
 
 injectIframe();
