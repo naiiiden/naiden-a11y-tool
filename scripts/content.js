a5e740ab6df4a30db75536ded2d27d4df6c35977
naiiiden <na4ko359@gmail.com>
2024-09-25 21:18:31 +0300

    prevent content script from running inside iframe, inject only into top-level page

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,8 @@
 function injectIframe() {
+  if (window.top !== window.self) {
+    return; // Exit if running inside an iframe
+  }
+
   const naidenA11yIframe = `
     <iframe id="naiden-a11y-tool" src="${chrome.runtime.getURL('index.html')}" style="
       position: fixed;
@@ -7,6 +11,7 @@ function injectIframe() {
       width: 50%;
       height: 50%;
       z-index: 9999;
+      background: #fff;
     "></iframe>
   `;
 
