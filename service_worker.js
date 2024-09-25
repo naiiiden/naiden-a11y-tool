ccd5d097b20ab6c43fb10a7e632b538c97a6972a
naiiiden <na4ko359@gmail.com>
2024-09-25 23:02:57 +0300

    context menu

--- service_worker.js
+++ service_worker.js
@@ -4,3 +4,20 @@ chrome.action.onClicked.addListener((tab) => {
     files: ['scripts/content.js']
   });
 });
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'run_naiden_a11y_tool',
+    title: 'run naiden a11y tool',
+    contexts: ['all']
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'run_naiden_a11y_tool') {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['scripts/content.js']
+    });
+  }
+});
