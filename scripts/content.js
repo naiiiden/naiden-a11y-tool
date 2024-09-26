e7deba798e89e2a8b2ba62257e475943c07ad699
naiiiden <na4ko359@gmail.com>
2024-09-26 23:13:26 +0300

    delete unused commented code, update variable names in content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,18 +1,14 @@
 function injectIframe() {
-  // if (window.top !== window.self) {
-  //   return; // Exit if running inside an iframe
-  // }
-
-  const naidenA11yIframe = `
+  const iframe = `
     <iframe id="naiden-a11y-tool" src="${chrome.runtime.getURL('index.html')}"></iframe>
   `;
 
-  const naidenIframe = document.getElementById('naiden-a11y-tool');
+  const naidenA11yIframe = document.getElementById('naiden-a11y-tool');
 
-  if (!naidenIframe) {
-    document.body.insertAdjacentHTML('afterbegin', naidenA11yIframe);
+  if (!naidenA11yIframe) {
+    document.body.insertAdjacentHTML('afterbegin', iframe);
   } else {
-    naidenIframe.remove();
+    naidenA11yIframe.remove();
     toggleStylesheets(false);
   }
 }
