8b67ef52be2c12097d3bf0de15b6692dc2a67e7b
naiiiden <na4ko359@gmail.com>
2024-09-27 20:42:32 +0300

    open/close panel on action click

--- service_worker.js
+++ service_worker.js
@@ -43,4 +43,6 @@ chrome.action.onClicked.addListener( ( tab ) => {
   } );
   chrome.sidePanel.open( { tabId: tab.id } );
 
-} );
+});
+
+chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
