1f45bd521037a8553e9572b8077a95e752e8e64e
naiiiden <na4ko359@gmail.com>
2024-09-29 23:47:55 +0300

    files update

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-console.log('naiden');
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleCheckbox = document.getElementById("toggle-stylesheets");
+
+  toggleCheckbox.addEventListener("change", () => {
+    const disableStylesheets = toggleCheckbox.checked;
+
+    // Get the active tab and send a message to content.js
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { toggleStylesheets: disableStylesheets });
+    });
+  });
+});
