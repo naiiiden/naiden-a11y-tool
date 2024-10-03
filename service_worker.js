89503be82c2dfd6079dd748962184286179332e1
naiiiden <na4ko359@gmail.com>
2024-10-03 13:54:29 +0300

    proceed with devtools page... until i change my mind again

--- service_worker.js
+++ service_worker.js
@@ -1,30 +1,30 @@
-function insAndExec(tab) {
-  chrome.scripting.executeScript({
-    target: {tabId: tab.id},
-    files: ['scripts/content.js']
-  });
-}
+// function insAndExec(tab) {
+//   chrome.scripting.executeScript({
+//     target: {tabId: tab.id},
+//     files: ['scripts/content.js']
+//   });
+// }
 
-chrome.action.onClicked.addListener((tab) => {
-  insAndExec(tab);
-});
+// chrome.action.onClicked.addListener((tab) => {
+//   insAndExec(tab);
+// });
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: 'run_nano_a11y_tool',
-    title: 'run nano a11y tool',
-    contexts: ['all']
-  });
-});
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.contextMenus.create({
+//     id: 'run_nano_a11y_tool',
+//     title: 'run nano a11y tool',
+//     contexts: ['all']
+//   });
+// });
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === 'run_nano_a11y_tool') {
-    insAndExec(tab);
-  }
-});
+// chrome.contextMenus.onClicked.addListener((info, tab) => {
+//   if (info.menuItemId === 'run_nano_a11y_tool') {
+//     insAndExec(tab);
+//   }
+// });
 
-chrome.commands.onCommand.addListener((command, tab) => {
-  if (command === 'toggle_nano_a11y_tool') {
-    insAndExec(tab);
-  }
-});
+// chrome.commands.onCommand.addListener((command, tab) => {
+//   if (command === 'toggle_nano_a11y_tool') {
+//     insAndExec(tab);
+//   }
+// });
