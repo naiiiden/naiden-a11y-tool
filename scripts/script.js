4072be65a3592bcf1558d9b05e8bad492eec671f
naiiiden <na4ko359@gmail.com>
2024-10-06 21:28:05 +0300

    lang attr check

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,12 @@
 const errors = [
   {
+    id: 0,
+    name: "Page lang attribute missing or invalid",
+    description: "The page is missing language attribute or its value is invalid",
+    wcagLink: "",
+    fix: "Add a valid lang attribute value"
+  },
+  {
     id: 1,
     name: "Page title missing",
     description: "The page is missing a title or the title is empty.",
@@ -80,12 +87,33 @@ async function runAudit() {
   let auditResults = [];
 
   try {
+    const htmlLanguage = await new Promise((resolve) => {
+      chrome.devtools.inspectedWindow.eval("document.documentElement", resolve);
+    });
+
+    const validLangValues = [
+      "ab", "aa", "af", "ak", "sq", "am", "ar", "an", "hy", "as", "av", "ae", "ay", "az", "bm", "ba", "eu", "be", "bn",
+      "bi", "bs", "br", "bg", "my", "ca", "ch", "ce", "zh", "cu", "cv", "kw", "co", "cr", "cs", "da", "dv", "nl", "dz",
+      "en", "eo", "et", "ee", "fo", "fj", "fi", "fr", "fy", "ff", "gd", "gl", "lg", "ka", "de", "ki", "el", "kl", "gn",
+      "gu", "ht", "ha", "he", "hz", "hi", "ho", "hr", "hu", "ig", "is", "io", "ii", "iu", "ie", "ia", "id", "ik", "it",
+      "jv", "ja", "kn", "kr", "ks", "kk", "km", "ki", "rw", "ky", "kv", "kg", "ko", "kj", "ku", "lo", "la", "lv", "li",
+      "ln", "lt", "lb", "lu", "mk", "mg", "ms", "ml", "mt", "gv", "mi", "mr", "mh", "mn", "na", "nv", "nd", "ne", "ng",
+      "nb", "nn", "no", "ii", "nr", "oc", "oj", "or", "om", "os", "pa", "pi", "fa", "pl", "pt", "ps", "qu", "rm", "ro",
+      "rn", "ru", "se", "sm", "sg", "sa", "sc", "sr", "sn", "sd", "si", "sk", "sl", "so", "st", "es", "su", "sw", "ss",
+      "sv", "ta", "te", "tg", "th", "ti", "bo", "tk", "tl", "tn", "to", "tr", "ts", "tt", "tw", "ty", "ug", "uk", "ur",
+      "uz", "ve", "vi", "vo", "wa", "cy", "wo", "xh", "yi", "yo", "za", "zu"
+    ];
+
+    if (!htmlLanguage.lang || !validLangValues.includes(htmlLanguage.split('-')[0])) {
+      auditResults.push(errors[0]);
+    }
+
     const pageTitle = await new Promise((resolve) => {
       chrome.devtools.inspectedWindow.eval("document.title", resolve);
     });
     
     if (!pageTitle || pageTitle === "") {
-      auditResults.push(errors[0]);
+      auditResults.push(errors[1]);
     }
 
     // Check each image for missing alt attributes (and ignore valid empty alts)
@@ -100,14 +128,14 @@ async function runAudit() {
     const missingAltImages = images.filter(img => img.alt === null);
     
     missingAltImages.forEach(img => {
-      const error = { ...errors[1]  }; 
+      const error = { ...errors[2]  }; 
       auditResults.push(error);
     });
 
     const linkedImagesWithoutAlt = images.filter(img => img.isLinked && img.alt === null);
     
     linkedImagesWithoutAlt.forEach(img => {
-      const error = { ...errors[2] };
+      const error = { ...errors[3] };
       auditResults.push(error);
     });
 
