2953a967955065fa3c8329295334f2ff4aef6d54
naiiiden <na4ko359@gmail.com>
2024-10-08 23:52:40 +0300

    missinglabels

--- scripts/formAudit.js
+++ scripts/formAudit.js
@@ -8,4 +8,17 @@ export async function formAudit(auditResults) {
     labels.forEach(() => {
         auditResults.push(formErrors[0]);
     });
+
+    const missingLabels = await new Promise((resolve) => {
+        chrome.devtools.inspectedWindow.eval(`
+            Array.from(document.querySelectorAll('input[id]')).filter(input => {
+                const labelCount = document.querySelectorAll('label[for="' + input.id + '"]').length;
+                return labelCount === 0;
+            }).map(input => input.outerHTML)
+        `, resolve);
+    });
+    
+    missingLabels.forEach(() => {
+        auditResults.push(formErrors[1]); 
+    });
 }
