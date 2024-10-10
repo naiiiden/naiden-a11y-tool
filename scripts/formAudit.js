9c6b67399a1e9c0a4195ffb44323e4ffb09e7c6e
naiiiden <na4ko359@gmail.com>
2024-10-10 17:20:01 +0300

    combine input and select labels check

--- scripts/formAudit.js
+++ scripts/formAudit.js
@@ -9,46 +9,28 @@ export async function formAudit(auditResults) {
         auditResults.push(formErrors[0]);
     });
 
-    const inputLabels = await new Promise((resolve) => {
+    const inputAndSelectLabels = await new Promise((resolve) => {
         chrome.devtools.inspectedWindow.eval(`
-          Array.from(document.querySelectorAll('input[id]')).map(input => {
-            const labelCount = document.querySelectorAll('label[for="' + input.id + '"]').length;
-            return { html: input.outerHTML, labelCount: labelCount };
+          Array.from(document.querySelectorAll('input[id], select[id]')).map(element => {
+            const labelCount = document.querySelectorAll('label[for="' + element.id + '"]').length;
+            return { html: element.outerHTML, labelCount: labelCount, tagName: element.tagName };
           })
         `, resolve);
-      });
+    });
       
-      inputLabels.forEach((input) => {
-        if (input.labelCount === 0) {
-          auditResults.push(formErrors[1]);  
-        } else if (input.labelCount > 1) {
-          auditResults.push(formErrors[2]);
+    inputAndSelectLabels.forEach((element) => {
+        if (element.labelCount === 0) {
+            if (element.tagName === 'INPUT') {
+              auditResults.push(formErrors[1]);
+            } else if (element.tagName === 'SELECT') {
+              auditResults.push(formErrors[3]);
+            }
+        } else if (element.labelCount > 1) {
+            if (element.tagName === 'INPUT') {
+              auditResults.push(formErrors[2]);
+            } else if (element.tagName === 'SELECT') {
+              auditResults.push(formErrors[4]);
+            }
         }
-    });
-
-    const missingSelectLabels = await new Promise((resolve) => {
-        chrome.devtools.inspectedWindow.eval(`
-            Array.from(document.querySelectorAll('select[id]')).filter(select => {
-                const labelCount = document.querySelectorAll('label[for="' + select.id + '"]').length;
-                return labelCount === 0;
-            }).map(select => select.outerHTML)
-        `, resolve);
-    });
-    
-    missingSelectLabels.forEach(() => {
-        auditResults.push(formErrors[3]); 
-    });
-
-    const multipleSelectLabels = await new Promise((resolve) => {
-        chrome.devtools.inspectedWindow.eval(`
-          Array.from(document.querySelectorAll('select[id]')).filter(select => {
-            const labelCount = document.querySelectorAll('label[for="' + select.id + '"]').length;
-            return labelCount > 1;
-          }).map(select => select.outerHTML)
-        `, resolve);
-    });
-    
-    multipleSelectLabels.forEach(() => {
-        auditResults.push(formErrors[4]);
     });
 }
