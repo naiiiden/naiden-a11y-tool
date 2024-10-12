860ecec820c1f3e09f24c4d5c0b97bf3af3be788
naiiiden <na4ko359@gmail.com>
2024-10-12 14:06:16 +0300

    heading hierarchy

--- scripts/semanticAudit.js
+++ scripts/semanticAudit.js
@@ -2,10 +2,37 @@ import { semanticErrors } from "./errors.js";
 
 export async function semanticAudit(auditResults) {
     const hasH1 = await new Promise((resolve) => {
-        chrome.devtools.inspectedWindow.eval(`document.querySelector('h1') !== null`, resolve);
+        chrome.devtools.inspectedWindow.eval(
+        `document.querySelector('h1') !== null`,
+        resolve
+        );
     });
-    
+
     if (!hasH1) {
         auditResults.push(semanticErrors[0]);
+    }
+
+    const headingLevels = await new Promise((resolve) => {
+        chrome.devtools.inspectedWindow.eval(
+            `Array.from(document.querySelectorAll('h1, h2, h3, h4, h5, h6')).map(heading => heading.tagName)`,
+            resolve
+        );
+    });
+
+    if (headingLevels.length > 0) {
+        let previousLevel = 1;
+
+        for (const heading of headingLevels) {
+            const currentLevel = parseInt(heading[1]);
+
+            if (currentLevel > previousLevel + 1) {
+                auditResults.push(semanticErrors[1]);
+                break;
+            }
+
+            if (currentLevel > previousLevel) {
+                previousLevel = currentLevel;
+            }
+        }
     }
 }
