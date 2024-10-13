1510f4a7014679f31058b1e2f8fbe30c1b4e8cea
naiiiden <na4ko359@gmail.com>
2024-10-14 00:35:55 +0300

    css errors

--- scripts/errors.js
+++ scripts/errors.js
@@ -229,3 +229,20 @@ export const semanticErrors = [
     fix: "Leave only one main tag",
   }
 ]
+
+const cssErrors = [
+  {
+    id: 0,
+    name: "Very small text",
+    description: "Text is hard to read",
+    wcagLink: "",
+    fix: "Make it larger",
+  },
+  {
+    id: 1,
+    name: "Underlined text",
+    description: "Text is underlined and looks like a link",
+    wcagLink: "",
+    fix: "Remove underline",
+  }
+]
