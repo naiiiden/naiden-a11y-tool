9b1ef28eb62dbafc357c5f6624c07169f8fad2ad
naiiiden <na4ko359@gmail.com>
2024-10-18 14:25:40 +0300

    empty headings refactor update selectors

--- scripts/emptyAudit.js
+++ scripts/emptyAudit.js
@@ -2,17 +2,16 @@ import { emptyErrors } from "./errors.js";
 import { getUniqueSelector, inspectedWindowEval } from "./utils.js";
 
 export async function emptyAudit(auditResults) {
-    const headings = await inspectedWindowEval(`
+    const emptyHeadings = await inspectedWindowEval(`
       const getUniqueSelector = ${getUniqueSelector.toString()};
-      return Array.from(document.querySelectorAll("h1:not(:has(img)), h2:not(:has(img)), h3:not(:has(img)), h4:not(:has(img)), h5:not(:has(img)), h6:not(:has(img))"))
-        .filter(heading => heading.innerText.trim() === "")
+      return Array.from(document.querySelectorAll("h1:not(:has(img)):empty, h2:not(:has(img)):empty, h3:not(:has(img)):empty, h4:not(:has(img)):empty, h5:not(:has(img)):empty, h6:not(:has(img)):empty"))
         .map(heading => ({
           outerHTML: heading.outerHTML,
           selector: getUniqueSelector(heading)
         }));
     `) 
 
-    headings.forEach(heading => {
+    emptyHeadings.forEach(heading => {
         auditResults.push({ ...emptyErrors[0], element: heading.outerHTML, selector: heading.selector });
     });
 
