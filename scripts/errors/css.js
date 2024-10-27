27568318baa72e7d066ad04ea579f9641172c7e4
naiiiden <na4ko359@gmail.com>
2024-10-27 22:34:24 +0200

    css erors

---
+++ scripts/errors/css.js
@@ -0,0 +1,16 @@
+const cssErrors = [
+    {
+      id: 0,
+      name: "Very small text",
+      description: "Text is hard to read",
+      wcagLink: "",
+      fix: "Make it larger",
+    },
+    {
+      id: 1,
+      name: "Underlined text",
+      description: "Text is underlined and looks like a link",
+      wcagLink: "",
+      fix: "Remove underline",
+    }
+]
