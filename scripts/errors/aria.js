eba78cd60b4e0bca7692eb1291eeb25344853436
naiiiden <na4ko359@gmail.com>
2024-10-28 23:44:22 +0200

    aria- errors

---
+++ scripts/errors/aria.js
@@ -0,0 +1,9 @@
+export const ariaErrors = [
+    {
+        id: 0,
+        name: "aria-hidden='true' must not be present on document body",
+        description: "aria-hidden='true' must not be present on document body",
+        wcagLink: "",
+        fix: "remove aria-hidden='true' from document body",
+    }
+]
