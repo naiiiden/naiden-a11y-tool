f19a4bb2533bb97e2ccd04f5eb66691ccceff590
naiiiden <na4ko359@gmail.com>
2024-10-31 21:11:13 +0200

    aria meter check

--- scripts/audits/ariaAudit.js
+++ scripts/audits/ariaAudit.js
@@ -30,12 +30,33 @@ export async function ariaAudit(auditResults) {
                 return !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim()));
             })
             .map(element => ({
-            outerHTML: element.outerHTML,
-            selector: getUniqueSelector(element)
+                outerHTML: element.outerHTML,
+                selector: getUniqueSelector(element)
             }));
     `)
 
     ariaCommands.forEach(element => {
         auditResults.push({ ...ariaErrors[1], element: element.outerHTML, selector: element.selector });
+    });
+
+    const ariaMeter = await inspectedWindowEval(`
+        const getUniqueSelector = ${getUniqueSelector.toString()};
+        return Array.from(document.querySelectorAll("[role='meter']"))
+            .filter(element => {
+                const ariaLabel = element.hasAttribute('aria-label') ? element.getAttribute('aria-label').trim() : null;
+                const ariaLabelledby = element.hasAttribute('aria-labelledby') 
+                ? document.getElementById(element.getAttribute('aria-labelledby')) 
+                : null;
+                
+                return !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim()));
+            })
+            .map(element => ({
+                outerHTML: element.outerHTML,
+                selector: getUniqueSelector(element)
+            }));
+    `)
+
+    ariaMeter.forEach(element => {
+        auditResults.push({ ...ariaErrors[2], element: element.outerHTML, selector: element.selector });
     });
 }
