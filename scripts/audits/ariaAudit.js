0afc58a30d9af86ca07e6cfcbe80c0caff2fe444
naiiiden <na4ko359@gmail.com>
2024-11-05 22:12:29 +0200

    aria treeitem name check

--- scripts/audits/ariaAudit.js
+++ scripts/audits/ariaAudit.js
@@ -364,4 +364,25 @@ export async function ariaAudit(auditResults) {
     ariaAppropriateRole.forEach(element => {
         auditResults.push({ ...ariaErrors[12], element: element.outerHTML, selector: element.selector });
     });
+
+    const ariaTreeitemNames = await inspectedWindowEval(`
+        const getUniqueSelector = ${getUniqueSelector.toString()};
+        return Array.from(document.querySelectorAll("[role='treeitem']:empty"))
+            .filter(element => {
+                const ariaLabel = element.hasAttribute('aria-label') ? element.getAttribute('aria-label').trim() : null;
+                const ariaLabelledby = element.hasAttribute('aria-labelledby') 
+                ? document.getElementById(element.getAttribute('aria-labelledby')) 
+                : null;
+                
+                return !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim()));
+            })
+            .map(element => ({
+                outerHTML: element.outerHTML,
+                selector: getUniqueSelector(element)
+            }));
+    `)
+
+    ariaTreeitemNames.forEach(element => {
+        auditResults.push({ ...ariaErrors[13], element: element.outerHTML, selector: element.selector });
+    });
 }
