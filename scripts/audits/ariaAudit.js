6c0b5bef805b711342368e7590bf8b3479d53970
naiiiden <na4ko359@gmail.com>
2024-11-05 10:39:59 +0200

    add missing template & textarea selectors

--- scripts/audits/ariaAudit.js
+++ scripts/audits/ariaAudit.js
@@ -340,6 +340,8 @@ export async function ariaAudit(auditResults) {
             span[role]:is([role='generic']), 
             style[role]:not([role='']), 
             summary:is(details summary)[role]:not([role='']), 
+            template[role]:not([role='']), 
+            textarea[role]:not([role=''], [role='textbox']), 
             th:is(table[role='table'] th)[role]:not([role='columnheader'], [role='rowheader'], [role='cell']), 
             th:is(:is(table[role='grid'], table[role='treegrid']) th)[role]:not([role='columnheader'], [role='rowheader'], [role='gridcell']), 
             title[role], 
