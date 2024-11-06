f5cdf6a4fc821dbe9e0fcaaaf7679f3ff56f20fb
naiiiden <na4ko359@gmail.com>
2024-11-06 22:18:31 +0200

    widget attributes

--- scripts/audits/ariaAudit.js
+++ scripts/audits/ariaAudit.js
@@ -399,7 +399,21 @@ export async function ariaAudit(auditResults) {
             [aria-hidden]:not([aria-hidden=''], [aria-hidden='true'], [aria-hidden='false'], [aria-hidden='undefined']), 
             [aria-invalid]:not([aria-invalid=''], [aria-invalid='true'], [aria-invalid='false'], [aria-invalid='grammar'], [aria-invalid='spelling']), 
             [aria-live]:not([aria-live=''], [aria-live='off'], [aria-live='assertive'], [aria-live='polite']), 
-            [aria-relevant]:not([aria-relevant=''], [aria-relevant='additions'], [aria-relevant='additions text'], [aria-relevant='all'], [aria-relevant='removals'], [aria-relevant='text'])
+            [aria-relevant]:not([aria-relevant=''], [aria-relevant='additions'], [aria-relevant='additions text'], [aria-relevant='all'], [aria-relevant='removals'], [aria-relevant='text']), 
+            [aria-autocomplete]:not([aria-autocomplete=''], [aria-autocomplete='inline'], [aria-autocomplete='list'], [aria-autocomplete='both'], [aria-autocomplete='none']), 
+            [aria-checked]:not([aria-checked=''], [aria-checked='true'], [aria-checked='false'], [aria-checked='mixed'], [aria-checked='undefined']), 
+            [aria-disabled]:not([aria-disabled=''], [aria-disabled='true'], [aria-disabled='false']), 
+            [aria-expanded]:not([aria-expanded=''], [aria-expanded='true'], [aria-expanded='false'], [aria-expanded='undefined']), 
+            [aria-modal]:not([aria-modal=''], [aria-modal='true'], [aria-modal='false']), 
+            [aria-multiline]:not([aria-multiline=''], [aria-multiline='true'], [aria-multiline='false']), 
+            [aria-multiselectable]:not([aria-multiselectable=''], [aria-multiselectable='true'], [aria-multiselectable='false']), 
+            [aria-orientation]:not([aria-orientation=''], [aria-orientation='horizontal'], [aria-orientation='vertical'], [aria-orientation='undefined']), 
+            [aria-pressed]:not([aria-pressed=''], [aria-pressed='true'], [aria-pressed='false'], [aria-pressed='mixed'], [aria-pressed='undefined']), 
+            [aria-readonly]:not([aria-readonly=''], [aria-readonly='true'], [aria-readonly='false']), 
+            [aria-required]:not([aria-required=''], [aria-required='true'], [aria-required='false']), 
+            [aria-selected]:not([aria-selected=''], [aria-selected='true'], [aria-selected='false'], [aria-selected='undefined']), 
+            [aria-sort]:not([aria-sort=''], [aria-sort='ascending'], [aria-sort='descending'], [aria-sort='none'], [aria-sort='other']), 
+            
         \`))
             .map(element => ({
                 outerHTML: element.outerHTML,
