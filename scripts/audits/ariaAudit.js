39d0852b694f1de5a629d10eaa745df1bea53acf
naiiiden <na4ko359@gmail.com>
2024-11-08 18:35:17 +0200

    elements^=H tested

--- scripts/audits/ariaAudit.js
+++ scripts/audits/ariaAudit.js
@@ -612,6 +612,12 @@ export async function ariaAudit(auditResults) {
             'aria-describedby', 'aria-details', 'aria-dropeffect', 'aria-errormessage', 'aria-flowto', 'aria-grabbed',
             'aria-haspopup', 'aria-hidden', 'aria-invalid', 'aria-keyshortcuts', 'aria-label', 'aria-labelledby',
             'aria-live', 'aria-owns', 'aria-relevant', 'aria-roledescription'
+        ],
+        heading: [  
+            'aria-atomic', 'aria-busy', 'aria-controls', 'aria-current', 'aria-describedby', 'aria-details',
+            'aria-disabled', 'aria-dropeffect', 'aria-errormessage', 'aria-flowto', 'aria-grabbed', 'aria-haspopup',
+            'aria-hidden', 'aria-invalid', 'aria-keyshortcuts', 'aria-label', 'aria-labelledby', 'aria-live',
+            'aria-owns', 'aria-relevant', 'aria-roledescription', 'aria-level'
         ]
     }
 
