c91c996fd21298d19799f465da9165d395e8a3b4
naiiiden <na4ko359@gmail.com>
2024-11-09 16:16:24 +0200

    aria roles required aria attributes selector

--- scripts/audits/ariaAudit.js
+++ scripts/audits/ariaAudit.js
@@ -980,7 +980,17 @@ export async function ariaAudit(auditResults) {
     const ariaRoleRequiredAriaAttributes = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         return Array.from(document.querySelectorAll(\`
-            [role='checkbox']:not([aria-checked])
+            [role='checkbox']:not([aria-checked]), 
+            [role='combobox']:not([aria-controls], [aria-expanded]), 
+            [role='heading']:not([aria-level]), 
+            [role='meter']:not([aria-valuenow]), 
+            [role='menuitemcheckbox']:not([aria-checked]), 
+            [role='option']:not([aria-selected]), 
+            [role='radio']:not([aria-checked]), 
+            [role='scrollbar']:not([aria-controls], [aria-valuenow]), 
+            [role='separator']:not([aria-valuenow]), 
+            [role='slider']:not([aria-valuenow]), 
+            [role='switch']:not([aria-checked])
         \`))
             .map(element => ({
                 outerHTML: element.outerHTML,
