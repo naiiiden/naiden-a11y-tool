2f44f59cd7670dfb92aee0a746200b5dc7620cc1
naiiiden <na4ko359@gmail.com>
2024-11-10 11:46:46 +0200

    ariarolepermittedattributes selector

--- scripts/audits/ariaAudit.js
+++ scripts/audits/ariaAudit.js
@@ -1072,7 +1072,17 @@ export async function ariaAudit(auditResults) {
     const ariaRolePermittedAttributes = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         return Array.from(document.querySelectorAll(\`
-            [role='caption']:has([aria-label], [aria-labelledby])
+            [role='caption']:has([aria-label], [aria-labelledby]), 
+            [role='code']:has([aria-label], [aria-labelledby]), 
+            [role='deletion']:has([aria-label], [aria-labelledby]), 
+            [role='emphasis']:has([aria-label], [aria-labelledby]), 
+            [role='generic']:has([aria-label], [aria-labelledby], [aria-roledescription]), 
+            [role='insertion']:has([aria-label], [aria-labelledby]), 
+            [role='paragraph']:has([aria-label], [aria-labelledby]), 
+            [role='presentation']:has([aria-label], [aria-labelledby]), 
+            [role='strong']:has([aria-label], [aria-labelledby]), 
+            [role='subscript']:has([aria-label], [aria-labelledby]), 
+            [role='superscript']:has([aria-label], [aria-labelledby])
         \`))
             .map(element => ({
                 outerHTML: element.outerHTML,
