d01dcf0a33d9a77f5347417c0bba819727d365c4
naiiiden <na4ko359@gmail.com>
2024-11-10 11:30:49 +0200

    ariarolerequiredchildren selector

--- scripts/audits/ariaAudit.js
+++ scripts/audits/ariaAudit.js
@@ -1017,7 +1017,19 @@ export async function ariaAudit(auditResults) {
     const ariaRoleRequiredChildren = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         return Array.from(document.querySelectorAll(\`
-            [role='feed']:not(:has([role='article']))
+            [role='feed']:not(:has([role='article'])), 
+            [role='grid']:not(:has([role='row'], [role='rowgroup'])), 
+            [role='list']:not(:has([role='listitem'])), 
+            [role='listbox']:not(:has([role='group'], [role='option'])), 
+            [role='menu']:not(:has([role='group'], [role='menuitem'], [role='menuitemcheckbox'], [role='menuitemradio'])), 
+            [role='menubar']:not(:has([role='group'], [role='menuitem'], [role='menuitemcheckbox'], [role='menuitemradio'])), 
+            [role='radiogroup']:not(:has([role='radio'])), 
+            [role='row']:not(:has([role='cell'], [role='columnheader'], [role='gridcell'], [role='rowheader'])), 
+            [role='rowgroup']:not(:has([role='row'])), 
+            [role='table']:not(:has([role='row'], [role='rowgroup'])), 
+            [role='tablist']:not(:has([role='tab'])), 
+            [role='tree']:not(:has([role='group'], [role='treeitem'])), 
+            [role='treegrid']:not(:has([role='row'], [role='rowgroup']))
         \`))
             .map(element => ({
                 outerHTML: element.outerHTML,
