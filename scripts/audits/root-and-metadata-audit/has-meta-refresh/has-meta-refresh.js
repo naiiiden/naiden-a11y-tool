ebece7859def02105264d1006e4337c1956cdbdb
naiiiden <na4ko359@gmail.com>
2024-11-14 00:20:39 +0200

    has-meta-refresh refactor

--- scripts/audits/root-and-metadata-audit/has-meta-refresh/has-meta-refresh.js
+++ scripts/audits/root-and-metadata-audit/has-meta-refresh/has-meta-refresh.js
@@ -1,12 +1,21 @@
 import { rootAndMetadataErrors } from "../../../errors/root-and-metadata.js";
+import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
+import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 
 export async function hasMetaRefresh(auditResults) {
-    const metaRefresh = await new Promise((resolve) => {
-        chrome.devtools.inspectedWindow.eval(`document.querySelector('meta[http-equiv="refresh"]')`, resolve);
-    })
+    const metaRefresh = await inspectedWindowEval(`
+        const getUniqueSelector = ${getUniqueSelector.toString()};
+        const metaRefresh = document.querySelector('meta[http-equiv="refresh"]');
+        const hasContentAttr = metaRefresh.getAttribute('content');
+
+        return {
+            hasContentAttr,
+            outerHTML: metaRefresh.outerHTML,
+            selector: getUniqueSelector(metaRefresh)
+        }
+    `)
 
     if ((metaRefresh && metaRefresh.content) || (metaRefresh && metaRefresh.content !== "")) {
-        auditResults.push(rootAndMetadataErrors[3]);
+        auditResults.push({... rootAndMetadataErrors[3], element: metaRefresh.outerHTML, selector: metaRefresh.selector });
     }
 }
-
