be1336053bd9b96269f9b476ee5db08ec393c1f0
naiiiden <na4ko359@gmail.com>
2024-11-13 23:16:38 +0200

    refactor has-page-title

--- scripts/audits/root-and-metadata-audit/has-page-title/has-page-title.js
+++ scripts/audits/root-and-metadata-audit/has-page-title/has-page-title.js
@@ -1,12 +1,21 @@
 import { rootAndMetadataErrors } from "../../../errors/root-and-metadata.js";
+import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
+import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 
 export async function hasPageTitle(auditResults) {
-    const pageTitle = await new Promise((resolve) => {
-        chrome.devtools.inspectedWindow.eval("document.title", resolve);
-    });
-    
-    if (!pageTitle || pageTitle === "") {
-        auditResults.push(rootAndMetadataErrors[2]);
+    const documentTitle = await inspectedWindowEval(`
+        const getUniqueSelector = ${getUniqueSelector.toString()};
+        const documentTitle = document.querySelector("title");
+
+        return {
+            documentTitle,
+            outerHTML: documentTitle.outerHTML,
+            selector: getUniqueSelector(documentTitle)
+        }
+    `)
+
+    if (documentTitle.documentTitle) {
+        auditResults.push({ ...rootAndMetadataErrors[2], element: documentTitle.outerHTML, selector: documentTitle.selector });
     }
 }
 
