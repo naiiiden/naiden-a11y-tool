d03e511d37971f747901a7aab12219082550c7e5
naiiiden <na4ko359@gmail.com>
2024-11-13 12:57:57 +0200

    html lang attr check refactor

--- scripts/audits/root-and-metadata-audit/has-html-lang-attr/has-html-lang-attr.js
+++ scripts/audits/root-and-metadata-audit/has-html-lang-attr/has-html-lang-attr.js
@@ -1,12 +1,27 @@
 import { rootAndMetadataErrors } from "../../../errors/root-and-metadata.js";
+import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
+import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 
 export async function hasHtmlLangAttr(auditResults) {
-    const htmlLanguage = await new Promise((resolve) => {
-        chrome.devtools.inspectedWindow.eval("document.documentElement.getAttribute('lang')", resolve);
-    });
+    // const htmlLanguage = await new Promise((resolve) => {
+    //     chrome.devtools.inspectedWindow.eval("document.documentElement.getAttribute('lang')", resolve);
+    // });
+
+    const htmlLanguage = await inspectedWindowEval(`
+        const getUniqueSelector = ${getUniqueSelector.toString()};
+        const rootDocument = document.documentElement;
+        const hasLangAttr = rootDocument.getAttribute('lang');
+
+        return {
+            hasLangAttr,
+            outerHTML: rootDocument.outerHTML,
+            selector: getUniqueSelector(rootDocument)
+        }
+
+    `)
     
-    if (!htmlLanguage) {
-        auditResults.push(rootAndMetadataErrors[0]);
+    if (!htmlLanguage.hasLangAttr) {
+        auditResults.push({ ...rootAndMetadataErrors[0], element: htmlLanguage.outerHTML, selector: htmlLanguage.selector });
     }
 }
 
