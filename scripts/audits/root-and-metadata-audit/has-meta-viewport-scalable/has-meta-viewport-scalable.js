41ece86dfbc81c5b537ce4497d65f4b152ccc2ee
naiiiden <na4ko359@gmail.com>
2024-11-14 00:04:24 +0200

    update name & remove clg

--- scripts/audits/root-and-metadata-audit/has-meta-viewport-scalable/has-meta-viewport-scalable.js
+++ scripts/audits/root-and-metadata-audit/has-meta-viewport-scalable/has-meta-viewport-scalable.js
@@ -2,7 +2,7 @@ import { rootAndMetadataErrors } from "../../../errors/root-and-metadata.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 
-export async function hasMetaViewport(auditResults) {
+export async function hasMetaViewportUserScalableNoOrZero(auditResults) {
     const metaViewport = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const metaViewport = document.querySelector('meta[name=\"viewport\"]');
@@ -14,8 +14,6 @@ export async function hasMetaViewport(auditResults) {
             selector: getUniqueSelector(metaViewport)
         }
     `)
-
-    console.log(1, metaViewport);
 
     if (metaViewport.hasContentAttr && (metaViewport.hasContentAttr.includes('user-scalable=no') || metaViewport.hasContentAttr.includes('user-scalable=0'))) {
         auditResults.push({... rootAndMetadataErrors[4], element: metaViewport.outerHTML, selector: metaViewport.selector });
--- scripts/audits/root-and-metadata-audit/root-and-metadata-audit.js
+++ scripts/audits/root-and-metadata-audit/root-and-metadata-audit.js
@@ -1,6 +1,6 @@
 import { hasHtmlLangAttr} from "./has-html-lang-attr/has-html-lang-attr.js";
 import { hasMetaRefresh } from "./has-meta-refresh/has-meta-refresh.js";
-import { hasMetaViewport } from "./has-meta-viewport/has-meta-viewport.js";
+import { hasMetaViewportUserScalableNoOrZero } from "./has-meta-viewport-scalable/has-meta-viewport-scalable.js";
 import { hasPageTitle } from "./has-page-title/has-page-title.js";
 import { hasValidHtmlLangAttr } from "./has-valid-html-lang-attr/has-valid-html-lang-attr.js";
 
@@ -9,5 +9,5 @@ export async function rootAndMetadataAudit(auditResults) {
     await hasHtmlLangAttr(auditResults);
     await hasPageTitle(auditResults);
     await hasMetaRefresh(auditResults);
-    await hasMetaViewport(auditResults);
+    await hasMetaViewportUserScalableNoOrZero(auditResults);
 }
