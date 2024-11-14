b89141c3d10af7dd60a898d8a818650ff02824f8
naiiiden <na4ko359@gmail.com>
2024-11-14 07:52:48 +0200

    interactive-controls-w-interactive-children update selector

--- scripts/audits/links-and-buttons-audit/interactive-controls-w-interactive-children/interactive-controls-w-interactive-children.js
+++ scripts/audits/links-and-buttons-audit/interactive-controls-w-interactive-children/interactive-controls-w-interactive-children.js
@@ -5,7 +5,7 @@ import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 export async function hasInteractiveControlsWithInteractiveControlsAsChildren(auditResults) {
     const interactiveControlsWithInteractiveControlsAsChildren = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
-        return Array.from(document.querySelectorAll(':is(button, a, [role="button"], [role="link"]):has(a, button, [role="button"], [role="link"], input, [tabindex], textarea, select)'))
+        return Array.from(document.querySelectorAll(':is(button, a, [role="button"], [role="link"], [tabindex]:not([tabindex^="-"])):has(a, button, [role="button"], [role="link"], input, [tabindex]:not([tabindex^="-"]), textarea, select)'))
             .map(element => {
                 return {
                     outerHTML: element.outerHTML,
