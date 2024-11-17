ee5780dfe28aaf017dc85a1dc89567129df05871
naiiiden <na4ko359@gmail.com>
2024-11-17 11:43:03 +0200

    aria-roles-valid-values move array outside eval func

--- scripts/audits/aria-audit/aria-roles-valid-values/aria-roles-valid-values.js
+++ scripts/audits/aria-audit/aria-roles-valid-values/aria-roles-valid-values.js
@@ -3,20 +3,21 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 
 export async function ariaRoleValidValues(auditResults) {
+    const validAriaRoles = new Set([
+        "", "application", "article", "blockquote", "caption", "document", "feed", "group", "heading", "list", "listitem",
+        "note", "paragraph", "separator", "toolbar", "code", "definition", "deletion", "emphasis", "figure", "img",
+        "insertion", "mark", "math", "meter", "strong", "subscript", "superscript", "term", "time", "tooltip", 
+        "banner", "complementary", "contentinfo", "form", "main", "navigation", "region", "search", "alert", "log",
+        "marquee", "status", "timer", "none", "generic", "presentation", "cell", "columnheader", "row", "rowgroup",
+        "rowheader", "table", "button", "checkbox", "gridcell", "link", "menuitem", "menuitemcheckbox", 
+        "menuitemradio", "option", "progressbar", "radio", "scrollbar", "searchbox", "slider", 
+        "spinbutton", "switch", "tab", "tabpanel", "textbox", "treeitem", "combobox", "grid", "listbox", "menu", 
+        "menubar", "radiogroup", "tablist", "tree", "treegrid", "alertdialog", "dialog"
+    ]);
+    
     // https://dequeuniversity.com/rules/axe/4.10/aria-roles
     const ariaRoleValidValues = await inspectedWindowEval(`
-        const validAriaRoles = new Set([
-            "", "application", "article", "blockquote", "caption", "document", "feed", "group", "heading", "list", "listitem",
-            "note", "paragraph", "separator", "toolbar", "code", "definition", "deletion", "emphasis", "figure", "img",
-            "insertion", "mark", "math", "meter", "strong", "subscript", "superscript", "term", "time", "tooltip", 
-            "banner", "complementary", "contentinfo", "form", "main", "navigation", "region", "search", "alert", "log",
-            "marquee", "status", "timer", "none", "generic", "presentation", "cell", "columnheader", "row", "rowgroup",
-            "rowheader", "table", "button", "checkbox", "gridcell", "link", "menuitem", "menuitemcheckbox", 
-            "menuitemradio", "option", "progressbar", "radio", "scrollbar", "searchbox", "slider", 
-            "spinbutton", "switch", "tab", "tabpanel", "textbox", "treeitem", "combobox", "grid", "listbox", "menu", 
-            "menubar", "radiogroup", "tablist", "tree", "treegrid", "alertdialog", "dialog"
-        ]);
-
+        const validAriaRoles = ${JSON.stringify(validAriaRoles)};
         const getUniqueSelector = ${getUniqueSelector.toString()};
         return Array.from(document.querySelectorAll("[role]"))
             .filter(element => !validAriaRoles.has(element.getAttribute("role").trim()))
