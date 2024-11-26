6940abe4683ab31d6aa91fbae886c1aa4e80e318
naiiiden <na4ko359@gmail.com>
2024-11-26 20:06:08 +0200

    aria-conditional-attributes update selector add rest of aria attributes

--- scripts/audits/aria-audit/aria-conditional-attributes/aria-conditional-attributes.js
+++ scripts/audits/aria-audit/aria-conditional-attributes/aria-conditional-attributes.js
@@ -7,7 +7,7 @@ export async function ariaConditionalAttributes(auditResults) {
     const ariaConditionalAttributes = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         return Array.from(document.querySelectorAll(\`
-            :is(tr, [role='row'])[aria-posinset]:not([aria-posinset='']):not([role='treegrid'] :is(tr, [role='row']))
+            :is(tr, [role='row']):is([aria-posinset]:not([aria-posinset='']), [aria-setsize]:not([aria-setsize='']), [aria-expanded]:not([aria-expanded='']), [aria-level]:not([aria-level=''])):not([role='treegrid'] :is(tr, [role='row']))
         \`))
             .map(element => ({
                 outerHTML: element.outerHTML,
