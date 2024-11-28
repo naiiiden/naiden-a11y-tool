d80b2a217ecb112a81a17f98e35623df51392559
naiiiden <na4ko359@gmail.com>
2024-11-29 00:41:36 +0200

    add global aria roles in selector

--- scripts/audits/aria-audit/has-presentation-or-none-role-conflict/has-presentation-or-none-role-conflict.js
+++ scripts/audits/aria-audit/has-presentation-or-none-role-conflict/has-presentation-or-none-role-conflict.js
@@ -6,7 +6,28 @@ export async function hasPresentationOrNoneRoleConflict(auditResults) {
     const hasPresentationOrNoneRoleConflict = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         return Array.from(document.querySelectorAll(\`
-            :is([role='none'], [role='presentation']):is([tabindex]:not([tabindex^='-'], [tabindex='']))
+            :is([role='none'], [role='presentation']):is([tabindex]:not([tabindex^='-'], [tabindex='']), 
+                                                         [aria-atomic]:not([aria-atomic='']),
+                                                         [aria-busy]:not([aria-busy='']),
+                                                         [aria-controls]:not([aria-controls='']),
+                                                         [aria-current]:not([aria-current='']),
+                                                         [aria-describedby]:not([aria-describedby='']),
+                                                         [aria-details]:not([aria-details='']),
+                                                         [aria-disabled]:not([aria-disabled='']),
+                                                         [aria-dropeffect]:not([aria-dropeffect='']),
+                                                         [aria-errormessage]:not([aria-errormessage='']),
+                                                         [aria-flowto]:not([aria-flowto='']),
+                                                         [aria-grabbed]:not([aria-grabbed='']),
+                                                         [aria-haspopup]:not([aria-haspopup='']),
+                                                         [aria-hidden]:not([aria-hidden='']),
+                                                         [aria-invalid]:not([aria-invalid='']),
+                                                         [aria-keyshortcuts]:not([aria-keyshortcuts='']),
+                                                         [aria-label]:not([aria-label='']),
+                                                         [aria-labelledby]:not([aria-labelledby='']),
+                                                         [aria-live]:not([aria-live='']),
+                                                         [aria-owns]:not([aria-owns='']),
+                                                         [aria-relevant]:not([aria-relevant='']),
+                                                         [aria-roledescription]:not([aria-roledescription='']))
         \`))
             .map(element => ({
                 outerHTML: element.outerHTML,
