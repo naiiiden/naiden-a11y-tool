0e3488710450a23786d0f4ccb4479576b6cf6892
naiiiden <na4ko359@gmail.com>
2024-11-30 22:10:16 +0200

    delete old

--- scripts/audits/empty-elements-audit/empty-audit.js
+++ scripts/audits/empty-elements-audit/empty-audit.js
@@ -2,12 +2,10 @@ import { hasEmptyHeadingsWithImages } from "./empty-headings-with-images/empty-h
 import { hasEmptyHeadings } from "./empty-headings/empty-headings.js";
 import { hasEmptyOrMissingSummaries } from "./empty-summaries/empty-summaries.js";
 import { hasEmptyTableHeadings } from "./empty-table-headings/empty-table-headings.js";
-import { hasEmptyIframeTitles } from "./iframe-titles/iframe-titles.js";
 
 export async function emptyAudit(auditResults) {
     await hasEmptyHeadings(auditResults);
     await hasEmptyHeadingsWithImages(auditResults);
     await hasEmptyTableHeadings(auditResults);
-    await hasEmptyIframeTitles(auditResults);
     await hasEmptyOrMissingSummaries(auditResults);
 }
