ec2b3c7248caecdf7737bd3e5ec587a8fc56900e
naiiiden <na4ko359@gmail.com>
2024-12-06 10:13:20 +0200

    object alt text error check

--- scripts/audits/embedded-elements-audit/embedded-elements-audit.js
+++ scripts/audits/embedded-elements-audit/embedded-elements-audit.js
@@ -1,5 +1,7 @@
 import { hasEmptyIframeTitles } from "./iframe-titles/iframe-titles.js";
+import { objectHasAltText } from "./object-has-alt-text/object-has-alt-text.js";
 
 export async function embeddedElementsAudit(auditResults) {
     await hasEmptyIframeTitles(auditResults);
+    await objectHasAltText(auditResults);
 }
---
+++ scripts/audits/embedded-elements-audit/object-has-alt-text/object-has-alt-text.js
@@ -0,0 +1,27 @@
+import { embeddedElementsErrors } from "../../../errors/embedded-elements.js";
+import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
+import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+
+export async function objectHasAltText(auditResults) {
+    const objectHasAltText = await inspectedWindowEval(`
+        const getUniqueSelector = ${getUniqueSelector.toString()};
+        return Array.from(document.querySelectorAll("object"))
+            .filter(element => {
+                const ariaLabel = element.hasAttribute('aria-label') ? element.getAttribute('aria-label').trim() : null;
+                const ariaLabelledby = element.hasAttribute('aria-labelledby') 
+                    ? document.getElementById(element.getAttribute('aria-labelledby')) 
+                    : null;
+                const title = element.hasAttribute('title') ? element.getAttribute('title').trim() : null;
+                
+                return !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim()) || title);
+            })
+            .map(element => ({
+                outerHTML: element.outerHTML,
+                selector: getUniqueSelector(element)
+            }));
+    `) 
+  
+    objectHasAltText.forEach(element => {
+        auditResults.push({ ...embeddedElementsErrors[1], element: element.outerHTML, selector: element.selector });
+    });
+}
