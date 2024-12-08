f4b4d0077d571764a06ff9b73d3accf90a34469d
naiiiden <na4ko359@gmail.com>
2024-12-08 14:11:52 +0200

    role img ignore svg elements

--- scripts/audits/images-audit/role-img/role-img.js
+++ scripts/audits/images-audit/role-img/role-img.js
@@ -5,7 +5,7 @@ import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 export async function hasRoleImg(auditResults) {
     const roleImg = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
-        return Array.from(document.querySelectorAll('[role="img"]'))
+        return Array.from(document.querySelectorAll('[role="img"]:not(svg[role="img"])'))
             .filter(img => {
                 const ariaLabel = img.hasAttribute('aria-label') ? img.getAttribute('aria-label').trim() : null;
                 const ariaLabelledby = img.hasAttribute('aria-labelledby') 
