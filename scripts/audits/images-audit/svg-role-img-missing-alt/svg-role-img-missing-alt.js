031dcceb35cb28931e6500ba4c2463763f0aabb1
naiiiden <na4ko359@gmail.com>
2024-12-08 14:10:32 +0200

    svg role img check for title element

--- scripts/audits/images-audit/svg-role-img-missing-alt/svg-role-img-missing-alt.js
+++ scripts/audits/images-audit/svg-role-img-missing-alt/svg-role-img-missing-alt.js
@@ -2,18 +2,18 @@ import { imageErrors } from "../../../errors/images.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 
-export async function hasMissingAltImages(auditResults) {
-    const missingAltImages = await inspectedWindowEval(`
+export async function hasSvgRoleImgMissingAlt(auditResults) {
+    const hasSvgRoleImgMissingAlt = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         return Array.from(document.querySelectorAll('svg[role="img"]'))
             .filter(img => {
-                const alt = img.getAttribute('alt');
                 const ariaLabel = img.hasAttribute('aria-label') ? img.getAttribute('aria-label').trim() : null;
                 const ariaLabelledby = img.hasAttribute('aria-labelledby') 
                     ? document.getElementById(img.getAttribute('aria-labelledby')) 
                     : null;
+                const title = img.querySelector("title") && img.querySelector("title").textContent.trim();
 
-                return alt === null && !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim())) && (!role || (role !== 'none' && role !== 'presentation'));
+                return !((ariaLabelledby && ariaLabelledby.textContent.trim()) || ariaLabel || title));
             })
             .map(img => ({
                 outerHTML: img.outerHTML,
@@ -21,9 +21,9 @@ export async function hasMissingAltImages(auditResults) {
             }));
     `);
   
-    missingAltImages.forEach(img => {
+    hasSvgRoleImgMissingAlt.forEach(img => {
         auditResults.push({ 
-            ...imageErrors[0],
+            ...imageErrors[7],
             element: img.outerHTML,
             selector: img.selector
         });
