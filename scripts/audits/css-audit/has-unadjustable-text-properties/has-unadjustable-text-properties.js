cdc2f23c3f9f11330e3cd89e93ca97b28bcf2050
naiiiden <na4ko359@gmail.com>
2024-12-10 06:16:46 +0200

    has-unadjustable-text-properties update

--- scripts/audits/css-audit/has-unadjustable-text-properties/has-unadjustable-text-properties.js
+++ scripts/audits/css-audit/has-unadjustable-text-properties/has-unadjustable-text-properties.js
@@ -7,17 +7,36 @@ export async function hasUnadjustableTextProperties(auditResults) {
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const propertiesToCheck = ['line-height', 'letter-spacing', 'word-spacing', 'font-size'];
 
-        return Array.from(document.querySelectorAll('*[style]'))
-            .filter(element => {
-                const style = element.getAttribute('style');
-                return propertiesToCheck.some(property => 
-                    new RegExp(\`\\\\b\${property}\\\\s*:\\\\s*[^;]+!important\`).test(style)
-                );
-            })
+        function hasImportantProperty(element, property) {
+            const importantRegex = new RegExp(\`\\\\b\${property}\\\\s*:\\\\s*[^;]+!important\`, 'i');
+
+            for (let i = 0; i < document.styleSheets.length; i++) {
+                try {
+                    const rules = document.styleSheets[i].cssRules || document.styleSheets[i].rules;
+                    for (let j = 0; j < rules.length; j++) {
+                        if (rules[j].style && importantRegex.test(rules[j].cssText)) {
+                            const selector = rules[j].selectorText;
+                            if (selector && element.matches(selector)) {
+                                return true;
+                            }
+                        }
+                    }
+                } catch (e) {
+                    continue;
+                }
+            }
+
+            return false;
+        }
+
+        return Array.from(document.querySelectorAll('*'))
+            .filter(element => 
+                propertiesToCheck.some(property => hasImportantProperty(element, property))
+            )
             .map(element => ({
                 outerHTML: element.outerHTML,
                 selector: getUniqueSelector(element),
-                inlineStyle: element.getAttribute('style')
+                offendingStyles: propertiesToCheck.filter(property => hasImportantProperty(element, property))
             }));
     `);
 
