e1864c1dc04e20da4a50eae8d75f670798e429e9
naiiiden <na4ko359@gmail.com>
2024-12-30 23:25:59 +0200

    deprecated elements error

--- scripts/errors/deprecated-elements.js
+++ scripts/errors/deprecated-elements.js
@@ -3,7 +3,16 @@ export const deprecatedElementsErrors = [
       id: 0,
       name: "marquee elements are deprecated and must not be used",
       description: "marquee elements are deprecated and must not be used",
-      wcagLinks: "",
+      wcagLinks: [
+        {
+          "name": "Pause, Stop, Hide (Level A)",
+          "url": "https://www.w3.org/WAI/WCAG21/Understanding/pause-stop-hide"
+        },
+        {
+          "name": "Failure of Success Criterion 2.2.2 due to including scrolling content where movement is not essential to the activity without also including a mechanism to pause and restart the content",
+          "url": "https://www.w3.org/WAI/WCAG21/Techniques/failures/F16.html"
+        }
+      ],
       fix: "ensure the marquee element is not used",
     }
 ]
