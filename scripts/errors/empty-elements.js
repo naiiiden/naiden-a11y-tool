e2c4dbab888518c866874d5b663c05b64bd05bb6
naiiiden <na4ko359@gmail.com>
2024-12-30 23:38:24 +0200

    empty elements error 0

--- scripts/errors/empty-elements.js
+++ scripts/errors/empty-elements.js
@@ -3,7 +3,16 @@ export const emptyErrors = [
       id: 0,
       name: "Empty heading",
       description: "A heading is empty.",
-      wcagLinks: "",
+      wcagLinks: [
+        {
+          "name": "Headings and Labels (Level AA)",
+          "url": "https://www.w3.org/WAI/WCAG22/Understanding/headings-and-labels"
+        },
+        {
+          "name": "Providing descriptive headings",
+          "url": "https://www.w3.org/WAI/WCAG21/Techniques/general/G130.html"
+        }
+      ],
       fix: "Add text to the empty heading.",
     },
     {
