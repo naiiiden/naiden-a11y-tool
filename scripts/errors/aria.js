7b5af29abc1a026004b343fed11882c7ed95818c
naiiiden <na4ko359@gmail.com>
2025-01-04 10:22:49 +0200

    aria errors 20

--- scripts/errors/aria.js
+++ scripts/errors/aria.js
@@ -370,7 +370,20 @@ export const ariaErrors = [
         id: 20,
         name: "aria role elements must use permitted aria attributes",
         description: "aria role elements must use permitted aria attributes",
-        wcagLinks: "",
+        wcagLinks: [
+            {
+                "name": "Name, Role, Value (Level A)",
+                "url": "https://www.w3.org/WAI/WCAG22/Understanding/name-role-value.html"
+            },
+            {
+                "name": "Categorization of roles",
+                "url": "https://www.w3.org/TR/wai-aria-1.3/#roles_categorization"
+            },
+            {
+                "name": "Taxonomy of WAI-ARIA states and properties",
+                "url": "https://www.w3.org/TR/wai-aria-1.3/#state_prop_taxonomy"
+            }
+        ],
         fix: "ensure aria attributes are not prohibited for the element's role"
     },
     {
