50342acaf72573e463afe1d47a4037ece2787c34
naiiiden <na4ko359@gmail.com>
2025-01-22 22:58:17 +0200

    update links

--- scripts/errors/deprecated-elements.js
+++ scripts/errors/deprecated-elements.js
@@ -6,11 +6,11 @@ export const deprecatedElementsErrors = [
       wcagLinks: [
         {
           "name": "Pause, Stop, Hide (Level A)",
-          "url": "https://www.w3.org/WAI/WCAG21/Understanding/pause-stop-hide"
+          "url": "https://www.w3.org/WAI/WCAG22/Understanding/pause-stop-hide"
         },
         {
           "name": "Failure of Success Criterion 2.2.2 due to including scrolling content where movement is not essential to the activity without also including a mechanism to pause and restart the content",
-          "url": "https://www.w3.org/WAI/WCAG21/Techniques/failures/F16.html"
+          "url": "https://www.w3.org/WAI/WCAG22/Techniques/failures/F16.html"
         }
       ],
       fix: "Remove the marquee element and replace it with CSS animations or other accessible methods. Ensure the user is allowed to pause or stop the animation.",
