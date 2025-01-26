e06b4a5222cb95c54ced5845e72331cc5df27eb4
naiiiden <na4ko359@gmail.com>
2025-01-26 23:18:23 +0200

    form errors 0

--- scripts/errors/forms.js
+++ scripts/errors/forms.js
@@ -20,6 +20,10 @@ export const formErrors = [
           "name": "Using aria-labelledby to provide a name for user interface controls",
           "url": "https://www.w3.org/WAI/WCAG22/Techniques/aria/ARIA16"
         },
+        {
+          "name": "Name, Role, Value (Level A)",
+          "url": "https://www.w3.org/WAI/WCAG22/Understanding/name-role-value.html"
+        },
       ],
       fix: "Add text to the empty form label",
     },
