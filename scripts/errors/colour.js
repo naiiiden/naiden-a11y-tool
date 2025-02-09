e21eddff684d45f52238ef12e7eaa2d2e6c18ffa
naiiiden <na4ko359@gmail.com>
2025-02-09 22:53:03 +0200

    colour errors

---
+++ scripts/errors/colour.js
@@ -0,0 +1,9 @@
+export const colourErrors = [
+    {
+      id: 0,
+      name: "Elements must meet minimum color contrast ratio thresholds",
+      description: "All text elements must have sufficient contrast between text in the foreground and background colors behind it in accordance with WCAG 2 AA contrast ratio thresholds.",
+      wcagLinks: "",
+      fix: "Ensure that the contrast between the foreground text and the background colors meet the WCAG 2 AA contrast ratio thresholds",
+    }
+]
