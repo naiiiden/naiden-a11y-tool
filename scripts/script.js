b836ba138a53988af67a941bc8a216f3d6cd8919
naiiiden <na4ko359@gmail.com>
2025-02-27 23:17:11 +0200

    move checkboxes logic inside controls

--- scripts/script.js
+++ scripts/script.js
@@ -28,72 +28,8 @@ document.addEventListener("DOMContentLoaded", () => {
     "colour": colourAudit
   };
   
-  const auditCheckboxes = document.querySelectorAll("input[type='checkbox'][id$='checkbox'");
-  const selectAllBtn = document.querySelector("input[type='button'][id='select-all']");
   const runAuditBtn = document.querySelector("#run-audit-btn");
-  const auditFuncsArray = [];
   let auditResults = [];
-
-  auditCheckboxes.forEach(checkbox => {
-    checkbox.checked = true;
-    const selectedAudit = auditFuncsMap[checkbox.value];
-    auditFuncsArray.push(selectedAudit);
-  });
-
-  function updateSelectAllButton() {
-    const allCheckboxesChecked = Array.from(auditCheckboxes).every(checkbox => checkbox.checked);
-    selectAllBtn.value = allCheckboxesChecked ? "deselect all" : "select all";  
-  }
-
-  function updateRunButton() {
-    const someCheckboxesChecked = Array.from(auditCheckboxes).some(checkbox => checkbox.checked);
-    runAuditBtn.disabled = someCheckboxesChecked ? false : true;
-  }
-
-  updateRunButton();
-  updateSelectAllButton();
-  
-  auditCheckboxes.forEach(input => {
-    input.addEventListener("click", (e) => {
-      const selectedAudit = auditFuncsMap[e.target.value];
-
-      if (!auditFuncsArray.includes(selectedAudit)) {
-        auditFuncsArray.push(selectedAudit);
-      } else if (auditFuncsArray.includes(e.target.value)) {
-        auditFuncsArray.splice(auditFuncsArray.indexOf(selectedAudit), 1);
-      }
-
-      updateSelectAllButton();
-      updateRunButton();
-    });
-  });
-  
-  selectAllBtn.addEventListener('click', () => {
-    const allCheckboxesChecked = Array.from(auditCheckboxes).every(checkbox => checkbox.checked);
-
-    if (!allCheckboxesChecked) {
-      auditCheckboxes.forEach(input => {
-        input.checked = true;
-        const selectedAudit = auditFuncsMap[input.value];
-
-        if (!auditFuncsArray.includes(selectedAudit)) {
-          auditFuncsArray.push(selectedAudit);
-        }
-      });
-    } else if (allCheckboxesChecked) {
-      auditCheckboxes.forEach(input => {
-        input.checked = false;
-        const selectedAudit = auditFuncsMap[input.value];
-        
-        if (auditFuncsArray.includes(selectedAudit)) {
-          auditFuncsArray.splice(auditFuncsArray.indexOf(selectedAudit), 1);
-        }
-      })
-    }
-
-    updateSelectAllButton();
-    updateRunButton();
-  });
 
   runAuditBtn.addEventListener("click", async () => {
     await runAudit(auditFuncsArray);
--- scripts/ui/controls.js
+++ scripts/ui/controls.js
@@ -1,10 +1,76 @@
 import { toggleStylesheets } from "../utils/toggle-stylesheets.js";
 
-export function uiControls() {
+export function uiControls(auditFuncsMap) {
+    const auditCheckboxes = document.querySelectorAll("input[type='checkbox'][id$='checkbox'");
+    const selectAllBtn = document.querySelector("input[type='button'][id='select-all']");
+    const runAuditBtn = document.querySelector("#run-audit-btn");
+    const auditFuncsArray = [];
+
     document.getElementById('toggle-stylesheets').addEventListener('change', () => {
         const disable = document.getElementById('toggle-stylesheets').checked;
         chrome.devtools.inspectedWindow.eval(
           `(${toggleStylesheets.toString()})(${disable});`
         );
+    });
+
+    auditCheckboxes.forEach(checkbox => {
+        checkbox.checked = true;
+        const selectedAudit = auditFuncsMap[checkbox.value];
+        auditFuncsArray.push(selectedAudit);
+    });
+    
+    function updateSelectAllButton() {
+        const allCheckboxesChecked = Array.from(auditCheckboxes).every(checkbox => checkbox.checked);
+        selectAllBtn.value = allCheckboxesChecked ? "deselect all" : "select all";  
+    }
+    
+    function updateRunButton() {
+        const someCheckboxesChecked = Array.from(auditCheckboxes).some(checkbox => checkbox.checked);
+        runAuditBtn.disabled = someCheckboxesChecked ? false : true;
+    }
+    
+    updateRunButton();
+    updateSelectAllButton();
+      
+    auditCheckboxes.forEach(input => {
+        input.addEventListener("click", (e) => {
+            const selectedAudit = auditFuncsMap[e.target.value];
+
+            if (!auditFuncsArray.includes(selectedAudit)) {
+                auditFuncsArray.push(selectedAudit);
+            } else if (auditFuncsArray.includes(e.target.value)) {
+                auditFuncsArray.splice(auditFuncsArray.indexOf(selectedAudit), 1);
+            }
+
+            updateSelectAllButton();
+            updateRunButton();
+        });
+    });
+
+    selectAllBtn.addEventListener('click', () => {
+        const allCheckboxesChecked = Array.from(auditCheckboxes).every(checkbox => checkbox.checked);
+    
+        if (!allCheckboxesChecked) {
+            auditCheckboxes.forEach(input => {
+                input.checked = true;
+                const selectedAudit = auditFuncsMap[input.value];
+    
+                if (!auditFuncsArray.includes(selectedAudit)) {
+                auditFuncsArray.push(selectedAudit);
+                }
+          });
+        } else if (allCheckboxesChecked) {
+            auditCheckboxes.forEach(input => {
+                input.checked = false;
+                const selectedAudit = auditFuncsMap[input.value];
+                
+                if (auditFuncsArray.includes(selectedAudit)) {
+                    auditFuncsArray.splice(auditFuncsArray.indexOf(selectedAudit), 1);
+                }
+          })
+        }
+    
+        updateSelectAllButton();
+        updateRunButton();
     });
 }
