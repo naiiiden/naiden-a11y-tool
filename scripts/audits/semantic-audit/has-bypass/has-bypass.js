e50ef3ba97e41b5c43d6bf859f9d8f5b335d2943
naiiiden <na4ko359@gmail.com>
2025-03-03 22:12:15 +0200

    fix hasbypass

--- scripts/audits/semantic-audit/has-bypass/has-bypass.js
+++ scripts/audits/semantic-audit/has-bypass/has-bypass.js
@@ -3,24 +3,17 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 
 export async function hasBypass(auditResults) {
-    const hasBypass = await inspectedWindowEval(`
-        const getUniqueSelector = ${getUniqueSelector.toString()};
+  const hasBypass = await inspectedWindowEval(`
+    const getUniqueSelector = ${getUniqueSelector.toString()};
+    const skipLinks = Array.from(document.querySelectorAll('a[href^="#"]')).filter(link => {
+      const linkText = link.innerText.toLowerCase();
+      return linkText.includes('skip') || linkText.includes('jump');
+    });
 
-        const skipLinks = Array.from(document.querySelectorAll('a[href^="#"]')).filter(link => {
-            const target = document.querySelector(link.getAttribute('href'));
-            return target && target.tagName === 'MAIN';
-        });
-        const mainElements = Array.from(document.querySelectorAll("main, [role='main']"));
-        const headings = Array.from(document.querySelectorAll("h1, h2, h3, h4, h5, h6"));
+    return { skipLinks };
+  `);
 
-        return {
-            skipLinks,
-            mainElements,
-            headings
-        }
-    `)
-
-    if (hasBypass.skipLinks.length === 0 && hasBypass.mainElements.length === 0 && hasBypass.headings.length === 0) {
-        auditResults.push(semanticErrors[18]);
-    }
+  if (hasBypass.skipLinks.length === 0) {
+    auditResults.push(semanticErrors[18]);
+  }
 }
