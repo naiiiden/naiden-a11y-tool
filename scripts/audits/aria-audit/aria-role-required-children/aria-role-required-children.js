04686235bebfa0269855e1ca2323c46078174d29
naiiiden <na4ko359@gmail.com>
2025-03-09 18:39:55 +0200

    revert code

--- scripts/audits/aria-audit/aria-role-required-children/aria-role-required-children.js
+++ scripts/audits/aria-audit/aria-role-required-children/aria-role-required-children.js
@@ -30,7 +30,7 @@ export async function ariaRoleRequiredChildren(auditResults) {
                 if (!requiredChildren) return null;
 
                 const hasRequiredChildren = requiredChildren.requiredChildrenWithRole.some(childRole =>
-                    element.querySelector(\`\${childRole}\`)
+                    element.querySelector(\`[role='\${childRole}']\`)
                 );
 
                 if (!hasRequiredChildren) {
