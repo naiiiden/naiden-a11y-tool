d169309cd872c73575017768dc969eec8336a903
naiiiden <na4ko359@gmail.com>
2025-04-14 17:12:59 +0300

    update getuniqueselector

--- scripts/utils/get-unique-selector.js
+++ scripts/utils/get-unique-selector.js
@@ -1,10 +1,27 @@
 export function getUniqueSelector(element) {
-    let path = '';
-    while (element.parentElement) {
+  const path = [];
+
+  while (
+    element &&
+    element.nodeType === Node.ELEMENT_NODE &&
+    element.tagName.toLowerCase() !== 'html' &&
+    element.tagName.toLowerCase() !== 'body'
+  ) {
+    const tag = element.tagName.toLowerCase();
+
+    const siblings = Array.from(element.parentElement.children).filter(
+      el => el.tagName === element.tagName
+    );
+
+    if (siblings.length === 1) {
+      path.unshift(tag);
+    } else {
       const index = Array.from(element.parentElement.children).indexOf(element) + 1;
-      path = `${element.tagName.toLowerCase()}:nth-child(${index}) > ${path}`;
-      element = element.parentElement;
+      path.unshift(`${tag}:nth-child(${index})`);
     }
-  
-    return path.slice(0, -3); // Remove trailing ' > '
+
+    element = element.parentElement;
+  }
+
+  return path.join(' > ');
 }
