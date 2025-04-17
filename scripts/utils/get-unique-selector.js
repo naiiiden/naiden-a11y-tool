d4e24fba3045a3f89dc7e4a51c1c25f9d3b6ebf6
naiiiden <na4ko359@gmail.com>
2025-04-17 09:39:48 +0300

    getuniqueselector use old code

--- scripts/utils/get-unique-selector.js
+++ scripts/utils/get-unique-selector.js
@@ -1,27 +1,10 @@
 export function getUniqueSelector(element) {
-  const path = [];
-
-  while (
-    element &&
-    element.nodeType === Node.ELEMENT_NODE &&
-    element.tagName.toLowerCase() !== 'html' &&
-    element.tagName.toLowerCase() !== 'body'
-  ) {
-    const tag = element.tagName.toLowerCase();
-
-    const siblings = Array.from(element.parentElement.children).filter(
-      el => el.tagName === element.tagName
-    );
-
-    if (siblings.length === 1) {
-      path.unshift(tag);
-    } else {
-      const index = Array.from(element.parentElement.children).indexOf(element) + 1;
-      path.unshift(`${tag}:nth-child(${index})`);
-    }
-
+  let path = '';
+  while (element.parentElement) {
+    const index = Array.from(element.parentElement.children).indexOf(element) + 1;
+    path = `${element.tagName.toLowerCase()}:nth-child(${index}) > ${path}`;
     element = element.parentElement;
   }
 
-  return path.join(' > ');
+  return path.slice(0, -3); // Remove trailing ' > '
 }
