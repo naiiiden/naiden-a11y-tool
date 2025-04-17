7daa9a788bdf9201739e0462a215e6fad1822b08
naiiiden <na4ko359@gmail.com>
2025-04-17 09:12:01 +0300

    uppercase

--- scripts/errors/colour.js
+++ scripts/errors/colour.js
@@ -10,11 +10,11 @@ export const colourErrors = [
           "url": "https://www.w3.org/WAI/WCAG22/Understanding/contrast-minimum.html"
         }, 
         {
-          "name": "text is less than 18 point if not bold and less than 14 point if bold: Ensuring that a contrast ratio of at least 4.5:1 exists between text (and images of text) and background behind the text",
+          "name": "Text is less than 18 point if not bold and less than 14 point if bold: Ensuring that a contrast ratio of at least 4.5:1 exists between text (and images of text) and background behind the text",
           "url": "https://www.w3.org/WAI/WCAG22/Techniques/general/G18"
         }, 
         {
-          "name": "text is at least 18 point if not bold or at least 14 point if bold: Ensuring that a contrast ratio of at least 3:1 exists between text (and images of text) and background behind the text",
+          "name": "Text is at least 18 point if not bold or at least 14 point if bold: Ensuring that a contrast ratio of at least 3:1 exists between text (and images of text) and background behind the text",
           "url": "https://www.w3.org/WAI/WCAG22/Techniques/general/G145"
         }
       ],
