0fe99cc369c7ead7fa99b4c8e6d074d06f9baac5
naiiiden <na4ko359@gmail.com>
2025-04-23 10:53:53 +0300

    has-bypass fix

--- scripts/audits/semantic-audit/has-bypass/has-bypass.js
+++ scripts/audits/semantic-audit/has-bypass/has-bypass.js
@@ -5,15 +5,20 @@ import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 export async function hasBypass(auditResults) {
   const hasBypass = await inspectedWindowEval(`
     const getUniqueSelector = ${getUniqueSelector.toString()};
-    const skipLinks = Array.from(document.querySelectorAll('a[href^="#"]')).filter(link => {
-      const linkText = link.innerText.toLowerCase();
-      return linkText.includes('skip') || linkText.includes('jump');
-    });
+    const skipLinks = Array.from(document.querySelectorAll('a[href^="#"]'))
+      .filter(link => {
+        const linkText = link.innerText.toLowerCase();
+        return linkText.includes('skip') || linkText.includes('jump');
+      })
+      .map(link => ({
+        selector: getUniqueSelector(link),
+        outerHTML: link.outerHTML
+      }));
 
     return { skipLinks };
   `);
 
-  if (hasBypass.skipLinks.length === 0) {
+  if (!hasBypass || hasBypass.skipLinks.length === 0) {
     auditResults.push(semanticErrors[18]);
   }
 }
