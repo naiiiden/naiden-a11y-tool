14030c136952259306c2d8cfdc8af86d200ba93f
naiiiden <na4ko359@gmail.com>
2025-04-27 11:58:22 +0300

    has-headings use inspectedwindoweval

--- scripts/audits/semantic-audit/has-headings/has-headings.js
+++ scripts/audits/semantic-audit/has-headings/has-headings.js
@@ -1,13 +1,10 @@
 import { semanticErrors } from "../../../errors/semantic.js";
-import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 
 export async function hasHeadings(auditResults) {
-    const hasHeadings = await new Promise((resolve) => {
-        chrome.devtools.inspectedWindow.eval(`
-            Array.from(document.querySelectorAll('h1, h2, h3, h4, h5, h6, [role="heading"]')).length > 0
-        `, resolve);
-    });
+    const hasHeadings = await inspectedWindowEval(`
+        return Array.from(document.querySelectorAll('h1, h2, h3, h4, h5, h6, [role="heading"]')).length > 0
+    `)
     
     if (!hasHeadings) {
         auditResults.push(semanticErrors[3]);
