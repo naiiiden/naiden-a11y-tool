f58a0e60f4b11e5eb1715985ec6a9845785c7fa8
naiiiden <na4ko359@gmail.com>
2025-04-27 12:02:14 +0300

    more than 1 contentinfo use inspectedwindoweval

--- scripts/audits/semantic-audit/has-more-than-one-contentinfo-landmark/has-more-than-one-contentinfo-landmark.js
+++ scripts/audits/semantic-audit/has-more-than-one-contentinfo-landmark/has-more-than-one-contentinfo-landmark.js
@@ -1,13 +1,10 @@
 import { semanticErrors } from "../../../errors/semantic.js";
-import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 
 export async function hasMoreThanOneContentinfoLandmark(auditResults) {
-    const moreThanOneContentinfoLandmark = await new Promise((resolve) => {
-        chrome.devtools.inspectedWindow.eval(`
-            document.querySelectorAll("[role='contentinfo']").length
-        `, resolve);
-    });
+    const moreThanOneContentinfoLandmark = await inspectedWindowEval(`
+        return document.querySelectorAll("[role='contentinfo']").length
+    `)
 
     if (moreThanOneContentinfoLandmark > 1) {
         auditResults.push(semanticErrors[8]);
