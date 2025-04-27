5b545f7871232bd6e4f1d3e6b5b62394ec853db3
naiiiden <na4ko359@gmail.com>
2025-04-27 12:00:44 +0300

    more than 1 banner landmark use inspectedwindoweval

--- scripts/audits/semantic-audit/has-more-than-one-banner-landmark/has-more-thank-one-banner-landmark.js
+++ scripts/audits/semantic-audit/has-more-than-one-banner-landmark/has-more-thank-one-banner-landmark.js
@@ -1,13 +1,10 @@
 import { semanticErrors } from "../../../errors/semantic.js";
-import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 
 export async function hasMoreThanOneBannerLandmark(auditResults) {
-    const moreThanOneBannerLandmark = await new Promise((resolve) => {
-        chrome.devtools.inspectedWindow.eval(`
-            document.querySelectorAll("[role='banner']").length
-        `, resolve);
-    });
+    const moreThanOneBannerLandmark = await inspectedWindowEval(`
+        return document.querySelectorAll("[role='banner']").length
+    `)
 
     if (moreThanOneBannerLandmark > 1) {
         auditResults.push(semanticErrors[7]);
