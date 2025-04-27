e5db5833d5850435265e869d07869e0a129809fb
naiiiden <na4ko359@gmail.com>
2025-04-27 11:56:52 +0300

    has-heading-level-one use inspectedwindoweval

--- scripts/audits/semantic-audit/has-heading-level-one/has-heading-level-one.js
+++ scripts/audits/semantic-audit/has-heading-level-one/has-heading-level-one.js
@@ -1,14 +1,10 @@
 import { semanticErrors } from "../../../errors/semantic.js";
-import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 
 export async function hasHeadingLevelOne(auditResults) {
-    const hasH1 = await new Promise((resolve) => {
-        chrome.devtools.inspectedWindow.eval(
-        `!!document.querySelector('h1') || !!document.querySelector('[role="heading"][aria-level="1"]')`,
-        resolve
-        );
-    });
+    const hasH1 = await inspectedWindowEval(`
+        return !!document.querySelector('h1') || !!document.querySelector('[role="heading"][aria-level="1"]')
+    `);
 
     if (!hasH1) {
         auditResults.push(semanticErrors[0]);
