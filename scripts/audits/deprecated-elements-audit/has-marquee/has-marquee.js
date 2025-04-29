dc2e1e8965faff4ca986417f9c567feb7c2dc2d5
naiiiden <na4ko359@gmail.com>
2025-04-29 23:29:13 +0300

    has-marquee filter

--- scripts/audits/deprecated-elements-audit/has-marquee/has-marquee.js
+++ scripts/audits/deprecated-elements-audit/has-marquee/has-marquee.js
@@ -1,11 +1,15 @@
 import { deprecatedElementsErrors } from "../../../errors/deprecated-elements.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasMarquee(auditResults) {
     const hasMarquee = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+
         return Array.from(document.querySelectorAll('marquee'))
+            .filter(element => isElementVisible(element))
             .map(element => ({
                 outerHTML: element.outerHTML,
                 selector: getUniqueSelector(element),
