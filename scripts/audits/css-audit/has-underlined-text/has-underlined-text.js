5a0db089e2a7b681b019b78282c487838c82d1fd
naiiiden <na4ko359@gmail.com>
2025-04-29 23:21:20 +0300

    has-underlined-text filter

--- scripts/audits/css-audit/has-underlined-text/has-underlined-text.js
+++ scripts/audits/css-audit/has-underlined-text/has-underlined-text.js
@@ -1,13 +1,19 @@
 import { cssErrors } from "../../../errors/css.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasUnderlinedText(auditResults) {
     const hasUnderlinedText = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
 
         return Array.from(document.querySelectorAll('*'))
             .filter(element => {
+                if (!isElementVisible(element)) {
+                    return false;
+                }
+
                 const isUnderlined = window.getComputedStyle(element).textDecorationLine === 'underline';
                 const isExplicitUElement = element.tagName.toLowerCase() === 'u';
                 const isNotLink = element.tagName.toLowerCase() !== 'a';
