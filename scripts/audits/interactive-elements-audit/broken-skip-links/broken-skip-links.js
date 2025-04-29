030740980544072f39b32c3ec12e127d04bd9c61
naiiiden <na4ko359@gmail.com>
2025-04-29 21:55:06 +0300

    broken-skip-links filter

--- scripts/audits/interactive-elements-audit/broken-skip-links/broken-skip-links.js
+++ scripts/audits/interactive-elements-audit/broken-skip-links/broken-skip-links.js
@@ -1,28 +1,33 @@
 import { interactiveElementsErrors } from "../../../errors/interactive-elements.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasBrokenSkipLinks(auditResults) {
     const brokenSkipLinks = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
-        return Array.from(document.querySelectorAll('a[href^="#"]')).map(link => {
-            const linkText = link.textContent.toLowerCase();
-            const targetId = link.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-            const isHidden = window.getComputedStyle(link).display === "none" || window.getComputedStyle(link).visibility === "hidden";
-          
-            return {
-                linkText,
-                targetExists: !!targetElement,
-                isHidden,
-                linkOuterHTML: link.outerHTML,
-                linkSelector: getUniqueSelector(link)
-            };
+        const isElementVisible = ${isElementVisible.toString()};
+
+        return Array.from(document.querySelectorAll('a[href^="#"]'))
+            .filter(link => 
+                isElementVisible(link)
+            )
+            .map(link => {
+                const linkText = link.textContent.toLowerCase();
+                const targetId = link.getAttribute('href').substring(1);
+                const targetElement = document.getElementById(targetId);
+            
+                return {
+                    linkText,
+                    targetExists: !!targetElement,
+                    linkOuterHTML: link.outerHTML,
+                    linkSelector: getUniqueSelector(link)
+                };
         });
     `) 
       
     brokenSkipLinks
-    .filter(link => (link.linkText.includes('skip') || link.linkText.includes('jump')) && (!link.targetExists || link.isHidden))
+    .filter(link => (link.linkText.includes('skip') || link.linkText.includes('jump')) && !link.targetExists)
     .forEach(link => {
         auditResults.push({
         ...interactiveElementsErrors[2],
