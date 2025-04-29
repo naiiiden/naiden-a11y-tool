d03f34ed36d89fd1ec30c07743bab408ee5e6211
naiiiden <na4ko359@gmail.com>
2025-04-29 23:31:49 +0300

    empty-headings filter

--- scripts/audits/empty-elements-audit/empty-headings/empty-headings.js
+++ scripts/audits/empty-elements-audit/empty-headings/empty-headings.js
@@ -1,12 +1,19 @@
 import { emptyElementsErrors } from "../../../errors/empty-elements.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasEmptyHeadings(auditResults) {
     const emptyHeadings = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+
         return Array.from(document.querySelectorAll(":is(h1, h2, h3, h4, h5, h6, [role='heading']):not(:has(img))"))
             .filter(heading => {
+                if (!isElementVisible(heading)) {
+                    return false;
+                }
+
                 const ariaLabel = heading.hasAttribute('aria-label') ? heading.getAttribute('aria-label').trim() : null;
                 const ariaLabelledby = heading.hasAttribute('aria-labelledby') 
                     ? document.getElementById(heading.getAttribute('aria-labelledby')) 
