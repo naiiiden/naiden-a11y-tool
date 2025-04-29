0a1bf166d7db1210321c2e29b11cfe7f14b9e010
naiiiden <na4ko359@gmail.com>
2025-04-29 23:44:17 +0300

    empty-table-headings filter

--- scripts/audits/empty-elements-audit/empty-table-headings/empty-table-headings.js
+++ scripts/audits/empty-elements-audit/empty-table-headings/empty-table-headings.js
@@ -1,12 +1,18 @@
 import { emptyElementsErrors } from "../../../errors/empty-elements.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasEmptyTableHeadings(auditResults) {
     const emptyTableHeadings = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+
         return Array.from(document.querySelectorAll("th, [role='rowheader'], [role='columnheader']"))
             .filter(tableHeading => {
+                if (!isElementVisible(tableHeading)) {
+                    return false;
+                }
                 return tableHeading.textContent.trim() === "";
             })
             .map(tableHeading => ({
