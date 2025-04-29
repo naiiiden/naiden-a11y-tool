4f61cb8b2d7aa1c2b7ccea3d277814988c70bc06
naiiiden <na4ko359@gmail.com>
2025-04-29 23:50:30 +0300

    empty-labels filter

--- scripts/audits/form-audit/empty-labels/empty-labels.js
+++ scripts/audits/form-audit/empty-labels/empty-labels.js
@@ -1,12 +1,19 @@
 import { formErrors } from "../../../errors/forms.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasEmptyLabels(auditResults) {
     const emptyLabels = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+
         return Array.from(document.querySelectorAll("label"))
             .filter(label => {
+                if (!isElementVisible(label)) {
+                    return false;
+                }
+
                 return label.textContent.trim() === "";
             })
             .map(label => ({ 
