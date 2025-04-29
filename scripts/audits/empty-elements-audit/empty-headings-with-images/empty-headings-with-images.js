0c4629af950c3e13997f9597450855740fbdca19
naiiiden <na4ko359@gmail.com>
2025-04-29 23:35:01 +0300

    empty-headings-with-images filter

--- scripts/audits/empty-elements-audit/empty-headings-with-images/empty-headings-with-images.js
+++ scripts/audits/empty-elements-audit/empty-headings-with-images/empty-headings-with-images.js
@@ -1,11 +1,17 @@
 import { emptyElementsErrors } from "../../../errors/empty-elements.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasEmptyHeadingsWithImages(auditResults) {
     const emptyHeadingsWithImages = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+        
         return Array.from(document.querySelectorAll(':is(h1, h2, h3, h4, h5, h6):has(img)'))
+            .filter(heading => 
+                isElementVisible(heading)
+            )
             .map(heading => {
                 const textContent = heading.textContent.trim();
                 const img = heading.querySelector('img');
