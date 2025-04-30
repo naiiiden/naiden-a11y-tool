05b7f90ded3fe6d98582c153eae60812acecbe34
naiiiden <na4ko359@gmail.com>
2025-04-30 18:15:10 +0300

    possible-headings filter

--- scripts/audits/semantic-audit/possible-headings/possible-headings.js
+++ scripts/audits/semantic-audit/possible-headings/possible-headings.js
@@ -1,11 +1,15 @@
 import { semanticErrors } from "../../../errors/semantic.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasPossibleHeadings(auditResults) {
     const possibleHeadings = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+
         return Array.from(document.querySelectorAll('p'))
+            .filter(p => isElementVisible(p))
             .filter(p => p.textContent.trim().length < 50)
                 .filter(p => {
                     const style = window.getComputedStyle(p);
