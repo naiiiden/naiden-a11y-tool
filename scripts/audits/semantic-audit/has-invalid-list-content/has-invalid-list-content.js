49226eca9e92738f22c60d227d9e0fc1f1f9a5a9
naiiiden <na4ko359@gmail.com>
2025-04-30 17:50:29 +0300

    has-invalid-list-content filter

--- scripts/audits/semantic-audit/has-invalid-list-content/has-invalid-list-content.js
+++ scripts/audits/semantic-audit/has-invalid-list-content/has-invalid-list-content.js
@@ -1,11 +1,15 @@
 import { semanticErrors } from "../../../errors/semantic.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasInvalidListContent(auditResults) {
     const invalidListContent = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+
         return Array.from(document.querySelectorAll(':is(ul, ol) > :not(li):not(script):not(template)'))
+        .filter(element => isElementVisible(element))
         .map(element => ({
             outerHTML: element.outerHTML,
             selector: getUniqueSelector(element)
