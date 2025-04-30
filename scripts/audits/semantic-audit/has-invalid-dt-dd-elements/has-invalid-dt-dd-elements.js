801edc82ab21c170637af655a06f7a5f93d5bf06
naiiiden <na4ko359@gmail.com>
2025-04-30 17:48:54 +0300

    has-invalid-dt-dd-elements filter

--- scripts/audits/semantic-audit/has-invalid-dt-dd-elements/has-invalid-dt-dd-elements.js
+++ scripts/audits/semantic-audit/has-invalid-dt-dd-elements/has-invalid-dt-dd-elements.js
@@ -1,11 +1,15 @@
 import { semanticErrors } from "../../../errors/semantic.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasInvalidDtDdElements(auditResults) {
     const invalidDtDdElements = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+
         return Array.from(document.querySelectorAll('dt:not(dl dt), dd:not(dl dd)'))
+            .filter(element => isElementVisible(element))
             .map(element => ({
                 outerHTML: element.outerHTML,
                 selector: getUniqueSelector(element)
