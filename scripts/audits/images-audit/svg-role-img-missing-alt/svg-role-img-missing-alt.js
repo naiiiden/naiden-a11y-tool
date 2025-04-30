b6ed95cf13b7fa1aae5ac60f2cc53cadde6ed398
naiiiden <na4ko359@gmail.com>
2025-04-30 10:26:18 +0300

    svg-role-img-missing-alt filter

--- scripts/audits/images-audit/svg-role-img-missing-alt/svg-role-img-missing-alt.js
+++ scripts/audits/images-audit/svg-role-img-missing-alt/svg-role-img-missing-alt.js
@@ -1,12 +1,19 @@
 import { imageErrors } from "../../../errors/images.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasSvgRoleImgMissingAlt(auditResults) {
     const hasSvgRoleImgMissingAlt = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+        
         return Array.from(document.querySelectorAll('svg[role="img"]'))
             .filter(img => {
+                if (!isElementVisible(img)) {
+                    return false;
+                }
+
                 const ariaLabel = img.hasAttribute('aria-label') ? img.getAttribute('aria-label').trim() : null;
 
                 const ariaLabelledby = img.hasAttribute('aria-labelledby')
