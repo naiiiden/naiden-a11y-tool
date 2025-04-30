f55a19c649aee387697067cbc7ef8df4f916b65e
naiiiden <na4ko359@gmail.com>
2025-04-30 11:07:04 +0300

    has-video-captions filter

--- scripts/audits/embedded-elements-audit/has-video-captions/has-video-captions.js
+++ scripts/audits/embedded-elements-audit/has-video-captions/has-video-captions.js
@@ -1,11 +1,15 @@
 import { embeddedElementsErrors } from "../../../errors/embedded-elements.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasVideoCaptions(auditResults) {
     const hasVideoCaptions = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+
         return Array.from(document.querySelectorAll("video:not(:has(track[kind='captions']))"))
+            .filter(element => isElementVisible(element))
             .map(element => ({
                 outerHTML: element.outerHTML,
                 selector: getUniqueSelector(element)
