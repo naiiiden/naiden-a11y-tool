33d064ad4aad55dcf0f50e777beaaf47093810c7
naiiiden <na4ko359@gmail.com>
2025-04-30 10:24:27 +0300

    server-side-img-maps filter

--- scripts/audits/images-audit/server-side-img-maps/server-side-img-maps.js
+++ scripts/audits/images-audit/server-side-img-maps/server-side-img-maps.js
@@ -1,14 +1,18 @@
 import { imageErrors } from "../../../errors/images.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasServerSideImgMaps(auditResults) {
     const hasServerSideImgMaps = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+        
         return Array.from(document.querySelectorAll('a[href$=".map"]:has(img[ismap])'))
-            .map(button => ({
-                outerHTML: button.outerHTML,
-                selector: getUniqueSelector(button)
+            .filter(img => isElementVisible(img))
+            .map(img => ({
+                outerHTML: img.outerHTML,
+                selector: getUniqueSelector(img)
             }));
     `);
     
