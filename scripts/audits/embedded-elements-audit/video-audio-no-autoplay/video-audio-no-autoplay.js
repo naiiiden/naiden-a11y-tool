620d62a3a1c63aef9350a66cc29e5f763469d7f7
naiiiden <na4ko359@gmail.com>
2025-04-30 11:12:23 +0300

    video-audio-no-autoplay filter

--- scripts/audits/embedded-elements-audit/video-audio-no-autoplay/video-audio-no-autoplay.js
+++ scripts/audits/embedded-elements-audit/video-audio-no-autoplay/video-audio-no-autoplay.js
@@ -1,11 +1,15 @@
 import { embeddedElementsErrors } from "../../../errors/embedded-elements.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function videOrAudioHasAutoplay(auditResults) {
     const videOrAudioHasAutoplay = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+
         return Array.from(document.querySelectorAll(":is(audio, video)[autoplay]:not([controls])"))
+            .filter(element => isElementVisible(element))
             .filter(element => {
                 return (element.duration || 0) > 3;    
             })
