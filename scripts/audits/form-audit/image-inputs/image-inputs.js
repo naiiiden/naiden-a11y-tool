b462c988d5b3f4a28d1d93626bec12a71c3913b7
naiiiden <na4ko359@gmail.com>
2025-04-30 09:51:30 +0300

    image-inputs filter

--- scripts/audits/form-audit/image-inputs/image-inputs.js
+++ scripts/audits/form-audit/image-inputs/image-inputs.js
@@ -1,12 +1,19 @@
 import { formErrors } from "../../../errors/forms.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasImageInputs(auditResults) {
     const imageInputs = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+
         return Array.from(document.querySelectorAll('input[type="image"]'))
             .filter(input => {
+                if (!isElementVisible(input)) {
+                    return false;
+                }
+
                 const alt = input.getAttribute('alt');
                 const ariaLabel = input.hasAttribute('aria-label') ? input.getAttribute('aria-label').trim() : null;
                 const ariaLabelledby = input.hasAttribute('aria-labelledby') 
