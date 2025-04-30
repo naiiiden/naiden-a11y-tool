9187809ed2f47b985f6ef40b3617d09e55a61c2e
naiiiden <na4ko359@gmail.com>
2025-04-30 17:51:42 +0300

    has-li-outside-list filter

--- scripts/audits/semantic-audit/has-li-outside-list/has-li-outside-list.js
+++ scripts/audits/semantic-audit/has-li-outside-list/has-li-outside-list.js
@@ -1,11 +1,15 @@
 import { semanticErrors } from "../../../errors/semantic.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasLiOutsideList(auditResults) {
     const liOutsideList = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+        
         return Array.from(document.querySelectorAll('li:not(ul li, ol li)'))
+        .filter(li => isElementVisible(li))
         .map(li => ({
             outerHTML: li.outerHTML,
             selector: getUniqueSelector(li)
