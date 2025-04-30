e1d2c39111deea0ade3448591a2a8ede9751b9c9
naiiiden <na4ko359@gmail.com>
2025-04-30 10:15:44 +0300

    linked-images filter

--- scripts/audits/images-audit/linked-images/linked-images.js
+++ scripts/audits/images-audit/linked-images/linked-images.js
@@ -1,11 +1,15 @@
 import { imageErrors } from "../../../errors/images.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasLinkedImages(auditResults) {
     const linkedImages = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+
         return Array.from(document.querySelectorAll('a img'))
+            .filter(img => isElementVisible(img))
             .map((img) => {
                 const parentText = img.closest('a').textContent.trim();
                 return { 
