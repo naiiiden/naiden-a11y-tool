df648023cee4945d369a1063fb8fc7b09d638a18
naiiiden <na4ko359@gmail.com>
2025-04-30 16:39:55 +0300

    has-aria-deprecated-roles filter

--- scripts/audits/aria-audit/has-aria-deprecated-roles/has-aria-deprecated-roles.js
+++ scripts/audits/aria-audit/has-aria-deprecated-roles/has-aria-deprecated-roles.js
@@ -1,16 +1,20 @@
 import { ariaErrors } from "../../../errors/aria.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasAriaDeprecatedRoles(auditResults) {
     // https://dequeuniversity.com/rules/axe/4.10/aria-deprecated-role
     const ariaDeprecatedRoles = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+
         return Array.from(document.querySelectorAll("[role='directory']"))
-        .map(element => ({
-            outerHTML: element.outerHTML,
-            selector: getUniqueSelector(element)
-        }));
+            .filter(element => isElementVisible(element))
+            .map(element => ({
+                outerHTML: element.outerHTML,
+                selector: getUniqueSelector(element)
+            }));
     `)
 
     ariaDeprecatedRoles.forEach(element => {
