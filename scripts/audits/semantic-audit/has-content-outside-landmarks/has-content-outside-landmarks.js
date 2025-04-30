877f635fb6b15548699902fb2bfed00068eeea88
naiiiden <na4ko359@gmail.com>
2025-04-30 17:43:27 +0300

    has-content-outside-landmarks filter

--- scripts/audits/semantic-audit/has-content-outside-landmarks/has-content-outside-landmarks.js
+++ scripts/audits/semantic-audit/has-content-outside-landmarks/has-content-outside-landmarks.js
@@ -1,12 +1,19 @@
 import { semanticErrors } from "../../../errors/semantic.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasContentOutsideLandmarks(auditResults) {
     const contentOutsideLandmarks = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+
         return Array.from(document.querySelectorAll('body > *:not(:is(header, nav, main, footer, section, aside, form, [role="banner"], [role="navigation"], [role="main"], [role="contentinfo"], [role="complementary"], [role="search"], :is([role="region"], [role="form"]):is([aria-labelledby]:not([aria-labelledby=""]), [aria-label]:not([aria-label=""]), [title]:not([title=""])), style, script))'))
             .filter(el => {
+                if (!isElementVisible(el)) {
+                    return false;
+                }
+
                 if (el.matches('a[href^="#"]')) {
                     const text = el.textContent.toLowerCase();
                     return !(text.includes('skip') || text.includes('jump'));
