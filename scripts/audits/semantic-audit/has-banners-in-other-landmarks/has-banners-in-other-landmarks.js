47a92488af5fd01b1e4bd2d738965a30ce322f48
naiiiden <na4ko359@gmail.com>
2025-04-30 17:36:34 +0300

    has-banners-in-other-landmarks filter

--- scripts/audits/semantic-audit/has-banners-in-other-landmarks/has-banners-in-other-landmarks.js
+++ scripts/audits/semantic-audit/has-banners-in-other-landmarks/has-banners-in-other-landmarks.js
@@ -1,10 +1,13 @@
 import { semanticErrors } from "../../../errors/semantic.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasBannersInOtherLandmarks(auditResults) {
     const bannersInOtherLandmarks = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+
         return Array.from(document.querySelectorAll(\`
           :is(header, nav, main, section, form, aside, footer, [role="banner"], [role="navigation"], [role="main"], 
                 :is([role="region"], [role="form"]):is(
@@ -13,6 +16,7 @@ export async function hasBannersInOtherLandmarks(auditResults) {
                 [role="complementary"], [role="contentinfo"], [role="search"]) 
           :is(header:not([role]):not(:is(article, aside, main, nav, section) header:not([role])), [role="banner"])
         \`))
+        .filter(header => isElementVisible(header))
         .map(header => ({
             outerHTML: header.outerHTML,
             selector: getUniqueSelector(header)
