3c89e6b26ef407032af6931b91e53f978050865f
naiiiden <na4ko359@gmail.com>
2025-04-30 11:00:56 +0300

    iframe-titles filter

--- scripts/audits/embedded-elements-audit/iframe-titles/iframe-titles.js
+++ scripts/audits/embedded-elements-audit/iframe-titles/iframe-titles.js
@@ -1,11 +1,15 @@
 import { embeddedElementsErrors } from "../../../errors/embedded-elements.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasEmptyIframeTitles(auditResults) {
     const iframeTitles = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+
         return Array.from(document.querySelectorAll("iframe"))
+            .filter(iframe => isElementVisible(iframe))
             .filter(iframe => !iframe.hasAttribute("title") || iframe.getAttribute("title").trim() === "")
             .map(iframe => 
                 ({ outerHTML: iframe.outerHTML, selector: getUniqueSelector(iframe) 
