3fcbaffad0888ffad9dde5d6eb79f7c4c751a3f6
naiiiden <na4ko359@gmail.com>
2025-04-30 15:53:58 +0300

    aria-conditional-attributes filter

--- scripts/audits/aria-audit/aria-conditional-attributes/aria-conditional-attributes.js
+++ scripts/audits/aria-audit/aria-conditional-attributes/aria-conditional-attributes.js
@@ -1,15 +1,19 @@
 import { ariaErrors } from "../../../errors/aria.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function ariaConditionalAttributes(auditResults) {
     // https://dequeuniversity.com/rules/axe/4.10/aria-conditional-attr
     const ariaConditionalAttributes = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+
         return Array.from(document.querySelectorAll(\`
             :is(tr, [role='row']):is([aria-posinset]:not([aria-posinset='']), [aria-setsize]:not([aria-setsize='']), [aria-expanded]:not([aria-expanded='']), [aria-level]:not([aria-level=''])):not([role='treegrid'] :is(tr, [role='row'])), 
             input[type='checkbox'][aria-checked]:not([aria-checked=''])
         \`))
+            .filter(element => isElementVisible(element))
             .map(element => ({
                 outerHTML: element.outerHTML,
                 selector: getUniqueSelector(element)
