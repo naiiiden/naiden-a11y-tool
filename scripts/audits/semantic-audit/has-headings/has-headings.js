e49994609076ad75c9a9519044de54670339f22d
naiiiden <na4ko359@gmail.com>
2025-04-30 17:46:44 +0300

    has-headings filter

--- scripts/audits/semantic-audit/has-headings/has-headings.js
+++ scripts/audits/semantic-audit/has-headings/has-headings.js
@@ -1,9 +1,13 @@
 import { semanticErrors } from "../../../errors/semantic.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasHeadings(auditResults) {
     const hasHeadings = await inspectedWindowEval(`
-        return Array.from(document.querySelectorAll('h1, h2, h3, h4, h5, h6, [role="heading"]')).length > 0
+        const isElementVisible = ${isElementVisible.toString()};
+
+        return Array.from(document.querySelectorAll('h1, h2, h3, h4, h5, h6, [role="heading"]'))
+            .filter(heading => isElementVisible(heading)).length > 0
     `)
     
     if (!hasHeadings) {
