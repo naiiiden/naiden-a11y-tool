1d1647585fa46b5fcc277b718790c8b47c522293
naiiiden <na4ko359@gmail.com>
2025-05-01 09:35:22 +0300

    has-regions-or-landmarks fix filter

--- scripts/audits/semantic-audit/has-regions-or-landmarks/has-regions-or-landmarks.js
+++ scripts/audits/semantic-audit/has-regions-or-landmarks/has-regions-or-landmarks.js
@@ -1,9 +1,13 @@
 import { semanticErrors } from "../../../errors/semantic.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasRegionsOrLandmarks(auditResults) {
     const hasRegionsOrLandmarks = await inspectedWindowEval(`
-        return document.querySelectorAll('header, nav, main, footer, aside, form, [role="banner"], [role="navigation"], [role="main"], [role="contentinfo"], [role="complementary"], [role="search"], :is([role="region"], [role="form"]):is([aria-labelledby]:not([aria-labelledby=""]), [aria-label]:not([aria-label=""]), [title]:not([title=""]))').length
+        const isElementVisible = ${isElementVisible.toString()};
+
+        return Array.from(document.querySelectorAll('header, nav, main, footer, aside, form, [role="banner"], [role="navigation"], [role="main"], [role="contentinfo"], [role="complementary"], [role="search"], :is([role="region"], [role="form"]):is([aria-labelledby]:not([aria-labelledby=""]), [aria-label]:not([aria-label=""]), [title]:not([title=""]))'))
+            .filter(regionOrLandmark => isElementVisible(regionOrLandmark)).length
     `);
 
     if (hasRegionsOrLandmarks < 1) {
