81a15d4cb142d714a48f3d00057c3817b4a8bd76
naiiiden <na4ko359@gmail.com>
2025-05-01 09:48:02 +0300

    has-more-than-one-contentinfo-landmark fix filter

--- scripts/audits/semantic-audit/has-more-than-one-contentinfo-landmark/has-more-than-one-contentinfo-landmark.js
+++ scripts/audits/semantic-audit/has-more-than-one-contentinfo-landmark/has-more-than-one-contentinfo-landmark.js
@@ -1,9 +1,13 @@
 import { semanticErrors } from "../../../errors/semantic.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasMoreThanOneContentinfoLandmark(auditResults) {
     const moreThanOneContentinfoLandmark = await inspectedWindowEval(`
-        return document.querySelectorAll("[role='contentinfo']").length
+        const isElementVisible = ${isElementVisible.toString()};
+
+        return Array.from(document.querySelectorAll("[role='contentinfo']"))
+            .filter(contentinfo => isElementVisible(contentinfo)).length
     `)
 
     if (moreThanOneContentinfoLandmark > 1) {
