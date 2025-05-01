fa37f029a5eed585273ce890729f9b803649f4c5
naiiiden <na4ko359@gmail.com>
2025-05-01 09:38:43 +0300

    has-more-than-one-banner-landmark fix filter

--- scripts/audits/semantic-audit/has-more-than-one-banner-landmark/has-more-thank-one-banner-landmark.js
+++ scripts/audits/semantic-audit/has-more-than-one-banner-landmark/has-more-thank-one-banner-landmark.js
@@ -1,9 +1,13 @@
 import { semanticErrors } from "../../../errors/semantic.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasMoreThanOneBannerLandmark(auditResults) {
     const moreThanOneBannerLandmark = await inspectedWindowEval(`
-        return document.querySelectorAll("[role='banner']").length
+        const isElementVisible = ${isElementVisible.toString()};
+        
+        return Array.from(document.querySelectorAll("[role='banner']"))
+            .filter(banner => isElementVisible(banner)).length
     `)
 
     if (moreThanOneBannerLandmark > 1) {
