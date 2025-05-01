be06727f4b6f7ca9a249865e234b1aa61a8a6649
naiiiden <na4ko359@gmail.com>
2025-05-01 10:12:35 +0300

    has-heading-level-one fix filter

--- scripts/audits/semantic-audit/has-heading-level-one/has-heading-level-one.js
+++ scripts/audits/semantic-audit/has-heading-level-one/has-heading-level-one.js
@@ -1,9 +1,14 @@
 import { semanticErrors } from "../../../errors/semantic.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export async function hasHeadingLevelOne(auditResults) {
     const hasH1 = await inspectedWindowEval(`
-        return !!document.querySelector('h1') || !!document.querySelector('[role="heading"][aria-level="1"]')
+        const isElementVisible = ${isElementVisible.toString()};
+        const h1 = document.querySelector("h1");
+        const ariaH1 = document.querySelector("[role='heading'][aria-level='1']");
+
+        return (h1 && isElementVisible(h1)) || (ariaH1 && isElementVisible(ariaH1));
     `);
 
     if (!hasH1) {
