aa633cf12f9e62e6bbbddfe08a60a0fd18b21985
naiiiden <na4ko359@gmail.com>
2025-05-06 23:03:36 +0300

    has-valid-html-lang-attr extract logic

--- scripts/audits/root-and-metadata-audit/has-valid-html-lang-attr/has-valid-html-lang-attr.js
+++ scripts/audits/root-and-metadata-audit/has-valid-html-lang-attr/has-valid-html-lang-attr.js
@@ -2,30 +2,36 @@ import { rootAndMetadataErrors } from "../../../errors/root-and-metadata.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 
-export async function hasValidHtmlLangAttr(auditResults) {
-    const validLangValues = [
-        "ab", "aa", "af", "ak", "sq", "am", "ar", "an", "hy", "as", "av", "ae", "ay", "az", "bm", "ba", "eu", "be", "bn",
-        "bi", "bs", "br", "bg", "my", "ca", "ch", "ce", "zh", "cu", "cv", "kw", "co", "cr", "cs", "da", "dv", "nl", "dz",
-        "en", "eo", "et", "ee", "fo", "fj", "fi", "fr", "fy", "ff", "gd", "gl", "lg", "ka", "de", "ki", "el", "kl", "gn",
-        "gu", "ht", "ha", "he", "hz", "hi", "ho", "hr", "hu", "ig", "is", "io", "ii", "iu", "ie", "ia", "id", "ik", "it",
-        "jv", "ja", "kn", "kr", "ks", "kk", "km", "ki", "rw", "ky", "kv", "kg", "ko", "kj", "ku", "lo", "la", "lv", "li",
-        "ln", "lt", "lb", "lu", "mk", "mg", "ms", "ml", "mt", "gv", "mi", "mr", "mh", "mn", "na", "nv", "nd", "ne", "ng",
-        "nb", "nn", "no", "ii", "nr", "oc", "oj", "or", "om", "os", "pa", "pi", "fa", "pl", "pt", "ps", "qu", "rm", "ro",
-        "rn", "ru", "se", "sm", "sg", "sa", "sc", "sr", "sn", "sd", "si", "sk", "sl", "so", "st", "es", "su", "sw", "ss",
-        "sv", "ta", "te", "tg", "th", "ti", "bo", "tk", "tl", "tn", "to", "tr", "ts", "tt", "tw", "ty", "ug", "uk", "ur",
-        "uz", "ve", "vi", "vo", "wa", "cy", "wo", "xh", "yi", "yo", "za", "zu"
-    ];
-    
+export const validLangValues = [
+    "ab", "aa", "af", "ak", "sq", "am", "ar", "an", "hy", "as", "av", "ae", "ay", "az", "bm", "ba", "eu", "be", "bn",
+    "bi", "bs", "br", "bg", "my", "ca", "ch", "ce", "zh", "cu", "cv", "kw", "co", "cr", "cs", "da", "dv", "nl", "dz",
+    "en", "eo", "et", "ee", "fo", "fj", "fi", "fr", "fy", "ff", "gd", "gl", "lg", "ka", "de", "ki", "el", "kl", "gn",
+    "gu", "ht", "ha", "he", "hz", "hi", "ho", "hr", "hu", "ig", "is", "io", "ii", "iu", "ie", "ia", "id", "ik", "it",
+    "jv", "ja", "kn", "kr", "ks", "kk", "km", "ki", "rw", "ky", "kv", "kg", "ko", "kj", "ku", "lo", "la", "lv", "li",
+    "ln", "lt", "lb", "lu", "mk", "mg", "ms", "ml", "mt", "gv", "mi", "mr", "mh", "mn", "na", "nv", "nd", "ne", "ng",
+    "nb", "nn", "no", "ii", "nr", "oc", "oj", "or", "om", "os", "pa", "pi", "fa", "pl", "pt", "ps", "qu", "rm", "ro",
+    "rn", "ru", "se", "sm", "sg", "sa", "sc", "sr", "sn", "sd", "si", "sk", "sl", "so", "st", "es", "su", "sw", "ss",
+    "sv", "ta", "te", "tg", "th", "ti", "bo", "tk", "tl", "tn", "to", "tr", "ts", "tt", "tw", "ty", "ug", "uk", "ur",
+    "uz", "ve", "vi", "vo", "wa", "cy", "wo", "xh", "yi", "yo", "za", "zu"
+];
+
+export function hasValidHtmlLangAttr() {
+    const rootDocument = document.documentElement;
+    const hasLangAttr = rootDocument.getAttribute('lang');
+
+    return {
+        hasLangAttr,
+        outerHTML: rootDocument.cloneNode().outerHTML,
+        selector: getUniqueSelector(rootDocument)
+    }
+}
+
+export async function hasValidHtmlLangAttrEval(auditResults) {
     const rootDocument = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
-        const rootDocument = document.documentElement;
-        const hasLangAttr = rootDocument.getAttribute('lang');
+        const hasValidHtmlLangAttr = ${hasValidHtmlLangAttr.toString()};
 
-        return {
-            hasLangAttr,
-            outerHTML: rootDocument.cloneNode().outerHTML,
-            selector: getUniqueSelector(rootDocument)
-        }
+        return hasValidHtmlLangAttr();
     `)
 
     if (rootDocument.hasLangAttr && !validLangValues.includes(rootDocument.hasLangAttr.split('-')[0])) {
--- scripts/audits/root-and-metadata-audit/root-and-metadata-audit.js
+++ scripts/audits/root-and-metadata-audit/root-and-metadata-audit.js
@@ -3,10 +3,10 @@ import { hasMetaRefreshEval } from "./has-meta-refresh/has-meta-refresh.js";
 import { hasMetaViewportMaximumScaleEval } from "./has-meta-viewport-maximum-scale/has-meta-viewport-maximum-scale.js";
 import { hasMetaViewportUserScalableNoOrZeroEval } from "./has-meta-viewport-scalable/has-meta-viewport-scalable.js";
 import { hasPageTitleEval } from "./has-page-title/has-page-title.js";
-import { hasValidHtmlLangAttr } from "./has-valid-html-lang-attr/has-valid-html-lang-attr.js";
+import { hasValidHtmlLangAttrEval } from "./has-valid-html-lang-attr/has-valid-html-lang-attr.js";
 
 export async function rootAndMetadataAudit(auditResults) {
-    await hasValidHtmlLangAttr(auditResults);
+    await hasValidHtmlLangAttrEval(auditResults);
     await hasHtmlLangAttrEval(auditResults);
     await hasPageTitleEval(auditResults);
     await hasMetaRefreshEval(auditResults);
