50fb4053f6f76b70e3e238ad677bd1dbdf0f2345
naiiiden <na4ko359@gmail.com>
2025-05-06 21:20:34 +0300

    has-html-lang-attr extract logic

--- scripts/audits/root-and-metadata-audit/has-html-lang-attr/has-html-lang-attr.js
+++ scripts/audits/root-and-metadata-audit/has-html-lang-attr/has-html-lang-attr.js
@@ -2,25 +2,31 @@ import { rootAndMetadataErrors } from "../../../errors/root-and-metadata.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 
-// https://www.w3.org/WAI/WCAG22/Techniques/html/H57
-export async function hasHtmlLangAttr(auditResults) {
-    const rootDocument = await inspectedWindowEval(`
-        const getUniqueSelector = ${getUniqueSelector.toString()};
-        const rootDocument = document.documentElement;
-        const hasLangAttr = rootDocument.getAttribute('lang');
+export function hasHtmlLangAttr() {
+    const rootDocument = document.documentElement;
+    const hasLangAttr = rootDocument.getAttribute('lang');
 
-        return {
-            hasLangAttr,
-            outerHTML: rootDocument.cloneNode().outerHTML,
-            selector: getUniqueSelector(rootDocument)
-        }
-    `)
+    return {
+        hasLangAttr,
+        outerHTML: rootDocument.cloneNode().outerHTML,
+        selector: getUniqueSelector(rootDocument)
+    }
+}
+
+// https://www.w3.org/WAI/WCAG22/Techniques/html/H57
+export async function hasHtmlLangAttrEval(auditResults) {
+    const htmlLangAttr = await inspectedWindowEval(`
+        const getUniqueSelector = ${getUniqueSelector.toString()};
+        const hasHtmlLangAttr = ${hasHtmlLangAttr.toString()};
     
-    if (!rootDocument.hasLangAttr) {
+        return hasHtmlLangAttr();
+    `);
+    
+    if (!htmlLangAttr.hasLangAttr) {
         auditResults.push({ 
             ...rootAndMetadataErrors[0], 
-            element: rootDocument.outerHTML, 
-            selector: rootDocument.selector 
+            element: htmlLangAttr.outerHTML, 
+            selector: htmlLangAttr.selector 
         });
     }
 }
--- scripts/audits/root-and-metadata-audit/root-and-metadata-audit.js
+++ scripts/audits/root-and-metadata-audit/root-and-metadata-audit.js
@@ -1,4 +1,4 @@
-import { hasHtmlLangAttr} from "./has-html-lang-attr/has-html-lang-attr.js";
+import { hasHtmlLangAttrEval} from "./has-html-lang-attr/has-html-lang-attr.js";
 import { hasMetaRefresh } from "./has-meta-refresh/has-meta-refresh.js";
 import { hasMetaViewportMaximumScale } from "./has-meta-viewport-maximum-scale/has-meta-viewport-maximum-scale.js";
 import { hasMetaViewportUserScalableNoOrZero } from "./has-meta-viewport-scalable/has-meta-viewport-scalable.js";
@@ -7,7 +7,7 @@ import { hasValidHtmlLangAttr } from "./has-valid-html-lang-attr/has-valid-html-
 
 export async function rootAndMetadataAudit(auditResults) {
     await hasValidHtmlLangAttr(auditResults);
-    await hasHtmlLangAttr(auditResults);
+    await hasHtmlLangAttrEval(auditResults);
     await hasPageTitle(auditResults);
     await hasMetaRefresh(auditResults);
     await hasMetaViewportUserScalableNoOrZero(auditResults);
