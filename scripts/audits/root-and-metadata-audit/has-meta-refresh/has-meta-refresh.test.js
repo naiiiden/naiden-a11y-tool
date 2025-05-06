852ef33ad8835f158fca972300293cce60ab5822
naiiiden <na4ko359@gmail.com>
2025-05-06 08:43:58 +0300

    has-meta-refresh.test.js

---
+++ scripts/audits/root-and-metadata-audit/has-meta-refresh/has-meta-refresh.test.js
@@ -0,0 +1,38 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+import { hasMetaRefresh } from './has-meta-refresh.js';
+
+beforeEach(() => {
+  chrome.devtools.inspectedWindow.eval.mockReset();
+});
+
+describe('hasHtmlLangAttr audit', () => {
+  it('pushes an error if the document has <meta http-equiv="refresh"> tag', async () => {
+    const mockEvalResult = {
+      hasContentAttr: true,
+      outerHTML: `<meta http-equiv="refresh" content="3;url=https://www.mozilla.org"/>`,
+    };
+
+    chrome.devtools.inspectedWindow.eval.mockImplementation((code, callback) => {
+      callback(mockEvalResult);
+    });
+
+    const results = [];
+    await hasMetaRefresh(results);
+    expect(results.length).toBe(1);
+  });
+
+  it('does not push an error if document doesn\'t have <meta http-equiv="refresh"> tag', async () => {
+    const mockEvalResult = {
+      hasContentAttr: null,
+      outerHTML: '<html lang="en"></html>',
+    };
+
+    chrome.devtools.inspectedWindow.eval.mockImplementation((code, callback) => {
+      callback(mockEvalResult);
+    });
+
+    const results = [];
+    await hasMetaRefresh(results);
+    expect(results.length).toBe(0);
+  });
+});
