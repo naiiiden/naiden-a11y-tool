d95e9c4df24dc38b8956975f3e10039fc0235eab
naiiiden <na4ko359@gmail.com>
2025-05-06 22:50:56 +0300

    has-page-title extract logic

--- scripts/audits/root-and-metadata-audit/has-page-title/has-page-title.js
+++ scripts/audits/root-and-metadata-audit/has-page-title/has-page-title.js
@@ -2,20 +2,26 @@ import { rootAndMetadataErrors } from "../../../errors/root-and-metadata.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 
-export async function hasPageTitle(auditResults) {
+export function hasPageTitle() {
+    const documentTitle = document.querySelector("title");
+
+    if (documentTitle) {
+        return {
+            documentTitle: documentTitle.textContent,
+            outerHTML: documentTitle.outerHTML,
+            selector: getUniqueSelector(documentTitle)
+        }
+    } else {
+        return;
+    }
+}
+
+export async function hasPageTitleEval(auditResults) {
     const documentTitle = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
-        const documentTitle = document.querySelector("title");
+        const hasPageTitle = ${hasPageTitle.toString()};
 
-        if (documentTitle) {
-            return {
-                documentTitle: documentTitle.textContent,
-                outerHTML: documentTitle.outerHTML,
-                selector: getUniqueSelector(documentTitle)
-            }
-        } else {
-            return;
-        }
+        return hasPageTitle();
     `);
 
     if (documentTitle?.documentTitle === "" || !documentTitle) {
--- scripts/audits/root-and-metadata-audit/root-and-metadata-audit.js
+++ scripts/audits/root-and-metadata-audit/root-and-metadata-audit.js
@@ -2,13 +2,13 @@ import { hasHtmlLangAttrEval} from "./has-html-lang-attr/has-html-lang-attr.js";
 import { hasMetaRefreshEval } from "./has-meta-refresh/has-meta-refresh.js";
 import { hasMetaViewportMaximumScaleEval } from "./has-meta-viewport-maximum-scale/has-meta-viewport-maximum-scale.js";
 import { hasMetaViewportUserScalableNoOrZeroEval } from "./has-meta-viewport-scalable/has-meta-viewport-scalable.js";
-import { hasPageTitle } from "./has-page-title/has-page-title.js";
+import { hasPageTitleEval } from "./has-page-title/has-page-title.js";
 import { hasValidHtmlLangAttr } from "./has-valid-html-lang-attr/has-valid-html-lang-attr.js";
 
 export async function rootAndMetadataAudit(auditResults) {
     await hasValidHtmlLangAttr(auditResults);
     await hasHtmlLangAttrEval(auditResults);
-    await hasPageTitle(auditResults);
+    await hasPageTitleEval(auditResults);
     await hasMetaRefreshEval(auditResults);
     await hasMetaViewportUserScalableNoOrZeroEval(auditResults);
     await hasMetaViewportMaximumScaleEval(auditResults);
