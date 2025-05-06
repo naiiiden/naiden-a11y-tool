94c0e9f17d2343942791319fa6a690615ff4659e
naiiiden <na4ko359@gmail.com>
2025-05-06 09:50:59 +0300

    reporters verbose

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    setupFiles: ['scripts/utils/tests-global-setup.js']
+    setupFiles: ['scripts/utils/tests-global-setup.js'],
+    reporters: ['verbose']
   },
 });
