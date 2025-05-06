3b62c546ed314594053986102a8562ad98febb3e
naiiiden <na4ko359@gmail.com>
2025-05-06 22:56:53 +0300

    has-page-title.test.js refactor

--- scripts/audits/root-and-metadata-audit/has-page-title/has-page-title.test.js
+++ scripts/audits/root-and-metadata-audit/has-page-title/has-page-title.test.js
@@ -1,34 +1,32 @@
-import { describe, it, expect, beforeEach } from 'vitest';
+import { describe, it, expect } from 'vitest';
+import { setupDOM } from '../../../utils/setup-dom.js';
 import { hasPageTitle } from './has-page-title.js';
 
-beforeEach(() => {
-  chrome.devtools.inspectedWindow.eval.mockReset();
-});
 
 describe('hasPageTitle audit', () => {
   it('pushes an error if the document doesn\'t have a <title>', async () => {
-    const mockEvalResult = undefined;
+    setupDOM(`
+      <html>
+        <head></head>
+        <body></body>
+      </html>      
+    `)
 
-    chrome.devtools.inspectedWindow.eval.mockImplementation((code, callback) => {
-      callback(mockEvalResult);
-    });
-
-    const results = [];
-    await hasPageTitle(results);
-    expect(results.length).toBe(1);
+    const results = hasPageTitle();
+    expect(results).toBe(undefined);
   });
 
   it('pushes an error if document has a <title> but it\'s empty', async () => {
-    const mockEvalResult = {
-      documentTitle: '',
-    };
+    setupDOM(`
+      <html>
+        <head>
+          <title></title>
+        </head>
+        <body></body>
+      </html>      
+    `)
 
-    chrome.devtools.inspectedWindow.eval.mockImplementation((code, callback) => {
-      callback(mockEvalResult);
-    });
-
-    const results = [];
-    await hasPageTitle(results);
-    expect(results.length).toBe(1);
+    const results = hasPageTitle();
+    expect(results.documentTitle).toBe("");
   });
 });
