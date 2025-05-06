2f627526cc75d58299b4fe82c766b41746fb66c2
naiiiden <na4ko359@gmail.com>
2025-05-06 17:07:59 +0300

    test extract logic from eval func

--- scripts/audits/interactive-elements-audit/broken-same-page-links/broken-same-page-links.js
+++ scripts/audits/interactive-elements-audit/broken-same-page-links/broken-same-page-links.js
@@ -3,32 +3,36 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
+export function findBrokenSamePageLinks(document, getUniqueSelector, isElementVisible) {
+    return Array.from(document.querySelectorAll('a[href^="#"]'))
+        .filter(link => {
+            if (!isElementVisible(link)) return false;
+
+            const linkText = link.textContent.toLowerCase();
+            return !(linkText.includes('jump') || linkText.includes('skip'));
+        })
+        .map(link => {
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            return {
+                targetExists: !!targetElement,
+                outerHTML: link.outerHTML,
+                selector: getUniqueSelector(link)
+            };
+        });
+}
+
+
 export async function hasBrokenSamePageLinks(auditResults) {
     const brokenSamePageLinks = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const isElementVisible = ${isElementVisible.toString()};
-        
-        return Array.from(document.querySelectorAll('a[href^="#"]'))
-            .filter(link => {
-                if (!isElementVisible(link)) {
-                    return false;
-                }
+        const findBrokenSamePageLinks = ${findBrokenSamePageLinks.toString()};
 
-                const linkText = link.textContent.toLowerCase();
-                return !(linkText.includes('jump') || linkText.includes('skip'));
-            })
-            .map(link => {
-                const targetId = link.getAttribute('href').substring(1);
-                const targetElement = document.getElementById(targetId);
-                
-                return {
-                    targetExists: !!targetElement,
-                    outerHTML: link.outerHTML,
-                    selector: getUniqueSelector(link)
-                };
-            });
-      `) 
-      
+        return findBrokenSamePageLinks(document, getUniqueSelector, isElementVisible);
+    `);
+
     brokenSamePageLinks
         .filter(link => !link.targetExists)
         .forEach(link => {
