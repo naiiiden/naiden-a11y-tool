4674418b407374637b658bb34d76939ba13ed657
naiiiden <na4ko359@gmail.com>
2025-05-07 09:56:53 +0300

    missing-alt-images extract logic

--- scripts/audits/images-audit/images-audit.js
+++ scripts/audits/images-audit/images-audit.js
@@ -2,13 +2,13 @@ import { hasButtonImagesEval } from "./button-images/button-images.js";
 import { hasImageMapsEval } from "./image-maps/image-maps.js";
 import { hasRedundantImgAltEval } from "./image-redundant-alt/image-redundant-alt.js";
 import { hasLinkedImagesEval } from "./linked-images/linked-images.js";
-import { hasMissingAltImages } from "./missing-alt-images/missing-alt-images.js";
+import { hasMissingAltImagesEval } from "./missing-alt-images/missing-alt-images.js";
 import { hasRoleImg } from "./role-img/role-img.js";
 import { hasServerSideImgMaps } from "./server-side-img-maps/server-side-img-maps.js";
 import { hasSvgRoleImgMissingAlt } from "./svg-role-img-missing-alt/svg-role-img-missing-alt.js";
 
 export async function imagesAudit(auditResults) {
-    await hasMissingAltImages(auditResults);
+    await hasMissingAltImagesEval(auditResults);
     await hasLinkedImagesEval(auditResults);
     await hasButtonImagesEval(auditResults);
     await hasImageMapsEval(auditResults);
--- scripts/audits/images-audit/missing-alt-images/missing-alt-images.js
+++ scripts/audits/images-audit/missing-alt-images/missing-alt-images.js
@@ -3,30 +3,35 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
-export async function hasMissingAltImages(auditResults) {
+export function hasMissingAltImages() {
+    return Array.from(document.querySelectorAll('img:not(a img):not(button img)'))
+        .filter(img => {
+            if (!isElementVisible(img)) {
+                return false;
+            }
+    
+            const alt = img.getAttribute('alt');
+            const ariaLabel = img.hasAttribute('aria-label') ? img.getAttribute('aria-label').trim() : null;
+            const ariaLabelledby = img.hasAttribute('aria-labelledby') 
+                ? document.getElementById(img.getAttribute('aria-labelledby')) 
+                : null;
+            const role = img.hasAttribute('role') ? img.getAttribute('role').trim() : null;
+    
+            return alt === null && !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim())) && (!role || (role !== 'none' && role !== 'presentation'));
+        })
+        .map(img => ({
+            outerHTML: img.outerHTML,
+            selector: getUniqueSelector(img)
+        }));
+}
+
+export async function hasMissingAltImagesEval(auditResults) {
     const missingAltImages = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const isElementVisible = ${isElementVisible.toString()};
+        const hasMissingAltImages = ${hasMissingAltImages.toString()};
 
-        return Array.from(document.querySelectorAll('img:not(a img):not(button img)'))
-            .filter(img => {
-                if (!isElementVisible(img)) {
-                    return false;
-                }
-
-                const alt = img.getAttribute('alt');
-                const ariaLabel = img.hasAttribute('aria-label') ? img.getAttribute('aria-label').trim() : null;
-                const ariaLabelledby = img.hasAttribute('aria-labelledby') 
-                    ? document.getElementById(img.getAttribute('aria-labelledby')) 
-                    : null;
-                const role = img.hasAttribute('role') ? img.getAttribute('role').trim() : null;
-
-                return alt === null && !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim())) && (!role || (role !== 'none' && role !== 'presentation'));
-            })
-            .map(img => ({
-                outerHTML: img.outerHTML,
-                selector: getUniqueSelector(img)
-            }));
+        return hasMissingAltImages();
     `);
   
     missingAltImages.forEach(img => {
