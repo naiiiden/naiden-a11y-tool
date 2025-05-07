e01c1f3c6f26be175b47f1e8a749e65a995c8d91
naiiiden <na4ko359@gmail.com>
2025-05-07 09:38:34 +0300

    has-duplicate-accesskeys extract logic

--- scripts/audits/interactive-elements-audit/has-duplicate-accesskey/has-duplicate-accesskey.js
+++ scripts/audits/interactive-elements-audit/has-duplicate-accesskey/has-duplicate-accesskey.js
@@ -2,29 +2,35 @@ import { interactiveElementsErrors } from "../../../errors/interactive-elements.
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 
-export async function hasDuplicateAccesskeys(auditResults) {
-    const hasDuplicateAccesskeys = await inspectedWindowEval(`
+export function hasDuplicateAccesskeys() {
+    const elementsWithAccesskeys = Array.from(document.querySelectorAll("[accesskey]:not([accesskey=''])"));
+    
+    const duplicates = elementsWithAccesskeys.reduce((acc, element) => {
+        const accesskey = element.getAttribute("accesskey").trim();
+        if (!acc[accesskey]) {
+            acc[accesskey] = [];
+        }
+        acc[accesskey].push(element);
+        return acc;
+    }, {});
+    
+    return Object.values(duplicates)
+        .filter(elements => elements.length > 1)
+        .map(elements => elements.map(element => ({
+            selector: getUniqueSelector(element),
+            outerHTML: element.cloneNode().outerHTML
+        })));
+}
+
+export async function hasDuplicateAccesskeysEval(auditResults) {
+    const duplicateAccesskeys = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
-        const elementsWithAccesskeys = Array.from(document.querySelectorAll("[accesskey]:not([accesskey=''])"));
+        const hasDuplicateAccesskeys = ${hasDuplicateAccesskeys.toString()};
 
-        const duplicates = elementsWithAccesskeys.reduce((acc, element) => {
-            const accesskey = element.getAttribute("accesskey").trim();
-            if (!acc[accesskey]) {
-                acc[accesskey] = [];
-            }
-            acc[accesskey].push(element);
-            return acc;
-        }, {});
-
-        return Object.values(duplicates)
-            .filter(elements => elements.length > 1)
-            .map(elements => elements.map(element => ({
-                selector: getUniqueSelector(element),
-                outerHTML: element.cloneNode().outerHTML
-            })));
+        return hasDuplicateAccesskeys();
     `);
 
-    hasDuplicateAccesskeys.forEach(group => {
+    duplicateAccesskeys.forEach(group => {
         const helperText = `Duplicate ID found on the following elements:\n` +
             group.map(d => `- Selector: ${d.selector}`).join("\n");
 
--- scripts/audits/interactive-elements-audit/interactive-elements-audit.js
+++ scripts/audits/interactive-elements-audit/interactive-elements-audit.js
@@ -2,7 +2,7 @@ import { hasBrokenSamePageLinksEval } from "./broken-same-page-links/broken-same
 import { hasBrokenSkipLinksEval } from "./broken-skip-links/broken-skip-links.js";
 import { hasEmptyButtonsEval } from "./empty-buttons/empty-buttons.js";
 import { hasEmptyLinksEval } from "./empty-links/empty-links.js";
-import { hasDuplicateAccesskeys } from "./has-duplicate-accesskey/has-duplicate-accesskey.js";
+import { hasDuplicateAccesskeysEval } from "./has-duplicate-accesskey/has-duplicate-accesskey.js";
 import { hasInteractiveControlsWithInteractiveControlsAsChildren } from "./interactive-controls-w-interactive-children/interactive-controls-w-interactive-children.js";
 import { scrollableRegionKeyboardAccess } from "./scrollable-region-keyboard-access/scrollable-region-keyboard-access.js";
 import { touchTargetSize } from "./touch-target-size/touch-target-size.js";
@@ -13,7 +13,7 @@ export async function interactiveElementsAudit(auditResults) {
     await hasBrokenSkipLinksEval(auditResults);
     await hasInteractiveControlsWithInteractiveControlsAsChildren(auditResults);
     await hasBrokenSamePageLinksEval(auditResults);
-    await hasDuplicateAccesskeys(auditResults);
+    await hasDuplicateAccesskeysEval(auditResults);
     await touchTargetSize(auditResults);
     await scrollableRegionKeyboardAccess(auditResults);
 }
