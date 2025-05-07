4f6777b0f6643de38e00c9b650a8c23da4901d77
naiiiden <na4ko359@gmail.com>
2025-05-07 09:35:50 +0300

    empty-links extract logic

--- scripts/audits/interactive-elements-audit/empty-links/empty-links.js
+++ scripts/audits/interactive-elements-audit/empty-links/empty-links.js
@@ -3,29 +3,34 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
-export async function hasEmptyLinks(auditResults) {
+export function hasEmptyLinks() {
+    return Array.from(document.querySelectorAll('a:not(:has(img))'))
+        .filter(link => {
+            if (!isElementVisible(link)) {
+                return false;
+            }
+    
+            const ariaLabel = link.hasAttribute('aria-label') ? link.getAttribute('aria-label').trim() : null;
+            const ariaLabelledby = link.hasAttribute('aria-labelledby') 
+            ? document.getElementById(link.getAttribute('aria-labelledby')) 
+            : null;
+            const title = link.hasAttribute('title') ? link.getAttribute('title').trim() : null;
+    
+            return link.textContent.trim() === "" && !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim()) || title);
+        })
+        .map(link => ({
+            outerHTML: link.outerHTML,
+            selector: getUniqueSelector(link)
+        }));
+}
+
+export async function hasEmptyLinksEval(auditResults) {
     const emptyLinks = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const isElementVisible = ${isElementVisible.toString()};
-        
-        return Array.from(document.querySelectorAll('a:not(:has(img))'))
-            .filter(link => {
-                if (!isElementVisible(link)) {
-                    return false;
-                }
+        const hasEmptyLinks = ${hasEmptyLinks.toString()};
 
-                const ariaLabel = link.hasAttribute('aria-label') ? link.getAttribute('aria-label').trim() : null;
-                const ariaLabelledby = link.hasAttribute('aria-labelledby') 
-                ? document.getElementById(link.getAttribute('aria-labelledby')) 
-                : null;
-                const title = link.hasAttribute('title') ? link.getAttribute('title').trim() : null;
-        
-                return link.textContent.trim() === "" && !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim()) || title);
-            })
-            .map(link => ({
-                outerHTML: link.outerHTML,
-                selector: getUniqueSelector(link)
-            }));
+        return hasEmptyLinks();
     `) 
   
     emptyLinks.forEach(link => {
--- scripts/audits/interactive-elements-audit/interactive-elements-audit.js
+++ scripts/audits/interactive-elements-audit/interactive-elements-audit.js
@@ -1,14 +1,14 @@
 import { hasBrokenSamePageLinksEval } from "./broken-same-page-links/broken-same-page-links.js";
 import { hasBrokenSkipLinksEval } from "./broken-skip-links/broken-skip-links.js";
 import { hasEmptyButtonsEval } from "./empty-buttons/empty-buttons.js";
-import { hasEmptyLinks } from "./empty-links/empty-links.js";
+import { hasEmptyLinksEval } from "./empty-links/empty-links.js";
 import { hasDuplicateAccesskeys } from "./has-duplicate-accesskey/has-duplicate-accesskey.js";
 import { hasInteractiveControlsWithInteractiveControlsAsChildren } from "./interactive-controls-w-interactive-children/interactive-controls-w-interactive-children.js";
 import { scrollableRegionKeyboardAccess } from "./scrollable-region-keyboard-access/scrollable-region-keyboard-access.js";
 import { touchTargetSize } from "./touch-target-size/touch-target-size.js";
 
 export async function interactiveElementsAudit(auditResults) {
-    await hasEmptyLinks(auditResults);
+    await hasEmptyLinksEval(auditResults);
     await hasEmptyButtonsEval(auditResults);
     await hasBrokenSkipLinksEval(auditResults);
     await hasInteractiveControlsWithInteractiveControlsAsChildren(auditResults);
