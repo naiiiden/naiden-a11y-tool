8ed163993e29a824fa9fef4c4adbe5e90cd27b8a
naiiiden <na4ko359@gmail.com>
2025-05-07 10:36:27 +0300

    empty-headings extract logic

--- scripts/audits/empty-elements-audit/empty-audit.js
+++ scripts/audits/empty-elements-audit/empty-audit.js
@@ -1,10 +1,10 @@
 import { hasEmptyHeadingsWithImages } from "./empty-headings-with-images/empty-headings-with-images.js";
-import { hasEmptyHeadings } from "./empty-headings/empty-headings.js";
+import { hasEmptyHeadingsEval } from "./empty-headings/empty-headings.js";
 import { hasEmptyOrMissingSummaries } from "./empty-summaries/empty-summaries.js";
 import { hasEmptyTableHeadings } from "./empty-table-headings/empty-table-headings.js";
 
 export async function emptyAudit(auditResults) {
-    await hasEmptyHeadings(auditResults);
+    await hasEmptyHeadingsEval(auditResults);
     await hasEmptyHeadingsWithImages(auditResults);
     await hasEmptyTableHeadings(auditResults);
     await hasEmptyOrMissingSummaries(auditResults);
--- scripts/audits/empty-elements-audit/empty-headings/empty-headings.js
+++ scripts/audits/empty-elements-audit/empty-headings/empty-headings.js
@@ -3,29 +3,34 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
-export async function hasEmptyHeadings(auditResults) {
+export function hasEmptyHeadings() {
+    return Array.from(document.querySelectorAll(":is(h1, h2, h3, h4, h5, h6, [role='heading']):not(:has(img))"))
+        .filter(heading => {
+            if (!isElementVisible(heading)) {
+                return false;
+            }
+    
+            const ariaLabel = heading.hasAttribute('aria-label') ? heading.getAttribute('aria-label').trim() : null;
+            const ariaLabelledby = heading.hasAttribute('aria-labelledby') 
+                ? document.getElementById(heading.getAttribute('aria-labelledby')) 
+                : null;
+            const title = heading.hasAttribute('title') ? heading.getAttribute('title').trim() : null;
+            
+            return heading.textContent.trim() === "" && !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim()) || title);
+        })
+        .map(heading => ({
+            outerHTML: heading.outerHTML,
+            selector: getUniqueSelector(heading)
+        }));
+}
+
+export async function hasEmptyHeadingsEval(auditResults) {
     const emptyHeadings = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const isElementVisible = ${isElementVisible.toString()};
+        const hasEmptyHeadings = ${hasEmptyHeadings.toString()};
 
-        return Array.from(document.querySelectorAll(":is(h1, h2, h3, h4, h5, h6, [role='heading']):not(:has(img))"))
-            .filter(heading => {
-                if (!isElementVisible(heading)) {
-                    return false;
-                }
-
-                const ariaLabel = heading.hasAttribute('aria-label') ? heading.getAttribute('aria-label').trim() : null;
-                const ariaLabelledby = heading.hasAttribute('aria-labelledby') 
-                    ? document.getElementById(heading.getAttribute('aria-labelledby')) 
-                    : null;
-                const title = heading.hasAttribute('title') ? heading.getAttribute('title').trim() : null;
-                
-                return heading.textContent.trim() === "" && !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim()) || title);
-            })
-            .map(heading => ({
-                outerHTML: heading.outerHTML,
-                selector: getUniqueSelector(heading)
-            }));
+        return hasEmptyHeadings();
     `) 
   
     emptyHeadings.forEach(heading => {
