9fc978007ccb960c45391bc962bff2c7d7435bcb
naiiiden <na4ko359@gmail.com>
2025-05-07 10:53:06 +0300

    frame-unique-title extract logic

--- scripts/audits/embedded-elements-audit/embedded-elements-audit.js
+++ scripts/audits/embedded-elements-audit/embedded-elements-audit.js
@@ -1,4 +1,4 @@
-import { hasFrameUniqueTitles } from "./frame-unique-title/frame-unique-title.js";
+import { hasFrameUniqueTitlesEval } from "./frame-unique-title/frame-unique-title.js";
 import { hasVideoCaptions } from "./has-video-captions/has-video-captions.js";
 import { hasEmptyIframeTitles } from "./iframe-titles/iframe-titles.js";
 import { objectHasAltText } from "./object-has-alt-text/object-has-alt-text.js";
@@ -7,7 +7,7 @@ import { videOrAudioHasAutoplay } from "./video-audio-no-autoplay/video-audio-no
 export async function embeddedElementsAudit(auditResults) {
     await hasEmptyIframeTitles(auditResults);
     await objectHasAltText(auditResults);
-    await hasFrameUniqueTitles(auditResults);
+    await hasFrameUniqueTitlesEval(auditResults);
     await videOrAudioHasAutoplay(auditResults);
     await hasVideoCaptions(auditResults);
 }
--- scripts/audits/embedded-elements-audit/frame-unique-title/frame-unique-title.js
+++ scripts/audits/embedded-elements-audit/frame-unique-title/frame-unique-title.js
@@ -3,36 +3,41 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
-export async function hasFrameUniqueTitles(auditResults) {
-    const hasFrameUniqueTitles = await inspectedWindowEval(`
+export function hasFrameUniqueTitles() {
+    const titleMap = new Map();
+    return Array.from(document.querySelectorAll('iframe'))
+        .filter(iframe => isElementVisible(iframe))
+        .map(iframe => {
+            const title = iframe.hasAttribute('title') ? iframe.getAttribute('title').trim() : null;
+    
+            if (!title) return null;
+    
+            const selector = getUniqueSelector(iframe);
+    
+            if (titleMap.has(title)) {
+                titleMap.get(title).push(selector);
+                return {
+                    element: iframe.outerHTML,
+                    selector,
+                };
+            }
+    
+            titleMap.set(title, [selector]);
+    
+            return null;
+        }).filter(issue => issue !== null);
+}
+
+export async function hasFrameUniqueTitlesEval(auditResults) {
+    const frameUniqueTitles = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const isElementVisible = ${isElementVisible.toString()};
+        const hasFrameUniqueTitles = ${hasFrameUniqueTitles.toString()};
 
-        const titleMap = new Map();
-        return Array.from(document.querySelectorAll('iframe'))
-            .filter(iframe => isElementVisible(iframe))
-            .map(iframe => {
-                const title = iframe.hasAttribute('title') ? iframe.getAttribute('title').trim() : null;
-
-                if (!title) return null;
-
-                const selector = getUniqueSelector(iframe);
-
-                if (titleMap.has(title)) {
-                    titleMap.get(title).push(selector);
-                    return {
-                        element: iframe.outerHTML,
-                        selector,
-                    };
-                }
-
-                titleMap.set(title, [selector]);
-
-                return null;
-            }).filter(issue => issue !== null);
+        return hasFrameUniqueTitles();
     `);
 
-    hasFrameUniqueTitles.forEach(element => {
+    frameUniqueTitles.forEach(element => {
         auditResults.push({
             ...embeddedElementsErrors[2],
             element: element.element,
