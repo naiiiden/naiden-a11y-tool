24a3c44b2fdac391ece65432c40b5fde2c5de94c
naiiiden <na4ko359@gmail.com>
2025-05-07 10:58:18 +0300

    object-has-alt-text extract logic

--- scripts/audits/embedded-elements-audit/embedded-elements-audit.js
+++ scripts/audits/embedded-elements-audit/embedded-elements-audit.js
@@ -1,12 +1,12 @@
 import { hasFrameUniqueTitlesEval } from "./frame-unique-title/frame-unique-title.js";
 import { hasVideoCaptionsEval } from "./has-video-captions/has-video-captions.js";
 import { hasEmptyIframeTitlesEval } from "./iframe-titles/iframe-titles.js";
-import { objectHasAltText } from "./object-has-alt-text/object-has-alt-text.js";
+import { hasObjectAltTextEval } from "./object-has-alt-text/object-has-alt-text.js";
 import { videOrAudioHasAutoplay } from "./video-audio-no-autoplay/video-audio-no-autoplay.js";
 
 export async function embeddedElementsAudit(auditResults) {
     await hasEmptyIframeTitlesEval(auditResults);
-    await objectHasAltText(auditResults);
+    await hasObjectAltTextEval(auditResults);
     await hasFrameUniqueTitlesEval(auditResults);
     await videOrAudioHasAutoplay(auditResults);
     await hasVideoCaptionsEval(auditResults);
--- scripts/audits/embedded-elements-audit/object-has-alt-text/object-has-alt-text.js
+++ scripts/audits/embedded-elements-audit/object-has-alt-text/object-has-alt-text.js
@@ -3,33 +3,38 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
-export async function objectHasAltText(auditResults) {
-    const objectHasAltText = await inspectedWindowEval(`
+export function hasObjectAltText() {
+    return Array.from(document.querySelectorAll("object"))
+        .filter(element => {
+            if (!isElementVisible(element)) {
+                return false;
+            }
+    
+            const ariaLabel = element.hasAttribute('aria-label') ? element.getAttribute('aria-label').trim() : null;
+            const ariaLabelledby = element.hasAttribute('aria-labelledby') 
+                ? document.getElementById(element.getAttribute('aria-labelledby')) 
+                : null;
+            const title = element.hasAttribute('title') ? element.getAttribute('title').trim() : null;
+            const role = element.hasAttribute('role') ? element.getAttribute('role').trim() : null;
+            
+            return !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim()) || title) && (!role || (role !== 'none' && role !== 'presentation'));
+        })
+        .map(element => ({
+            outerHTML: element.outerHTML,
+            selector: getUniqueSelector(element)
+        }));
+}
+
+export async function hasObjectAltTextEval(auditResults) {
+    const objectAltText = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const isElementVisible = ${isElementVisible.toString()};
-        
-        return Array.from(document.querySelectorAll("object"))
-            .filter(element => {
-                if (!isElementVisible(element)) {
-                    return false;
-                }
+        const hasObjectAltText = ${hasObjectAltText.toString()};
 
-                const ariaLabel = element.hasAttribute('aria-label') ? element.getAttribute('aria-label').trim() : null;
-                const ariaLabelledby = element.hasAttribute('aria-labelledby') 
-                    ? document.getElementById(element.getAttribute('aria-labelledby')) 
-                    : null;
-                const title = element.hasAttribute('title') ? element.getAttribute('title').trim() : null;
-                const role = element.hasAttribute('role') ? element.getAttribute('role').trim() : null;
-                
-                return !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim()) || title) && (!role || (role !== 'none' && role !== 'presentation'));
-            })
-            .map(element => ({
-                outerHTML: element.outerHTML,
-                selector: getUniqueSelector(element)
-            }));
+        return hasObjectAltText();
     `) 
   
-    objectHasAltText.forEach(element => {
+    objectAltText.forEach(element => {
         auditResults.push({ ...embeddedElementsErrors[1], element: element.outerHTML, selector: element.selector });
     });
 }
