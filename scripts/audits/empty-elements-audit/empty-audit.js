b4ef885efad60428dd0223beaf08915ae6eedf58
naiiiden <na4ko359@gmail.com>
2025-05-07 10:39:11 +0300

    empty-summaries extract logic

--- scripts/audits/empty-elements-audit/empty-audit.js
+++ scripts/audits/empty-elements-audit/empty-audit.js
@@ -1,11 +1,11 @@
 import { hasEmptyHeadingsWithImagesEval } from "./empty-headings-with-images/empty-headings-with-images.js";
 import { hasEmptyHeadingsEval } from "./empty-headings/empty-headings.js";
-import { hasEmptyOrMissingSummaries } from "./empty-summaries/empty-summaries.js";
+import { hasEmptyOrMissingSummariesEval } from "./empty-summaries/empty-summaries.js";
 import { hasEmptyTableHeadings } from "./empty-table-headings/empty-table-headings.js";
 
 export async function emptyAudit(auditResults) {
     await hasEmptyHeadingsEval(auditResults);
     await hasEmptyHeadingsWithImagesEval(auditResults);
     await hasEmptyTableHeadings(auditResults);
-    await hasEmptyOrMissingSummaries(auditResults);
+    await hasEmptyOrMissingSummariesEval(auditResults);
 }
--- scripts/audits/empty-elements-audit/empty-summaries/empty-summaries.js
+++ scripts/audits/empty-elements-audit/empty-summaries/empty-summaries.js
@@ -3,35 +3,40 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
-export async function hasEmptyOrMissingSummaries(auditResults) {
+export function hasEmptyOrMissingSummaries() {
+    return Array.from(document.querySelectorAll("details"))
+        .filter(details => {
+            if (!isElementVisible(details)) {
+                return false;
+            }
+            const summary = details.querySelector("summary");
+    
+            if (summary) {
+                const ariaLabel = summary.hasAttribute('aria-label') ? summary.getAttribute('aria-label').trim() : null;
+                const ariaLabelledby = summary.hasAttribute('aria-labelledby') 
+                    ? document.getElementById(summary.getAttribute('aria-labelledby')) 
+                    : null;
+                const title = summary.hasAttribute('title') ? summary.getAttribute('title').trim() : null;
+        
+                return summary.textContent.trim() === "" && !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim()) || title);
+            }
+    
+            return !summary;
+        })
+      .map(details => ({
+            outerHTML: details.outerHTML,
+            selector: getUniqueSelector(details),
+            summaryExists: !!details.querySelector("summary")
+      }));
+}
+
+export async function hasEmptyOrMissingSummariesEval(auditResults) {
     const emptyOrMissingSummaries = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const isElementVisible = ${isElementVisible.toString()};
+        const hasEmptyOrMissingSummaries = ${hasEmptyOrMissingSummaries.toString()};
 
-        return Array.from(document.querySelectorAll("details"))
-            .filter(details => {
-                if (!isElementVisible(details)) {
-                    return false;
-                }
-                const summary = details.querySelector("summary");
-        
-                if (summary) {
-                    const ariaLabel = summary.hasAttribute('aria-label') ? summary.getAttribute('aria-label').trim() : null;
-                    const ariaLabelledby = summary.hasAttribute('aria-labelledby') 
-                        ? document.getElementById(summary.getAttribute('aria-labelledby')) 
-                        : null;
-                    const title = summary.hasAttribute('title') ? summary.getAttribute('title').trim() : null;
-            
-                    return summary.textContent.trim() === "" && !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim()) || title);
-                }
-        
-                return !summary;
-            })
-          .map(details => ({
-                outerHTML: details.outerHTML,
-                selector: getUniqueSelector(details),
-                summaryExists: !!details.querySelector("summary")
-          }));
+        return hasEmptyOrMissingSummaries();
     `);
       
     emptyOrMissingSummaries.forEach(summary => {
