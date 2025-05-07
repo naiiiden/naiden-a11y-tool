e57ca97ae64c56c9afb111b8bff571ab2431df98
naiiiden <na4ko359@gmail.com>
2025-05-07 10:05:27 +0300

    empty-labels extract logic

--- scripts/audits/form-audit/empty-labels/empty-labels.js
+++ scripts/audits/form-audit/empty-labels/empty-labels.js
@@ -3,29 +3,34 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
-export async function hasEmptyLabels(auditResults) {
+export function hasEmptyLabels() {
+    return Array.from(document.querySelectorAll("label"))
+        .filter(label => {
+            if (!isElementVisible(label)) {
+                return false;
+            }
+    
+            const ariaLabel = label.hasAttribute('aria-label') ? label.getAttribute('aria-label').trim() : null;
+            const ariaLabelledby = label.hasAttribute('aria-labelledby') 
+            ? document.getElementById(label.getAttribute('aria-labelledby')) 
+            : null;
+            const title = label.hasAttribute('title') ? label.getAttribute('title').trim() : null;
+    
+            return label.textContent.trim() === "" && !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim()) || title);
+        })
+        .map(label => ({ 
+            outerHTML: label.outerHTML, 
+            selector: getUniqueSelector(label) 
+        }))
+}
+
+export async function hasEmptyLabelsEval(auditResults) {
     const emptyLabels = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const isElementVisible = ${isElementVisible.toString()};
+        const hasEmptyLabels = ${hasEmptyLabels.toString()};
 
-        return Array.from(document.querySelectorAll("label"))
-            .filter(label => {
-                if (!isElementVisible(label)) {
-                    return false;
-                }
-
-                const ariaLabel = label.hasAttribute('aria-label') ? label.getAttribute('aria-label').trim() : null;
-                const ariaLabelledby = label.hasAttribute('aria-labelledby') 
-                ? document.getElementById(label.getAttribute('aria-labelledby')) 
-                : null;
-                const title = label.hasAttribute('title') ? label.getAttribute('title').trim() : null;
-
-                return label.textContent.trim() === "" && !(ariaLabel || (ariaLabelledby && ariaLabelledby.textContent.trim()) || title);
-            })
-            .map(label => ({ 
-                outerHTML: label.outerHTML, 
-                selector: getUniqueSelector(label) 
-            }))
+        return hasEmptyLabels();
     `)
 
     emptyLabels.forEach(label => {
--- scripts/audits/form-audit/form-audit.js
+++ scripts/audits/form-audit/form-audit.js
@@ -1,4 +1,4 @@
-import { hasEmptyLabels } from "./empty-labels/empty-labels.js";
+import { hasEmptyLabelsEval } from "./empty-labels/empty-labels.js";
 import { hasEmptySubmitButonOrResetInputValues } from "./empty-submit-button-reset-values/empty-submit-button-reset-values.js";
 import { hasFieldsetsMissingLegend } from "./fieldsets-missing-legend/fieldsets-missing-legend.js";
 import { hasFormControlLabels } from "./form-control-labels/form-control-labels.js";
@@ -8,7 +8,7 @@ import { hasImageInputs } from "./image-inputs/image-inputs.js";
 import { relatedFormControlsMissingFieldset } from "./related-form-controls-without-fieldset/related-form-controls-without-fieldset.js.js";
 
 export async function formAudit(auditResults) {
-    await hasEmptyLabels(auditResults);
+    await hasEmptyLabelsEval(auditResults);
     await hasFormControlLabels(auditResults);
     await hasFieldsetsMissingLegend(auditResults);
     await relatedFormControlsMissingFieldset(auditResults);
