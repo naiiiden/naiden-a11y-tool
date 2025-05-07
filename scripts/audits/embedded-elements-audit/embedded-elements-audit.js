af2180a5dd31158c559ec9a66783d418ea030b8f
naiiiden <na4ko359@gmail.com>
2025-05-07 10:56:06 +0300

    iframe-titles extract logic

--- scripts/audits/embedded-elements-audit/embedded-elements-audit.js
+++ scripts/audits/embedded-elements-audit/embedded-elements-audit.js
@@ -1,11 +1,11 @@
 import { hasFrameUniqueTitlesEval } from "./frame-unique-title/frame-unique-title.js";
 import { hasVideoCaptionsEval } from "./has-video-captions/has-video-captions.js";
-import { hasEmptyIframeTitles } from "./iframe-titles/iframe-titles.js";
+import { hasEmptyIframeTitlesEval } from "./iframe-titles/iframe-titles.js";
 import { objectHasAltText } from "./object-has-alt-text/object-has-alt-text.js";
 import { videOrAudioHasAutoplay } from "./video-audio-no-autoplay/video-audio-no-autoplay.js";
 
 export async function embeddedElementsAudit(auditResults) {
-    await hasEmptyIframeTitles(auditResults);
+    await hasEmptyIframeTitlesEval(auditResults);
     await objectHasAltText(auditResults);
     await hasFrameUniqueTitlesEval(auditResults);
     await videOrAudioHasAutoplay(auditResults);
--- scripts/audits/embedded-elements-audit/iframe-titles/iframe-titles.js
+++ scripts/audits/embedded-elements-audit/iframe-titles/iframe-titles.js
@@ -3,17 +3,22 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
-export async function hasEmptyIframeTitles(auditResults) {
+export function hasEmptyIframeTitles() {
+    return Array.from(document.querySelectorAll("iframe"))
+        .filter(iframe => isElementVisible(iframe))
+        .filter(iframe => !iframe.hasAttribute("title") || iframe.getAttribute("title").trim() === "")
+        .map(iframe => 
+            ({ outerHTML: iframe.outerHTML, selector: getUniqueSelector(iframe) 
+        }));
+}
+
+export async function hasEmptyIframeTitlesEval(auditResults) {
     const iframeTitles = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const isElementVisible = ${isElementVisible.toString()};
+        const hasEmptyIframeTitles = ${hasEmptyIframeTitles.toString()};
 
-        return Array.from(document.querySelectorAll("iframe"))
-            .filter(iframe => isElementVisible(iframe))
-            .filter(iframe => !iframe.hasAttribute("title") || iframe.getAttribute("title").trim() === "")
-            .map(iframe => 
-                ({ outerHTML: iframe.outerHTML, selector: getUniqueSelector(iframe) 
-            }));
+        return hasEmptyIframeTitles();
     `) 
   
     iframeTitles.forEach(iframe => {
