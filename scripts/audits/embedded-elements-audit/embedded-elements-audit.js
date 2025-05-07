1f4ec61bd53770df42410749c88a87b54b64195a
naiiiden <na4ko359@gmail.com>
2025-05-07 10:54:36 +0300

    has-video-captions extract logic

--- scripts/audits/embedded-elements-audit/embedded-elements-audit.js
+++ scripts/audits/embedded-elements-audit/embedded-elements-audit.js
@@ -1,5 +1,5 @@
 import { hasFrameUniqueTitlesEval } from "./frame-unique-title/frame-unique-title.js";
-import { hasVideoCaptions } from "./has-video-captions/has-video-captions.js";
+import { hasVideoCaptionsEval } from "./has-video-captions/has-video-captions.js";
 import { hasEmptyIframeTitles } from "./iframe-titles/iframe-titles.js";
 import { objectHasAltText } from "./object-has-alt-text/object-has-alt-text.js";
 import { videOrAudioHasAutoplay } from "./video-audio-no-autoplay/video-audio-no-autoplay.js";
@@ -9,5 +9,5 @@ export async function embeddedElementsAudit(auditResults) {
     await objectHasAltText(auditResults);
     await hasFrameUniqueTitlesEval(auditResults);
     await videOrAudioHasAutoplay(auditResults);
-    await hasVideoCaptions(auditResults);
+    await hasVideoCaptionsEval(auditResults);
 }
--- scripts/audits/embedded-elements-audit/has-video-captions/has-video-captions.js
+++ scripts/audits/embedded-elements-audit/has-video-captions/has-video-captions.js
@@ -3,20 +3,25 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
-export async function hasVideoCaptions(auditResults) {
-    const hasVideoCaptions = await inspectedWindowEval(`
+export function hasVideoCaptions() {
+    return Array.from(document.querySelectorAll("video:not(:has(track[kind='captions']))"))
+        .filter(element => isElementVisible(element))
+        .map(element => ({
+            outerHTML: element.outerHTML,
+            selector: getUniqueSelector(element)
+        }));
+}
+
+export async function hasVideoCaptionsEval(auditResults) {
+    const videoCaptions = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const isElementVisible = ${isElementVisible.toString()};
+        const hasVideoCaptions = ${hasVideoCaptions.toString()};
 
-        return Array.from(document.querySelectorAll("video:not(:has(track[kind='captions']))"))
-            .filter(element => isElementVisible(element))
-            .map(element => ({
-                outerHTML: element.outerHTML,
-                selector: getUniqueSelector(element)
-            }));
+        return hasVideoCaptions();
     `) 
   
-    hasVideoCaptions.forEach(element => {
+    videoCaptions.forEach(element => {
         auditResults.push({ ...embeddedElementsErrors[4], element: element.outerHTML, selector: element.selector });
     });
 }
