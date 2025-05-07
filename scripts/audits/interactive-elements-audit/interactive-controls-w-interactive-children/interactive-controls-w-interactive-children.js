2afbad3ff22c91c6145348fa875df8b060d7e1d1
naiiiden <na4ko359@gmail.com>
2025-05-07 09:41:50 +0300

    interactive-controls-w-interactive-children extract logic

--- scripts/audits/interactive-elements-audit/interactive-controls-w-interactive-children/interactive-controls-w-interactive-children.js
+++ scripts/audits/interactive-elements-audit/interactive-controls-w-interactive-children/interactive-controls-w-interactive-children.js
@@ -3,48 +3,53 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
-export async function hasInteractiveControlsWithInteractiveControlsAsChildren(auditResults) {
-    const interactiveControlsWithInteractiveControlsAsChildren = await inspectedWindowEval(`
-        const getUniqueSelector = ${getUniqueSelector.toString()};
-        const isElementVisible = ${isElementVisible.toString()};
-        
-        const parents = \`
-            button:not(:disabled), 
+export function hasInteractiveControlsWithInteractiveControlsAsChildren() {
+    const parents = `
+        button:not(:disabled), 
+        a[href], 
+        :is([role='button'], [role='link'])[tabindex]:not([tabindex^='-'], [tabindex='']), 
+        [contenteditable]:not([contenteditable='false']), 
+        [tabindex]:not([tabindex^="-"], [tabindex=''])
+    `;
+
+    const children = `
+        :is(
             a[href], 
             :is([role='button'], [role='link'])[tabindex]:not([tabindex^='-'], [tabindex='']), 
             [contenteditable]:not([contenteditable='false']), 
-            [tabindex]:not([tabindex^="-"], [tabindex=''])
-        \`;
+            [tabindex]:not([tabindex^="-"], [tabindex='']), 
+            :is(input:not([type='hidden']), textarea, select, button):not(:disabled), 
+            summary:not([tabindex^="-"], [tabindex='']), 
+            :is(audio, video)[controls],
+            embed,
+            area[href]:is(map[name]:not([name='']) area
+        )):not([tabindex='-1'])
+    `;
 
-        const children = \`
-            :is(
-                a[href], 
-                :is([role='button'], [role='link'])[tabindex]:not([tabindex^='-'], [tabindex='']), 
-                [contenteditable]:not([contenteditable='false']), 
-                [tabindex]:not([tabindex^="-"], [tabindex='']), 
-                :is(input:not([type='hidden']), textarea, select, button):not(:disabled), 
-                summary:not([tabindex^="-"], [tabindex='']), 
-                :is(audio, video)[controls],
-                embed,
-                area[href]:is(map[name]:not([name='']) area
-            )):not([tabindex='-1'])
-        \`;
+    return Array.from(document.querySelectorAll(parents))
+        .filter(parent => {
+            if (!isElementVisible(parent)) {
+                return false;
+            }
 
-        return Array.from(document.querySelectorAll(parents))
-            .filter(parent => {
-                if (!isElementVisible(parent)) {
-                    return false;
-                }
+            return Array.from(parent.querySelectorAll(children))
+                .some(child => isElementVisible(child));
+        })
+        .map(parent => {
+            return {
+                outerHTML: parent.outerHTML,
+                selector: getUniqueSelector(parent)
+            }    
+        })
+}
 
-                return Array.from(parent.querySelectorAll(children))
-                    .some(child => isElementVisible(child));
-            })
-            .map(parent => {
-                return {
-                    outerHTML: parent.outerHTML,
-                    selector: getUniqueSelector(parent)
-                }    
-            })
+export async function hasInteractiveControlsWithInteractiveControlsAsChildrenEval(auditResults) {
+    const interactiveControlsWithInteractiveControlsAsChildren = await inspectedWindowEval(`
+        const getUniqueSelector = ${getUniqueSelector.toString()};
+        const isElementVisible = ${isElementVisible.toString()};
+        const hasInteractiveControlsWithInteractiveControlsAsChildren = ${hasInteractiveControlsWithInteractiveControlsAsChildren.toString()};
+
+        return hasInteractiveControlsWithInteractiveControlsAsChildren();
     `) 
       
     interactiveControlsWithInteractiveControlsAsChildren.forEach((element) => {
--- scripts/audits/interactive-elements-audit/interactive-elements-audit.js
+++ scripts/audits/interactive-elements-audit/interactive-elements-audit.js
@@ -3,7 +3,7 @@ import { hasBrokenSkipLinksEval } from "./broken-skip-links/broken-skip-links.js
 import { hasEmptyButtonsEval } from "./empty-buttons/empty-buttons.js";
 import { hasEmptyLinksEval } from "./empty-links/empty-links.js";
 import { hasDuplicateAccesskeysEval } from "./has-duplicate-accesskey/has-duplicate-accesskey.js";
-import { hasInteractiveControlsWithInteractiveControlsAsChildren } from "./interactive-controls-w-interactive-children/interactive-controls-w-interactive-children.js";
+import { hasInteractiveControlsWithInteractiveControlsAsChildrenEval } from "./interactive-controls-w-interactive-children/interactive-controls-w-interactive-children.js";
 import { scrollableRegionKeyboardAccess } from "./scrollable-region-keyboard-access/scrollable-region-keyboard-access.js";
 import { touchTargetSize } from "./touch-target-size/touch-target-size.js";
 
@@ -11,7 +11,7 @@ export async function interactiveElementsAudit(auditResults) {
     await hasEmptyLinksEval(auditResults);
     await hasEmptyButtonsEval(auditResults);
     await hasBrokenSkipLinksEval(auditResults);
-    await hasInteractiveControlsWithInteractiveControlsAsChildren(auditResults);
+    await hasInteractiveControlsWithInteractiveControlsAsChildrenEval(auditResults);
     await hasBrokenSamePageLinksEval(auditResults);
     await hasDuplicateAccesskeysEval(auditResults);
     await touchTargetSize(auditResults);
