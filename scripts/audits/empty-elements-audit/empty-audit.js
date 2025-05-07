2c2bdb3bc24b58515517ed8439c600fc9340e51b
naiiiden <na4ko359@gmail.com>
2025-05-07 10:37:52 +0300

    empty-headings-with-images extract logic

--- scripts/audits/empty-elements-audit/empty-audit.js
+++ scripts/audits/empty-elements-audit/empty-audit.js
@@ -1,11 +1,11 @@
-import { hasEmptyHeadingsWithImages } from "./empty-headings-with-images/empty-headings-with-images.js";
+import { hasEmptyHeadingsWithImagesEval } from "./empty-headings-with-images/empty-headings-with-images.js";
 import { hasEmptyHeadingsEval } from "./empty-headings/empty-headings.js";
 import { hasEmptyOrMissingSummaries } from "./empty-summaries/empty-summaries.js";
 import { hasEmptyTableHeadings } from "./empty-table-headings/empty-table-headings.js";
 
 export async function emptyAudit(auditResults) {
     await hasEmptyHeadingsEval(auditResults);
-    await hasEmptyHeadingsWithImages(auditResults);
+    await hasEmptyHeadingsWithImagesEval(auditResults);
     await hasEmptyTableHeadings(auditResults);
     await hasEmptyOrMissingSummaries(auditResults);
 }
--- scripts/audits/empty-elements-audit/empty-headings-with-images/empty-headings-with-images.js
+++ scripts/audits/empty-elements-audit/empty-headings-with-images/empty-headings-with-images.js
@@ -3,27 +3,32 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
-export async function hasEmptyHeadingsWithImages(auditResults) {
+export function hasEmptyHeadingsWithImages() {
+    return Array.from(document.querySelectorAll(':is(h1, h2, h3, h4, h5, h6):has(img)'))
+        .filter(heading => 
+            isElementVisible(heading)
+        )
+        .map(heading => {
+            const textContent = heading.textContent.trim();
+            const img = heading.querySelector('img');
+            const imgAlt = img ? img.getAttribute('alt') : null;
+            return {
+                hasText: textContent.length > 0,
+                hasImage: img !== null,
+                hasImageAlt: imgAlt !== null && imgAlt.trim() !== "",
+                outerHTML: heading.outerHTML,
+                selector: getUniqueSelector(heading)
+            };
+        });
+}
+
+export async function hasEmptyHeadingsWithImagesEval(auditResults) {
     const emptyHeadingsWithImages = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const isElementVisible = ${isElementVisible.toString()};
-        
-        return Array.from(document.querySelectorAll(':is(h1, h2, h3, h4, h5, h6):has(img)'))
-            .filter(heading => 
-                isElementVisible(heading)
-            )
-            .map(heading => {
-                const textContent = heading.textContent.trim();
-                const img = heading.querySelector('img');
-                const imgAlt = img ? img.getAttribute('alt') : null;
-                return {
-                    hasText: textContent.length > 0,
-                    hasImage: img !== null,
-                    hasImageAlt: imgAlt !== null && imgAlt.trim() !== "",
-                    outerHTML: heading.outerHTML,
-                    selector: getUniqueSelector(heading)
-                };
-            });
+        const hasEmptyHeadingsWithImages = ${hasEmptyHeadingsWithImages.toString()};
+
+        return hasEmptyHeadingsWithImages();
     `)
       
     emptyHeadingsWithImages.forEach(heading => {
