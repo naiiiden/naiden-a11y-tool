0a8a12c77a4f3377a9fc719a21781188f6511318
naiiiden <na4ko359@gmail.com>
2025-05-07 11:20:27 +0300

    has-underlined-text extract logic

--- scripts/audits/css-audit/css-audit.js
+++ scripts/audits/css-audit/css-audit.js
@@ -1,9 +1,9 @@
 import { hasUnadjustableTextPropertiesEval } from "./has-unadjustable-text-properties/has-unadjustable-text-properties.js";
-import { hasUnderlinedText } from "./has-underlined-text/has-underlined-text.js";
+import { hasUnderlinedTextEval } from "./has-underlined-text/has-underlined-text.js";
 import { hasVerySmallText } from "./has-very-small-text/has-very-small-text.js";
 
 export async function cssAudit(auditResults) {
     await hasVerySmallText(auditResults);
-    await hasUnderlinedText(auditResults);
+    await hasUnderlinedTextEval(auditResults);
     await hasUnadjustableTextPropertiesEval(auditResults);
 }
--- scripts/audits/css-audit/has-underlined-text/has-underlined-text.js
+++ scripts/audits/css-audit/has-underlined-text/has-underlined-text.js
@@ -3,30 +3,35 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
-export async function hasUnderlinedText(auditResults) {
-    const hasUnderlinedText = await inspectedWindowEval(`
+export function hasUnderlinedText() {
+    return Array.from(document.querySelectorAll('*'))
+        .filter(element => {
+            if (!isElementVisible(element)) {
+                return false;
+            }
+    
+            const isUnderlined = window.getComputedStyle(element).textDecorationLine === 'underline';
+            const isExplicitUElement = element.tagName.toLowerCase() === 'u';
+            const isNotLink = element.tagName.toLowerCase() !== 'a';
+    
+            return (isUnderlined || isExplicitUElement) && isNotLink && element.textContent.trim().length > 0;
+        })
+        .map(element => ({
+            outerHTML: element.outerHTML,
+            selector: getUniqueSelector(element),
+        }));
+}
+
+export async function hasUnderlinedTextEval(auditResults) {
+    const underlinedText = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const isElementVisible = ${isElementVisible.toString()};
+        const hasUnderlinedText = ${hasUnderlinedText.toString()};
 
-        return Array.from(document.querySelectorAll('*'))
-            .filter(element => {
-                if (!isElementVisible(element)) {
-                    return false;
-                }
-
-                const isUnderlined = window.getComputedStyle(element).textDecorationLine === 'underline';
-                const isExplicitUElement = element.tagName.toLowerCase() === 'u';
-                const isNotLink = element.tagName.toLowerCase() !== 'a';
-
-                return (isUnderlined || isExplicitUElement) && isNotLink && element.textContent.trim().length > 0;
-            })
-            .map(element => ({
-                outerHTML: element.outerHTML,
-                selector: getUniqueSelector(element),
-            }));
+        return hasUnderlinedText();
     `);
 
-    hasUnderlinedText.forEach(element => {
+    underlinedText.forEach(element => {
         auditResults.push({
             ...cssErrors[1],
             element: element.outerHTML,
