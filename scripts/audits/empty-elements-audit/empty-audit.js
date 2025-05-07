a2942c77846cdfc8b9982dd2acb1b4f04285e742
naiiiden <na4ko359@gmail.com>
2025-05-07 10:40:44 +0300

    empty-table-headings extract logic

--- scripts/audits/empty-elements-audit/empty-audit.js
+++ scripts/audits/empty-elements-audit/empty-audit.js
@@ -1,11 +1,11 @@
 import { hasEmptyHeadingsWithImagesEval } from "./empty-headings-with-images/empty-headings-with-images.js";
 import { hasEmptyHeadingsEval } from "./empty-headings/empty-headings.js";
 import { hasEmptyOrMissingSummariesEval } from "./empty-summaries/empty-summaries.js";
-import { hasEmptyTableHeadings } from "./empty-table-headings/empty-table-headings.js";
+import { hasEmptyTableHeadingsEval } from "./empty-table-headings/empty-table-headings.js";
 
 export async function emptyAudit(auditResults) {
     await hasEmptyHeadingsEval(auditResults);
     await hasEmptyHeadingsWithImagesEval(auditResults);
-    await hasEmptyTableHeadings(auditResults);
+    await hasEmptyTableHeadingsEval(auditResults);
     await hasEmptyOrMissingSummariesEval(auditResults);
 }
--- scripts/audits/empty-elements-audit/empty-table-headings/empty-table-headings.js
+++ scripts/audits/empty-elements-audit/empty-table-headings/empty-table-headings.js
@@ -3,22 +3,27 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
-export async function hasEmptyTableHeadings(auditResults) {
+export function hasEmptyTableHeading() {
+    return Array.from(document.querySelectorAll("th, [role='rowheader'], [role='columnheader']"))
+        .filter(tableHeading => {
+            if (!isElementVisible(tableHeading)) {
+                return false;
+            }
+            return tableHeading.textContent.trim() === "";
+        })
+        .map(tableHeading => ({
+            outerHTML: tableHeading.outerHTML,
+            selector: getUniqueSelector(tableHeading)
+        }));
+}
+
+export async function hasEmptyTableHeadingsEval(auditResults) {
     const emptyTableHeadings = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const isElementVisible = ${isElementVisible.toString()};
+        const hasEmptyTableHeading = ${hasEmptyTableHeading.toString()};
 
-        return Array.from(document.querySelectorAll("th, [role='rowheader'], [role='columnheader']"))
-            .filter(tableHeading => {
-                if (!isElementVisible(tableHeading)) {
-                    return false;
-                }
-                return tableHeading.textContent.trim() === "";
-            })
-            .map(tableHeading => ({
-                outerHTML: tableHeading.outerHTML,
-                selector: getUniqueSelector(tableHeading)
-            }));
+        return hasEmptyTableHeading();
     `)
       
     emptyTableHeadings.forEach(tableHeading => {
