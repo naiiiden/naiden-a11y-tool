d82f37ea9955151c8b43aabf9f8b7ac70e0d8a3a
naiiiden <na4ko359@gmail.com>
2025-05-07 11:21:50 +0300

    has-very-small-text extract logic

--- scripts/audits/css-audit/css-audit.js
+++ scripts/audits/css-audit/css-audit.js
@@ -1,9 +1,9 @@
 import { hasUnadjustableTextPropertiesEval } from "./has-unadjustable-text-properties/has-unadjustable-text-properties.js";
 import { hasUnderlinedTextEval } from "./has-underlined-text/has-underlined-text.js";
-import { hasVerySmallText } from "./has-very-small-text/has-very-small-text.js";
+import { hasVerySmallTextEval } from "./has-very-small-text/has-very-small-text.js";
 
 export async function cssAudit(auditResults) {
-    await hasVerySmallText(auditResults);
+    await hasVerySmallTextEval(auditResults);
     await hasUnderlinedTextEval(auditResults);
     await hasUnadjustableTextPropertiesEval(auditResults);
 }
--- scripts/audits/css-audit/has-very-small-text/has-very-small-text.js
+++ scripts/audits/css-audit/has-very-small-text/has-very-small-text.js
@@ -3,29 +3,34 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
-export async function hasVerySmallText(auditResults) {
-    const hasVerySmallText = await inspectedWindowEval(`
+export function hasVerySmallText() {
+    return Array.from(document.querySelectorAll('*'))
+        .filter(element => {
+            if (!isElementVisible(element)) {
+                return false;
+            }
+    
+            const computedStyle = window.getComputedStyle(element);
+            const fontSize = parseFloat(computedStyle.fontSize);
+            return fontSize > 0 && fontSize <= 10 && element.textContent.trim().length > 0;
+        })
+        .map(element => ({
+            outerHTML: element.outerHTML,
+            selector: getUniqueSelector(element),
+            fontSize: window.getComputedStyle(element).fontSize
+        }));
+}
+
+export async function hasVerySmallTextEval(auditResults) {
+    const verySmallText = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const isElementVisible = ${isElementVisible.toString()};
+        const hasVerySmallText = ${hasVerySmallText.toString()};
 
-        return Array.from(document.querySelectorAll('*'))
-            .filter(element => {
-                if (!isElementVisible(element)) {
-                    return false;
-                }
-
-                const computedStyle = window.getComputedStyle(element);
-                const fontSize = parseFloat(computedStyle.fontSize);
-                return fontSize > 0 && fontSize <= 10 && element.textContent.trim().length > 0;
-            })
-            .map(element => ({
-                outerHTML: element.outerHTML,
-                selector: getUniqueSelector(element),
-                fontSize: window.getComputedStyle(element).fontSize
-            }));
+        return hasVerySmallText();
     `);
 
-    hasVerySmallText.forEach(element => {
+    verySmallText.forEach(element => {
         auditResults.push({
             ...cssErrors[0],
             element: element.outerHTML,
