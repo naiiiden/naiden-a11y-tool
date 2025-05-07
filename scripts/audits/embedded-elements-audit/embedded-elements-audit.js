b129a2d5b00188f3cbbcf5a78570f665bb1a4c90
naiiiden <na4ko359@gmail.com>
2025-05-07 11:01:04 +0300

    video-audio-no-autoplay extract logic

--- scripts/audits/embedded-elements-audit/embedded-elements-audit.js
+++ scripts/audits/embedded-elements-audit/embedded-elements-audit.js
@@ -2,12 +2,12 @@ import { hasFrameUniqueTitlesEval } from "./frame-unique-title/frame-unique-titl
 import { hasVideoCaptionsEval } from "./has-video-captions/has-video-captions.js";
 import { hasEmptyIframeTitlesEval } from "./iframe-titles/iframe-titles.js";
 import { hasObjectAltTextEval } from "./object-has-alt-text/object-has-alt-text.js";
-import { videOrAudioHasAutoplay } from "./video-audio-no-autoplay/video-audio-no-autoplay.js";
+import { hasVideOrAudioAutoplayEval } from "./video-audio-no-autoplay/video-audio-no-autoplay.js";
 
 export async function embeddedElementsAudit(auditResults) {
     await hasEmptyIframeTitlesEval(auditResults);
     await hasObjectAltTextEval(auditResults);
     await hasFrameUniqueTitlesEval(auditResults);
-    await videOrAudioHasAutoplay(auditResults);
+    await hasVideOrAudioAutoplayEval(auditResults);
     await hasVideoCaptionsEval(auditResults);
 }
--- scripts/audits/embedded-elements-audit/video-audio-no-autoplay/video-audio-no-autoplay.js
+++ scripts/audits/embedded-elements-audit/video-audio-no-autoplay/video-audio-no-autoplay.js
@@ -3,23 +3,28 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
-export async function videOrAudioHasAutoplay(auditResults) {
-    const videOrAudioHasAutoplay = await inspectedWindowEval(`
+export function hasVideOrAudioAutoplay() {
+    return Array.from(document.querySelectorAll(":is(audio, video)[autoplay]:not([controls])"))
+        .filter(element => isElementVisible(element))
+        .filter(element => {
+            return (element.duration || 0) > 3;    
+        })
+        .map(element => ({
+            outerHTML: element.outerHTML,
+            selector: getUniqueSelector(element)
+        }));
+}
+
+export async function hasVideOrAudioAutoplayEval(auditResults) {
+    const videOrAudioAutoplay = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const isElementVisible = ${isElementVisible.toString()};
+        const hasVideOrAudioAutoplay = ${hasVideOrAudioAutoplay.toString()};
 
-        return Array.from(document.querySelectorAll(":is(audio, video)[autoplay]:not([controls])"))
-            .filter(element => isElementVisible(element))
-            .filter(element => {
-                return (element.duration || 0) > 3;    
-            })
-            .map(element => ({
-                outerHTML: element.outerHTML,
-                selector: getUniqueSelector(element)
-            }));
+        return hasVideOrAudioAutoplay();
     `) 
   
-    videOrAudioHasAutoplay.forEach(element => {
+    videOrAudioAutoplay.forEach(element => {
         auditResults.push({ ...embeddedElementsErrors[3], element: element.outerHTML, selector: element.selector });
     });
 }
