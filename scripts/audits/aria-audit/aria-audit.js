5e73e75fb48a6a3b3c9965ec268d41f9d6f1a94c
naiiiden <na4ko359@gmail.com>
2025-05-09 09:15:13 +0300

    aria-label-content-name-mismatch extract logic

--- scripts/audits/aria-audit/aria-audit.js
+++ scripts/audits/aria-audit/aria-audit.js
@@ -21,7 +21,7 @@ import { ariaValidAttributes } from "./aria-valid-attributes/aria-valid-attribut
 import { hasAriaDeprecatedRoles } from "./has-aria-deprecated-roles/has-aria-deprecated-roles.js";
 import { hasAriaHiddenBody } from "./has-aria-hidden-body/has-aria-hidden-body.js";
 import { hasRolePresentationOrNoneConflict } from "./has-role-presentation-or-none-conflict/has-role-presentation-or-none-conflict.js";
-import { ariaLabelContentNameMismatch } from "./aria-label-content-name-mismatch/aria-label-content-name-mismatch.js";
+import { hasAriaLabelContentNameMismatchEval } from "./aria-label-content-name-mismatch/aria-label-content-name-mismatch.js";
 
 export async function ariaAudit(auditResults) {
     await hasAriaHiddenBody(auditResults);
@@ -47,5 +47,5 @@ export async function ariaAudit(auditResults) {
     await ariaRoleProhibitedAttributes(auditResults);
     await hasAriaConditionalAttributesEval(auditResults);
     await hasRolePresentationOrNoneConflict(auditResults);
-    await ariaLabelContentNameMismatch(auditResults);
+    await hasAriaLabelContentNameMismatchEval(auditResults);
 }
--- scripts/audits/aria-audit/aria-label-content-name-mismatch/aria-label-content-name-mismatch.js
+++ scripts/audits/aria-audit/aria-label-content-name-mismatch/aria-label-content-name-mismatch.js
@@ -3,25 +3,30 @@ import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 import { isElementVisible } from "../../../utils/is-element-visible.js";
 
-export async function ariaLabelContentNameMismatch(auditResults) {
+export function AriaLabelContentNameMismatch() {
+    return Array.from(document.querySelectorAll(":is([role='button'], [role='checkbox'], [role='gridcell'], [role='link'], [role='menuitem'], [role='menuitemcheckbox'], [role='menuitemradio'], [role='option'], [role='radio'], [role='searchbox'], [role='switch'], [role='tab'], [role='treeitem'])[aria-label]:not([aria-label=''])"))
+        .filter(element => {
+            if (!isElementVisible(element)) {
+                return false;
+            }
+    
+            const elementTextContent = element.textContent.trim().toLowerCase();
+            const elementAriaLabel = element.getAttribute("aria-label");
+            return !elementAriaLabel.includes(elementTextContent);
+        })
+        .map(element => ({
+            outerHTML: element.outerHTML,
+            selector: getUniqueSelector(element)
+        }));
+}
+
+export async function hasAriaLabelContentNameMismatchEval(auditResults) {
     const ariaLabelContentNameMismatch = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
         const isElementVisible = ${isElementVisible.toString()};
+        const AriaLabelContentNameMismatch = ${AriaLabelContentNameMismatch.toString()};
 
-        return Array.from(document.querySelectorAll(":is([role='button'], [role='checkbox'], [role='gridcell'], [role='link'], [role='menuitem'], [role='menuitemcheckbox'], [role='menuitemradio'], [role='option'], [role='radio'], [role='searchbox'], [role='switch'], [role='tab'], [role='treeitem'])[aria-label]:not([aria-label=''])"))
-            .filter(element => {
-                if (!isElementVisible(element)) {
-                    return false;
-                }
-
-                const elementTextContent = element.textContent.trim().toLowerCase();
-                const elementAriaLabel = element.getAttribute("aria-label");
-                return !elementAriaLabel.includes(elementTextContent);
-            })
-            .map(element => ({
-                outerHTML: element.outerHTML,
-                selector: getUniqueSelector(element)
-            }));
+        return AriaLabelContentNameMismatch();
     `) 
     
     ariaLabelContentNameMismatch.forEach(element => {
