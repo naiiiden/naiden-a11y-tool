f275fcf320c6258c65bdc42a94dd97a0bfb11179
naiiiden <na4ko359@gmail.com>
2025-05-09 10:00:58 +0300

    has-aria-hidden-body extract logic

--- scripts/audits/aria-audit/aria-audit.js
+++ scripts/audits/aria-audit/aria-audit.js
@@ -19,12 +19,12 @@ import { hasAriaTooltipNamesEval } from "./aria-tooltip-names/aria-tooltip-names
 import { hasAriaTreeitemNamesEval } from "./aria-treeitem-names/aria-treeitem-names.js";
 import { hasAriaValidAttributesEval } from "./aria-valid-attributes/aria-valid-attributes.js";
 import { hasAriaDeprecatedRolesEval } from "./has-aria-deprecated-roles/has-aria-deprecated-roles.js";
-import { hasAriaHiddenBody } from "./has-aria-hidden-body/has-aria-hidden-body.js";
+import { hasAriaHiddenBodyEval } from "./has-aria-hidden-body/has-aria-hidden-body.js";
 import { hasRolePresentationOrNoneConflict } from "./has-role-presentation-or-none-conflict/has-role-presentation-or-none-conflict.js";
 import { hasAriaLabelContentNameMismatchEval } from "./aria-label-content-name-mismatch/aria-label-content-name-mismatch.js";
 
 export async function ariaAudit(auditResults) {
-    await hasAriaHiddenBody(auditResults);
+    await hasAriaHiddenBodyEval(auditResults);
     await hasAriaCommandsNamesEval(auditResults);
     await hasAriaMeterNamesEval(auditResults);
     await hasAriaProgressbarNamesEval(auditResults);
--- scripts/audits/aria-audit/has-aria-hidden-body/has-aria-hidden-body.js
+++ scripts/audits/aria-audit/has-aria-hidden-body/has-aria-hidden-body.js
@@ -2,18 +2,24 @@ import { ariaErrors } from "../../../errors/aria.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
 
-export async function hasAriaHiddenBody(auditResults) {
+export function hasAriaHiddenBody() {
+    const body = document.body;
+    const isHidden = body.hasAttribute('aria-hidden') && body.getAttribute('aria-hidden') === 'true';
+    
+    return {
+        isHidden,
+        outerHTML: body.cloneNode().outerHTML,
+        selector: getUniqueSelector(body)
+    }
+}
+
+export async function hasAriaHiddenBodyEval(auditResults) {
     // https://dequeuniversity.com/rules/axe/4.10/aria-hidden-body
     const documentBody = await inspectedWindowEval(`
         const getUniqueSelector = ${getUniqueSelector.toString()};
-        const body = document.body;
-        const isHidden = body.hasAttribute('aria-hidden') && body.getAttribute('aria-hidden') === 'true';
-        
-        return {
-            isHidden,
-            outerHTML: body.cloneNode().outerHTML,
-            selector: getUniqueSelector(body)
-        }
+        const hasAriaHiddenBody = ${hasAriaHiddenBody.toString()};
+
+        return hasAriaHiddenBody();
     `)
 
     if (documentBody.isHidden) {
