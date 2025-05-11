e0b546e37e36c87293b43c3ef1e9eb8c21501b44
naiiiden <na4ko359@gmail.com>
2025-05-11 15:45:38 +0300

    empty elements naming

--- scripts/audits/empty-elements-audit/empty-audit.js
+++ scripts/audits/empty-elements-audit/empty-audit.js
@@ -1,7 +1,7 @@
-import { hasEmptyHeadingsWithImagesEval } from "./empty-headings-with-images/empty-headings-with-images.js";
-import { hasEmptyHeadingsEval } from "./empty-headings/empty-headings.js";
-import { hasEmptyOrMissingSummariesEval } from "./empty-summaries/empty-summaries.js";
-import { hasEmptyTableHeadingsEval } from "./empty-table-headings/empty-table-headings.js";
+import { hasEmptyHeadingsWithImagesEval } from "./has-empty-headings-with-images/has-empty-headings-with-images.js";
+import { hasEmptyHeadingsEval } from "./has-empty-headings/has-empty-headings.js";
+import { hasEmptyOrMissingSummariesEval } from "./has-empty-summaries/has-empty-summaries.js";
+import { hasEmptyTableHeadingsEval } from "./has-empty-table-headings/has-empty-table-headings.js";
 
 export async function emptyAudit(auditResults) {
   await hasEmptyHeadingsEval(auditResults);
