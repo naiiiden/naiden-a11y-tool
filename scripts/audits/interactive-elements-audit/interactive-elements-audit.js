bec9148791e4f31b9c892e42267ef3cbc1ea2997
naiiiden <na4ko359@gmail.com>
2025-05-11 14:03:32 +0300

    follow naming convention pt2

--- scripts/audits/interactive-elements-audit/interactive-elements-audit.js
+++ scripts/audits/interactive-elements-audit/interactive-elements-audit.js
@@ -1,10 +1,10 @@
 import { hasBrokenSamePageLinksEval } from "./has-broken-same-page-links/has-broken-same-page-links.js";
 import { hasBrokenSkipLinksEval } from "./has-broken-skip-links/has-broken-skip-links.js";
-import { hasEmptyButtonsEval } from "./empty-buttons/empty-buttons.js";
-import { hasEmptyLinksEval } from "./empty-links/empty-links.js";
+import { hasEmptyButtonsEval } from "./has-empty-buttons/has-empty-buttons.js";
+import { hasEmptyLinksEval } from "./has-empty-links/has-empty-links.js";
 import { hasDuplicateAccesskeysEval } from "./has-duplicate-accesskey/has-duplicate-accesskey.js";
 import { hasInteractiveControlsWithInteractiveControlsAsChildrenEval } from "./has-interactive-controls-w-interactive-children/has-interactive-controls-w-interactive-children.js";
-import { scrollableRegionKeyboardAccess } from "./scrollable-region-keyboard-access/scrollable-region-keyboard-access.js";
+import { scrollableRegionKeyboardAccess } from "./has-scrollable-region-keyboard-access/has-scrollable-region-keyboard-access.js";
 import { touchTargetSizeEval } from "./has-sufficient-touch-target-size/has-sufficient-touch-target-size.js";
 
 export async function interactiveElementsAudit(auditResults) {
