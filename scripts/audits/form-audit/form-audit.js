44eae6bebe14d81e511183533f682f1fd5df6c40
naiiiden <na4ko359@gmail.com>
2025-05-11 15:49:23 +0300

    form audit naming

--- scripts/audits/form-audit/form-audit.js
+++ scripts/audits/form-audit/form-audit.js
@@ -1,11 +1,11 @@
-import { hasEmptyLabelsEval } from "./empty-labels/empty-labels.js";
-import { hasEmptySubmitButonOrResetInputValuesEval } from "./empty-submit-button-reset-values/empty-submit-button-reset-values.js";
-import { hasFieldsetsMissingLegendEval } from "./fieldsets-missing-legend/fieldsets-missing-legend.js";
-import { hasFormControlLabelsEval } from "./form-control-labels/form-control-labels.js";
-import { hasVisibleFormControlLabelsEval } from "./form-control-visible-labels/form-control-visible-labels.js";
+import { hasEmptyLabelsEval } from "./has-empty-labels/has-empty-labels.js";
+import { hasEmptySubmitButonOrResetInputValuesEval } from "./has-empty-submit-button-reset-values/has-empty-submit-button-reset-values.js";
+import { hasFieldsetsMissingLegendEval } from "./has-fieldsets-missing-legend/has-fieldsets-missing-legend.js";
+import { hasFormControlLabelsEval } from "./has-form-control-labels/has-form-control-labels.js";
+import { hasVisibleFormControlLabelsEval } from "./has-form-control-visible-labels/has-form-control-visible-labels.js";
 import { hasAutocompleteValidValuesEval } from "./has-autocomplete-valid-values/has-autocomplete-valid-values.js";
-import { hasImageInputsEval } from "./image-inputs/image-inputs.js";
-import { hasRelatedFormControlsMissingFieldsetEval } from "./related-form-controls-without-fieldset/related-form-controls-without-fieldset.js.js";
+import { hasImageInputsEval } from "./has-image-inputs/has-image-inputs.js";
+import { hasRelatedFormControlsMissingFieldsetEval } from "./has-related-form-controls-without-fieldset/has-related-form-controls-without-fieldset.js.js";
 
 export async function formAudit(auditResults) {
   await hasEmptyLabelsEval(auditResults);
