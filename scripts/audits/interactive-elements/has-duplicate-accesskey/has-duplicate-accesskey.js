3b76b7bbe342b38c0ee7e010a815d52c30ee49b4
naiiiden <na4ko359@gmail.com>
2025-05-13 10:08:10 +0300

    has-duplicate-accesskey use iselementvisible

--- scripts/audits/interactive-elements/has-duplicate-accesskey/has-duplicate-accesskey.js
+++ scripts/audits/interactive-elements/has-duplicate-accesskey/has-duplicate-accesskey.js
@@ -1,20 +1,23 @@
 import { interactiveElementsErrors } from "../../../errors/interactive-elements.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export function hasDuplicateAccesskeys() {
   const elementsWithAccesskeys = Array.from(
-    document.querySelectorAll("[accesskey]:not([accesskey=''])"),
+    document.querySelectorAll("[accesskey]:not([accesskey=''])")
   );
 
-  const duplicates = elementsWithAccesskeys.reduce((acc, element) => {
-    const accesskey = element.getAttribute("accesskey").trim();
-    if (!acc[accesskey]) {
-      acc[accesskey] = [];
-    }
-    acc[accesskey].push(element);
-    return acc;
-  }, {});
+  const duplicates = elementsWithAccesskeys
+    .filter((element) => isElementVisible(element))
+    .reduce((acc, element) => {
+      const accesskey = element.getAttribute("accesskey").trim();
+      if (!acc[accesskey]) {
+        acc[accesskey] = [];
+      }
+      acc[accesskey].push(element);
+      return acc;
+    }, {});
 
   return Object.values(duplicates)
     .filter((elements) => elements.length > 1)
@@ -22,13 +25,14 @@ export function hasDuplicateAccesskeys() {
       elements.map((element) => ({
         selector: getUniqueSelector(element),
         outerHTML: element.cloneNode().outerHTML,
-      })),
+      }))
     );
 }
 
 export async function hasDuplicateAccesskeysEval(auditResults) {
   const duplicateAccesskeys = await inspectedWindowEval(`
     const getUniqueSelector = ${getUniqueSelector.toString()};
+    const isElementVisible = ${isElementVisible.toString()};
     const hasDuplicateAccesskeys = ${hasDuplicateAccesskeys.toString()};
 
     return hasDuplicateAccesskeys();
