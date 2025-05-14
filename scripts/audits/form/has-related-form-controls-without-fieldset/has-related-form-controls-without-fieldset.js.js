d34d954bff27b5f9d4db678b9bcbcb91a3cea5ea
naiiiden <na4ko359@gmail.com>
2025-05-14 11:19:33 +0300

    has-related-form-controls-without-fieldset isElementVisible

--- scripts/audits/form/has-related-form-controls-without-fieldset/has-related-form-controls-without-fieldset.js.js
+++ scripts/audits/form/has-related-form-controls-without-fieldset/has-related-form-controls-without-fieldset.js.js
@@ -1,18 +1,21 @@
 import { formErrors } from "../../../errors/forms.js";
 import { getUniqueSelector } from "../../../utils/get-unique-selector.js";
 import { inspectedWindowEval } from "../../../utils/inspected-window-eval.js";
+import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export function hasRelatedFormControlsMissingFieldset() {
   const elements = Array.from(
     document.querySelectorAll(
-      'input[type="radio"]:not(fieldset input, [name=""], :not([name])), input[type="checkbox"]:not(fieldset input, [name=""], :not([name]))',
-    ),
+      'input[type="radio"]:not(fieldset input, [name=""], :not([name])), input[type="checkbox"]:not(fieldset input, [name=""], :not([name]))'
+    )
   );
-  const groupedByName = elements.reduce((groups, input) => {
-    if (!groups[input.name]) groups[input.name] = [];
-    groups[input.name].push(input);
-    return groups;
-  }, {});
+  const groupedByName = elements
+    .filter((element) => isElementVisible(element))
+    .reduce((groups, input) => {
+      if (!groups[input.name]) groups[input.name] = [];
+      groups[input.name].push(input);
+      return groups;
+    }, {});
 
   return Object.values(groupedByName)
     .filter((group) => group.length > 1)
@@ -20,13 +23,14 @@ export function hasRelatedFormControlsMissingFieldset() {
       group.map((input) => ({
         outerHTML: input.outerHTML,
         selector: getUniqueSelector(input),
-      })),
+      }))
     );
 }
 
 export async function hasRelatedFormControlsMissingFieldsetEval(auditResults) {
   const relatedFormControlsMissingFieldset = await inspectedWindowEval(`
     const getUniqueSelector = ${getUniqueSelector.toString()};
+    const isElementVisible = ${isElementVisible.toString()};
     const hasRelatedFormControlsMissingFieldset = ${hasRelatedFormControlsMissingFieldset.toString()};
 
     return hasRelatedFormControlsMissingFieldset();
