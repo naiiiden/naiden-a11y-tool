2df2b34a4254fed0bb236eb9140e4ca0ab4e6504
naiiiden <na4ko359@gmail.com>
2025-05-15 09:22:07 +0300

    has-contentinfo-in-other-landmarks

--- scripts/audits/semantic/has-contentinfo-in-other-landmarks/has-contentinfo-in-other-landmarks.js
+++ scripts/audits/semantic/has-contentinfo-in-other-landmarks/has-contentinfo-in-other-landmarks.js
@@ -5,13 +5,48 @@ import { isElementVisible } from "../../../utils/is-element-visible.js";
 
 export function hasContentinfoInOtherLandmarks() {
   return Array.from(
+    // :is(header, nav, main, section, form, aside, footer, [role="banner"], [role="navigation"], [role="main"], 
+    //         :is([role="region"], [role="form"]):is(
+    //             [aria-labelledby]:not([aria-labelledby=""]), [aria-label]:not([aria-label=""]), [title]:not([title=""])
+    //         ), 
+    //         [role="complementary"], [role="contentinfo"], [role="search"])
+    // :is(footer:not([role]):not(:is(article, aside, main, nav, section) footer:not([role])), [role="contentinfo"])
     document.querySelectorAll(`
-        :is(header, nav, main, section, form, aside, footer, [role="banner"], [role="navigation"], [role="main"], 
-                :is([role="region"], [role="form"]):is(
-                    [aria-labelledby]:not([aria-labelledby=""]), [aria-label]:not([aria-label=""]), [title]:not([title=""])
-                ), 
-                [role="complementary"], [role="contentinfo"], [role="search"])
-        :is(footer:not([role]):not(:is(article, aside, main, nav, section) footer:not([role])), [role="contentinfo"])
+      header footer:not([role]), 
+      header [role="contentinfo"], 
+      nav [role="contentinfo"],
+      main [role="contentinfo"],
+      section [role="contentinfo"], 
+      form footer:not([role]), 
+      form [role="contentinfo"], 
+      article [role="contentinfo"], 
+      aside [role="contentinfo"],
+      footer footer:not([role]), 
+      footer [role="contentinfo"],
+      [role="banner"] footer:not([role]), 
+      [role="banner"] [role="contentinfo"], 
+      [role="navigation"] footer:not([role]), 
+      [role="navigation"] [role="contentinfo"], 
+      [role="main"] footer:not([role]), 
+      [role="main"] [role="contentinfo"], 
+      [role="complementary"] footer:not([role]), 
+      [role="complementary"] [role="contentinfo"],
+      [role="contentinfo"] footer:not([role]), 
+      [role="contentinfo"] [role="contentinfo"],
+      [role="search"] footer:not([role]), 
+      [role="search"] [role="contentinfo"], 
+      [role="region"]:is([aria-labelledby]:not([aria-labelledby=""])) footer:not([role]), 
+      [role="region"]:is([aria-labelledby]:not([aria-labelledby=""])) [role="contentinfo"],
+      [role="region"]:is([aria-label]:not([aria-label=""])) footer:not([role]), 
+      [role="region"]:is([aria-label]:not([aria-label=""])) [role="contentinfo"], 
+      [role="region"]:is([title]:not([title=""])) footer:not([role]), 
+      [role="region"]:is([title]:not([title=""])) [role="contentinfo"], 
+      [role="form"]:is([aria-labelledby]:not([aria-labelledby=""])) footer:not([role]), 
+      [role="form"]:is([aria-labelledby]:not([aria-labelledby=""])) [role="contentinfo"],
+      [role="form"]:is([aria-label]:not([aria-label=""])) footer:not([role]), 
+      [role="form"]:is([aria-label]:not([aria-label=""])) [role="contentinfo"], 
+      [role="form"]:is([title]:not([title=""])) footer:not([role]), 
+      [role="form"]:is([title]:not([title=""])) [role="contentinfo"]
     `),
   )
     .filter((footer) => isElementVisible(footer))
