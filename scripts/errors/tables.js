4684e80defe789ae5ae78884b8be115b5ec7716f
naiiiden <na4ko359@gmail.com>
2025-05-26 22:44:11 +0300

    table error 1

--- scripts/errors/tables.js
+++ scripts/errors/tables.js
@@ -13,4 +13,18 @@ export const tableErrors = [
     ],
     fix: "Check that the scope attribute is only being used on th elements. The value must be either row or col",
   },
+  {
+    type: "table",
+    id: 1,
+    name: "Tables should not have the same summary and caption",
+    description:
+      "Data tables that have both a summary and caption should not have the same text",
+    wcagLinks: [
+      {
+        name: "",
+        url: "",
+      },
+    ],
+    fix: "Ensure that the content of the summary attribute and the <caption> elements are not identical",
+  },
 ];
