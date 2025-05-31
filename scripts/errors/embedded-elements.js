4429df2e89db1467e63a65b149a1046cedaeb9e0
naiiiden <na4ko359@gmail.com>
2025-05-31 21:59:59 +0300

    embedded elements texts update

--- scripts/errors/embedded-elements.js
+++ scripts/errors/embedded-elements.js
@@ -2,11 +2,11 @@ export const embeddedElementsErrors = [
   {
     type: "embedded-elements",
     id: 0,
-    name: "Empty or missing iframe title",
+    name: "Empty or missing <iframe> title",
     description: "Iframe has an empty or missing title.",
     wcagLinks: [
       {
-        name: "Using the title attribute of the iframe element",
+        name: "Using the title attribute of the <iframe> element",
         url: "https://www.w3.org/WAI/WCAG22/Techniques/html/H64",
       },
       {
@@ -14,16 +14,16 @@ export const embeddedElementsErrors = [
         url: "https://www.w3.org/WAI/WCAG22/Understanding/name-role-value.html",
       },
     ],
-    fix: "Ensure every iframe element has an accessible name.",
+    fix: "Ensure every <iframe> element has an accessible name.",
   },
   {
     type: "embedded-elements",
     id: 1,
-    name: "Object elements must have alt text",
-    description: "Object element is missing alt text",
+    name: "<object> elements must have alt text",
+    description: "<object> element is missing alt text",
     wcagLinks: [
       {
-        name: "Using the body of the object element",
+        name: "Using the body of the <object> element",
         url: "https://www.w3.org/WAI/WCAG22/Techniques/html/H53",
       },
       {
@@ -31,31 +31,31 @@ export const embeddedElementsErrors = [
         url: "https://www.w3.org/WAI/WCAG22/Understanding/non-text-content.html",
       },
     ],
-    fix: "Ensures every object element has alt text",
+    fix: "Ensures every <object> element has alt text",
   },
   {
     type: "embedded-elements",
     id: 2,
-    name: "Iframe elements must have a unique title attribute",
-    description: "Iframe element has a non-unique title attribute",
+    name: "<iframe> elements must have a unique title attribute",
+    description: "<iframe> element has a non-unique title attribute",
     wcagLinks: [
       {
         name: "Name, Role, Value (Level A)",
         url: "https://www.w3.org/WAI/WCAG22/Understanding/name-role-value.html",
       },
       {
-        name: "Using the title attribute of the iframe element",
+        name: "Using the title attribute of the <iframe> element",
         url: "https://www.w3.org/WAI/WCAG22/Techniques/html/H64",
       },
     ],
-    fix: "Ensure all iframe elements contain a unique and non-empty title attribute",
+    fix: "Ensure all <iframe> elements contain a unique and non-empty title attribute",
   },
   {
     type: "embedded-elements",
     id: 3,
-    name: "Video or audio elements must not autoplay",
+    name: "<video> or <audio> elements must not autoplay",
     description:
-      "A video or audio element autoplays audio for more than 3 seconds without a control mechanism to stop or mute the audio",
+      "A <video> or <audio> element autoplays audio for more than 3 seconds without a control mechanism to stop or mute the audio",
     wcagLinks: [
       {
         name: "Audio Control (Level A)",
@@ -74,14 +74,14 @@ export const embeddedElementsErrors = [
         url: "https://www.w3.org/WAI/WCAG22/Techniques/general/G171",
       },
     ],
-    fix: "Ensure all video or audio elements do not autoplay",
+    fix: "Ensure all <video> or <audio> elements do not autoplay",
   },
   {
     type: "embedded-elements",
     id: 4,
-    name: "Video elements must have captions",
+    name: "<video> elements must have captions",
     description:
-      "Video element lacks must have captions which should convey all meaningful auditory information in the video including dialogue, musical cues, sound effects, and other relevant information for deaf users",
+      "<video> element lacks captions which should convey all meaningful auditory information in the video including dialogue, musical cues, sound effects, and other relevant information for deaf users",
     wcagLinks: [
       {
         name: "Providing closed captions",
@@ -100,6 +100,6 @@ export const embeddedElementsErrors = [
         url: "https://www.w3.org/WAI/WCAG22/Understanding/captions-prerecorded.html",
       },
     ],
-    fix: "Ensure video elements have captions",
+    fix: "Ensure <video> elements have captions",
   },
 ];
